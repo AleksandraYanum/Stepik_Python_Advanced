87c9c3c31373e81eeb080034f569b9d8d6ee4164
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-04-20 13:31:46 +0200

    Refactoring: split logic to different finctions

--- Chapter_0/0_0/Valid_phone_number.py
+++ Chapter_0/0_0/Valid_phone_number.py
@@ -1,35 +1,40 @@
-PATTERN_DIGIT_CHAR = 'd'
+PATTERN_DIGIT = 'd'
 PATTERN_SEP_CHAR = 'p'
 
 PATTERN_SEP_LIST = '-_.,:'
 
 
-def check_phone_number(phone_number, pattern_list):
-    is_any_pattern_matched = False
-
+def check_phone_number_by_pattern(phone_number, pattern):
+    is_pattern_matched = True
     phone_number_len = len(phone_number)
 
-    for pattern in pattern_list:
-        is_pattern_matched = True
+    if phone_number_len != len(pattern):
+        is_pattern_matched = False
+    else:
+        for i in range(phone_number_len):
 
-        if phone_number_len != len(pattern):
-            is_pattern_matched = False
-        else:
-            for i in range(phone_number_len):
-
-                if pattern[i] == PATTERN_DIGIT_CHAR:
-                    if not phone_number[i].isdigit():
-                        is_pattern_matched = False
-                        break
-
-                elif pattern[i] == PATTERN_SEP_CHAR:
-                    if phone_number[i] not in PATTERN_SEP_LIST:
-                        is_pattern_matched = False
-                        break
-
-                elif pattern[i] != phone_number[i]:
+            if pattern[i] == PATTERN_DIGIT:
+                if not phone_number[i].isdigit():
                     is_pattern_matched = False
                     break
+
+            elif pattern[i] == PATTERN_SEP_CHAR:
+                if phone_number[i] not in PATTERN_SEP_LIST:
+                    is_pattern_matched = False
+                    break
+
+            elif pattern[i] != phone_number[i]:
+                is_pattern_matched = False
+                break
+
+    return is_pattern_matched
+
+
+def check_phone_number_by_pattern_list(phone_number, pattern_list):
+    is_any_pattern_matched = False
+
+    for pattern in pattern_list:
+        is_pattern_matched = check_phone_number_by_pattern(phone_number, pattern)
 
         if is_pattern_matched:
             is_any_pattern_matched = True
@@ -40,6 +45,7 @@ def check_phone_number(phone_number, pattern_list):
 
 ##################################################################################
 
+
 PATTERN_LIST = ['7-ddd-ddd-dddd', '7-dddpddpdd', 'ddd-ddd-dddd', '+375-dd-ddd-dddd', 'X_1_dps']
 VALID_MSG = "Phone number is VALID"
 NOT_VALID_MSG = "Phone number is NOT VALID"
@@ -48,7 +54,7 @@ NOT_VALID_MSG = "Phone number is NOT VALID"
 def main():
     phone_number = input()
 
-    is_phone_valid = check_phone_number(phone_number, PATTERN_LIST)
+    is_phone_valid = check_phone_number_by_pattern_list(phone_number, PATTERN_LIST)
 
     output = NOT_VALID_MSG
     if is_phone_valid:
