8f1fe831b238cd1a5d8b1ba60d2eb596d2b8bc4d
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-03 22:08:47 +0200

    Created 4_more_than_average.py

---
+++ 4_Lists/4_4_Matrix_Part_1/4_more_than_average.py
@@ -0,0 +1,16 @@
+arithmetic_mean = 0
+result_arr = []
+
+n = int(input())
+
+matrix = [[int(num) for num in input().split()] for _ in range(n)]
+
+for row in range(n):
+    num_count = 0
+    arithmetic_mean = sum(matrix[row]) / n
+    for col in range(n):
+        if matrix[row][col] > arithmetic_mean:
+            num_count += 1
+    result_arr.append(num_count)
+
+print(*result_arr, sep='\n')
