530df99266ed7095136033a239d4d2fcbb68230d
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-05-03 21:41:05 +0200

    4_4_6_Max_in_place (#1)
    * Create 6_Max_in_place.py
    
    * Added comment
    
    * Removed comment
    
    * Added new line

---
+++ 4_Lists/4_4_Matrix_Part_1/6_Max_in_place.py
@@ -0,0 +1,20 @@
+def main():
+    matrix_size = int(input())
+
+    matrix = [[int(num) for num in input().split()] for _ in range(matrix_size)]
+
+    max_matrix_num = matrix[0][0]
+    for row in range(matrix_size // 2 + matrix_size % 2):
+        for col in range(row + 1):
+            current_max_num = max([matrix[row][col],
+                                   matrix[row][matrix_size - col - 1],
+                                   matrix[matrix_size - row - 1][matrix_size - col - 1],
+                                   matrix[matrix_size - row - 1][col]
+                                   ])
+            max_matrix_num = max(max_matrix_num, current_max_num)
+
+    print(max_matrix_num)
+
+
+main()
+
