c569dbf339625f629365fb51a6f841101d72b4be
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-04 14:04:19 +0200

    Created 3_computer_science_lesson.py

---
+++ 8_set/8_7_set_methods_part_3/3_computer_science_lesson.py
@@ -0,0 +1,8 @@
+student_1, student_2, student_3 = set(int(i) for i in input().split()), \
+    set(int(i) for i in input().split()), \
+    set(int(i) for i in input().split())
+
+student_1_2 = student_1.intersection(student_2)
+student_1_2_dif_3_sorted = sorted(student_1_2.difference(student_3), reverse=True)
+
+print(*student_1_2_dif_3_sorted)
