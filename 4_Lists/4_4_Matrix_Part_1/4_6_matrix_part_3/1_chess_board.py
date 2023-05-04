a584c9c43c6a33a738329a2072bc69ea2a994316
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-04 12:15:27 +0200

    Created 1_chess_board.py

---
+++ 4_Lists/4_4_Matrix_Part_1/4_6_matrix_part_3/1_chess_board.py
@@ -0,0 +1,40 @@
+EVEN_SYM = '.'
+ODD_SYM = '*'
+
+EVEN_CHAR_CODE = 0
+ODD_CHAR_CODE = 1
+
+
+def print_visual_matrix(matrix, row_count, col_count):
+    for row in range(row_count):
+        for col in range(col_count):
+            if matrix[row][col] == ODD_CHAR_CODE:
+                print(ODD_SYM, end=' ')
+            else:
+                print(EVEN_SYM, end=' ')
+        print()
+
+
+def set_matrix_staggered_old(matrix, row_count, col_count):
+    for row in range(row_count):
+        for col in range(1, col_count, 2):
+            matrix[row][col] = ODD_CHAR_CODE
+
+
+def set_matrix_staggered(matrix, row_count, col_count):
+
+    for row in range(row_count):
+        for col in range((row + 1) % 2, col_count, 2):
+            matrix[row][col] = ODD_CHAR_CODE
+
+
+def main():
+    row_count, col_count = [int(i) for i in input().split()]
+    matrix = [[EVEN_CHAR_CODE] * col_count for _ in range(row_count)]
+
+    set_matrix_staggered(matrix, row_count, col_count)
+
+    print_visual_matrix(matrix, row_count, col_count)
+
+
+main()
