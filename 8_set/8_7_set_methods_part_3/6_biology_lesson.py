ec9fe306ebda686dea0f3b283548908516a76951
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-05 10:17:20 +0200

    Moved sort to business logic

--- 8_set/8_7_set_methods_part_3/6_biology_lesson.py
+++ 8_set/8_7_set_methods_part_3/6_biology_lesson.py
@@ -4,6 +4,6 @@ STUDENT_AMOUNT = 3
 student_1, student_2, student_3 = [set([int(i) for i in input().split()]) for n in range(STUDENT_AMOUNT)]
 
 all_student_marks = student_1.union(student_2, student_3)
-not_found_marks = ALL_POSSIBLE_MARKS.difference(all_student_marks)
+not_found_marks_sorted = sorted(ALL_POSSIBLE_MARKS.difference(all_student_marks))
 
-print(*sorted(not_found_marks))
+print(not_found_marks_sorted)
