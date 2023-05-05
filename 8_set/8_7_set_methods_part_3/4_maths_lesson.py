056f6d85a9a39108ff21c47e1d40f91efd7bcb36
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-05 10:08:31 +0200

    Added set chain approach

--- 8_set/8_7_set_methods_part_3/4_maths_lesson.py
+++ 8_set/8_7_set_methods_part_3/4_maths_lesson.py
@@ -2,8 +2,8 @@ STUDENT_AMOUNT = 3
 
 student_1, student_2, student_3 = [set([int(i) for i in input().split()]) for n in range(STUDENT_AMOUNT)]
 
-all_student_marks = student_1.union(student_2, student_3)
 found_in_all_students = student_1.intersection(student_2, student_3)
-found_in_no_more_than_2_students = all_student_marks - found_in_all_students
+found_in_no_more_than_2_students_sorted = sorted(student_1.union(student_2, student_3).
+                                                           difference(found_in_all_students))
 
-print(*sorted(found_in_no_more_than_2_students))
+print(*found_in_no_more_than_2_students_sorted)
