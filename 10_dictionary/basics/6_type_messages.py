7680536333f785b5684b2837f14a88d5b5fb7038
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-11 10:47:20 +0200

    Moved phone_digit_chars dictionary to constant

--- 10_dictionary/basics/6_type_messages.py
+++ 10_dictionary/basics/6_type_messages.py
@@ -1,13 +1,23 @@
-input_text = input().upper()
+PHONE_DIGIT_CHARS = {
+    "0": " ",
+    "1": ".,?!:",
+    "2": "ABC",
+    "3": "DEF",
+    "4": "GHI",
+    "5": "JKL",
+    "6": "MNO",
+    "7": "PQRS",
+    "8": "TUV",
+    "9": "WXYZ",
+}
 
-phone_digits = [str(num) for num in range(10)]
-digit_chars = [" ", ".,?!:", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]
-phone_digit_chars = dict(zip(phone_digits, digit_chars))
+
+input_text = input().upper()
 
 output_digits = []
 
 for c in input_text:
-    for key, value in phone_digit_chars.items():
+    for key, value in PHONE_DIGIT_CARS.items():
         if c in value:
             output_digits.append(key * (value.index(c) + 1))
             break
