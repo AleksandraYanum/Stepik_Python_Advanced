74a03279e4b485fab1bc0c18efcdf8a6f3ca24c7
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-11 10:31:43 +0200

    Created 6_type_messages.py

---
+++ 10_dictionary/basics/6_type_messages.py
@@ -0,0 +1,14 @@
+input_text = input().upper()
+
+phone_digits = [str(num) for num in range(10)]
+digit_chars = [" ", ".,?!:", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]
+phone_digit_chars = dict(zip(phone_digits, digit_chars))
+
+output_digits = []
+
+for c in input_text:
+    for key in phone_digit_chars.keys():
+        if c in phone_digit_chars[key]:
+            output_digits.append(key * (phone_digit_chars[key].index(c) + 1))
+
+print(*output_digits, sep='')
