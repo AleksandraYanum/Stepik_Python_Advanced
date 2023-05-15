f143a93308b7f4add1550d9f6bfbbaf722bb869a
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-05-15 13:20:33 +0200

    10_3_6 The rarest word (#42)
    * Created 6_the_rarest_word.py
    
    * Created initial func find_the_rarest_word_in_text()
    
    * Added logic to func find_the_rarest_word_in_text()
    
    * Removed extra print from main()
    
    * Renamed var text_list -> word_list

---
+++ 10_dictionary/10_3_dictionary_methods/6_the_rarest_word.py
@@ -0,0 +1,23 @@
+MARKS = ' .,-_:!@#$%^&?'
+
+def find_the_rarest_word_in_text(text):
+    word_list = text.lower().split()
+    word_frequencies = {}
+    for word in word_list:
+        word = word.strip(MARKS)
+        word_frequencies[word] = word_frequencies.get(word, 0) + 1
+    
+    min_frequency = min(word_frequencies.values())
+    the_rarest_words = [word for word, amount in word_frequencies.items() if amount == min_frequency]
+    the_rarest_word = min(the_rarest_words)
+ 
+    return the_rarest_word
+
+
+def main():
+    input_text = input()
+    the_rarest_word = find_the_rarest_word_in_text(input_text)
+    print(the_rarest_word)
+
+
+main()
