6923a79db18d81947d9f7c464f3760fef641d206
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-15 12:05:56 +0200

    Created initial func find_the_rarest_word_in_text()

--- 10_dictionary/10_3_dictionary_methods/6_the_rarest_word.py
+++ 10_dictionary/10_3_dictionary_methods/6_the_rarest_word.py
@@ -1,1 +1,12 @@
-text = input()
+def find_the_rarest_word_in_text(input_text):
+    word = ''
+    return word
+
+
+def main():
+    input_text = input()
+    the_rarest_word = find_the_rarest_word_in_text(input_text)
+    print(the_rarest_word)
+
+
+main()
