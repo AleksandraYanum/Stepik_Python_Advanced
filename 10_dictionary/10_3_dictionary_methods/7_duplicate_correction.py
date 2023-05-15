1d11eb6ca2234d0fb97f0e88390f6560e3bdffee
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-15 17:09:51 +0200

    Added logic to fix_duplicates

--- 10_dictionary/10_3_dictionary_methods/7_duplicate_correction.py
+++ 10_dictionary/10_3_dictionary_methods/7_duplicate_correction.py
@@ -1,5 +1,17 @@
 def fix_duplicates(text):
-    return text
+    word_list = text.split()
+    deduplicated_word_list = []
+    word_duplicate_amount = {}
+    for word in word_list:
+        if word not in  deduplicated_word_list:
+            deduplicated_word_list.append(word)
+        else:
+            word_duplicate_amount[word] = word_duplicate_amount.get(word, 0) + 1
+            deduplicated_word_list.append(word + '_' + str(word_duplicate_amount[word]))
+
+    deduplicated_text = ' '.join(deduplicated_word_list)
+
+    return deduplicated_text
 
 
 def main():
@@ -7,3 +19,6 @@ def main():
     deduplicated_text = fix_duplicates(input_text)
 
     print(deduplicated_text)
+
+
+main()
