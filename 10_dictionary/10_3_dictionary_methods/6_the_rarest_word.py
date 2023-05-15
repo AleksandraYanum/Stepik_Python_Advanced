7a0ed5f1d93406426fd8793c2c86765dfb8155dd
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-15 13:17:37 +0200

    Renamed var text_list -> word_list

--- 10_dictionary/10_3_dictionary_methods/6_the_rarest_word.py
+++ 10_dictionary/10_3_dictionary_methods/6_the_rarest_word.py
@@ -1,9 +1,9 @@
 MARKS = ' .,-_:!@#$%^&?'
 
 def find_the_rarest_word_in_text(text):
-    text_list = text.lower().split()
+    word_list = text.lower().split()
     word_frequencies = {}
-    for word in text_list:
+    for word in word_list:
         word = word.strip(MARKS)
         word_frequencies[word] = word_frequencies.get(word, 0) + 1
     
