7ba61043fb1e32b328994f175389157050a30656
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-05-17 11:35:10 +0200

    10_3_7 Duplicate correction: pattern usage (#47)
    * Added task comment
    
    * Added print pattern to fix_duplicates
    
    * Extracted pattern calc to var
    
    * Added default pattern const
    
    * Refactoring
    
    * Added custom duplicate pattern sample

--- 10_dictionary/10_3_dictionary_methods/7_duplicate_correction.py
+++ 10_dictionary/10_3_dictionary_methods/7_duplicate_correction.py
@@ -1,4 +1,9 @@
-def fix_duplicates(text):
+PATTERN_WORD_CHAR = 'w'
+PATTERN_NUM_CHAR = 'n'
+DEFAULT_PATTERN = 'w_n'
+
+
+def fix_duplicates(text, pattern=DEFAULT_PATTERN):
     word_list = text.split()
     deduplicated_word_list = []
     word_duplicate_amount = {}
@@ -7,15 +12,23 @@ def fix_duplicates(text):
             deduplicated_word_list.append(word)
         else:
             word_duplicate_amount[word] = word_duplicate_amount.get(word, 0) + 1
-            deduplicated_word_list.append(word + '_' + str(word_duplicate_amount[word]))
+            deduplicated_word = pattern.replace(PATTERN_WORD_CHAR, word).\
+                                        replace(PATTERN_NUM_CHAR, str(word_duplicate_amount[word]))
+            deduplicated_word_list.append(deduplicated_word)
 
     deduplicated_text = ' '.join(deduplicated_word_list)
 
     return deduplicated_text
 
 
+###################################################################################
+
+DUPLICATE_PATTERN = 'w(n)'
+
+
 def main():
     input_text = input()
+    # deduplicated_text = fix_duplicates(input_text, DUPLICATE_PATTERN)
     deduplicated_text = fix_duplicates(input_text)
 
     print(deduplicated_text)
