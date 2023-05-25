78370d3dce0a0463ede70e87f698fe513a71cef2
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-25 11:46:46 +0200

    Removed unused variable

--- 10_dictionary/10_4_dictionary_tasks/1_programmer_dictionary.py
+++ 10_dictionary/10_4_dictionary_tasks/1_programmer_dictionary.py
@@ -1,10 +1,10 @@
 def main():
-    programmer_data =  [(input().split(':')) for i in range(int(input()))]
-    programmer_dict = {i[0].lower(): i[1].strip() for i in programmer_data}
+    programmer_slang_dict = {i[0].lower(): i[1].strip() for i in \
+        [input().split(':') for j in range(int(input()))]}
 
     # also it's possible to split print and logic: append values to list 
     for word in [input().lower() for i in range(int(input()))]:
-        print(programmer_dict.get(word, "Не найдено"))
+        print(programmer_slang_dict.get(word, "Не найдено"))
 
 
 main()
