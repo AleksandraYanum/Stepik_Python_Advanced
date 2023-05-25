fc11b3793cd967699ae6f1f6b0c6c06764792692
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-25 10:55:51 +0200

    Added full logic

--- 10_dictionary/10_4_dictionary_tasks/1_programmer_dictionary.py
+++ 10_dictionary/10_4_dictionary_tasks/1_programmer_dictionary.py
@@ -1,5 +1,10 @@
 def main():
-    pass
+    programmer_data =  [(input().split(':')) for i in range(int(input()))]
+    programmer_dict = {i[0].lower(): i[1].strip() for i in programmer_data}
+
+    # also it's possible to split print and logic: append values to list 
+    for word in [input().lower() for i in range(int(input()))]:
+        print(programmer_dict.get(word, "Не найдено"))
 
 
 main()
