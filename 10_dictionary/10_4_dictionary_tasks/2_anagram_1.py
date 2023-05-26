30c314c878afb51df5cd4c2ffd1b64a391e8a9c5
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-26 18:44:02 +0200

    Optimized to prevent extra calc on negative number

--- 10_dictionary/10_4_dictionary_tasks/2_anagram_1.py
+++ 10_dictionary/10_4_dictionary_tasks/2_anagram_1.py
@@ -3,14 +3,18 @@ NEGATIVE_OUTPUT = 'NO'
 
 
 def main():
+    is_anagram = False
     word_letter_amount = {}
 
     for letter in input().lower():
         word_letter_amount[letter] = word_letter_amount.get(letter, 0) + 1
     for letter in input().lower():
         word_letter_amount[letter] = word_letter_amount.get(letter, 0) - 1
+        if word_letter_amount[letter] < 0:
+            break
+    else:
+        is_anagram = set(word_letter_amount.values()) == {0}
 
-    is_anagram = set(word_letter_amount.values()) == {0}
     output = POSITIVE_OUTPUT if is_anagram else NEGATIVE_OUTPUT
   
     print(output)
