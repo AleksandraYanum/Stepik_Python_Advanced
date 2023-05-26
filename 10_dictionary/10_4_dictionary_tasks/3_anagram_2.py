ed53864e0f3457354697cc4e504b992ad71929a4
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-26 20:39:34 +0200

    Used the prev task and changed input logic

--- 10_dictionary/10_4_dictionary_tasks/3_anagram_2.py
+++ 10_dictionary/10_4_dictionary_tasks/3_anagram_2.py
@@ -1,6 +1,23 @@
+POSITIVE_OUTPUT = 'YES'
+NEGATIVE_OUTPUT = 'NO'
+
+
 def main():
-    pass
-    return 0
+    is_anagram = False
+    word_letter_amount = {}
+
+    for letter in ''.join(c.lower() for c in input() if c.isalpha()):
+        word_letter_amount[letter] = word_letter_amount.get(letter, 0) + 1
+    for letter in ''.join(c.lower() for c in input() if c.isalpha()):
+        word_letter_amount[letter] = word_letter_amount.get(letter, 0) - 1
+        if word_letter_amount[letter] < 0:
+            break
+    else:
+        is_anagram = set(word_letter_amount.values()) == {0}
+
+    output = POSITIVE_OUTPUT if is_anagram else NEGATIVE_OUTPUT
+  
+    print(output)
 
 
 main()
