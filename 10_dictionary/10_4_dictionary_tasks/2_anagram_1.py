5cc1bf12a6b19db59d5c985dba6a5b7057735222
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-26 17:39:11 +0200

    Added ternary operator

--- 10_dictionary/10_4_dictionary_tasks/2_anagram_1.py
+++ 10_dictionary/10_4_dictionary_tasks/2_anagram_1.py
@@ -10,14 +10,9 @@ def main():
     for letter in input().lower():
         word_letter_amount[letter] = word_letter_amount.get(letter, 0) - 1
 
-
     is_anagram = set(word_letter_amount.values()) == {0}
-
-    if is_anagram:
-        output = POSITIVE_OUTPUT
-    else:
-        output = NEGATIVE_OUTPUT
-
+    output = POSITIVE_OUTPUT if is_anagram else NEGATIVE_OUTPUT
+  
     print(output)
 
 
