0c6f05cac0790a782e082686f42440b901d66f74
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-05-29 13:51:37 +0200

    10_4_2  Anagram 1 (#67)
    * Created 2_anagram_1.py
    
    * Added full logic without dict
    
    * Changed logic to use dict
    
    * Added ternary operator
    
    * Optimized to prevent extra calc on negative number
    
    * Oprimization: removed unnecessary dict calc
    
    * Refactoring: if -> elif
    
    * Removed unused if branch
    
    * Removed unnecessary keys ref
    
    * Replaced dict get with key access

---
+++ 10_dictionary/10_4_dictionary_tasks/2_anagram_1.py
@@ -0,0 +1,26 @@
+POSITIVE_OUTPUT = 'YES'
+NEGATIVE_OUTPUT = 'NO'
+
+
+def main():
+    is_anagram = False
+    word_letter_amount = {}
+
+    for letter in input().lower():
+        word_letter_amount[letter] = word_letter_amount.get(letter, 0) + 1
+    for letter in input().lower():
+        if letter not in word_letter_amount:
+            break
+        else:
+            word_letter_amount[letter] = word_letter_amount[letter] - 1
+            if word_letter_amount[letter] == 0:
+                del word_letter_amount[letter]
+    else:
+        is_anagram = word_letter_amount == {}
+
+    output = POSITIVE_OUTPUT if is_anagram else NEGATIVE_OUTPUT
+  
+    print(output)
+
+
+main()
