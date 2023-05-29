46ef6c5f1900a6a6ab6e70844430e6de86421f0f
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-29 13:33:53 +0200

    Optimized logic from prev task

--- 10_dictionary/10_4_dictionary_tasks/3_anagram_2.py
+++ 10_dictionary/10_4_dictionary_tasks/3_anagram_2.py
@@ -9,11 +9,17 @@ def main():
     for letter in ''.join(c.lower() for c in input() if c.isalpha()):
         word_letter_amount[letter] = word_letter_amount.get(letter, 0) + 1
     for letter in ''.join(c.lower() for c in input() if c.isalpha()):
-        word_letter_amount[letter] = word_letter_amount.get(letter, 0) - 1
-        if word_letter_amount[letter] < 0:
+        if letter not in word_letter_amount.keys():
             break
+        else:
+            word_letter_amount[letter] = word_letter_amount.get(letter) - 1
+            if word_letter_amount[letter] < 0:
+                break
+            elif word_letter_amount[letter] == 0:
+                del word_letter_amount[letter]
     else:
-        is_anagram = set(word_letter_amount.values()) == {0}
+        is_anagram = word_letter_amount == {}
+
 
     output = POSITIVE_OUTPUT if is_anagram else NEGATIVE_OUTPUT
   
