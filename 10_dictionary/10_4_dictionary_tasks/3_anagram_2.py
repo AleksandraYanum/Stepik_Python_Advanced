eff2d2cad945993ec06990d82263cd3004a07e27
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-29 14:38:21 +0200

    Extracted is_anagram func

--- 10_dictionary/10_4_dictionary_tasks/3_anagram_2.py
+++ 10_dictionary/10_4_dictionary_tasks/3_anagram_2.py
@@ -2,13 +2,13 @@ POSITIVE_OUTPUT = 'YES'
 NEGATIVE_OUTPUT = 'NO'
 
 
-def main():
-    is_anagram = False
+def is_anagram(string_1, string_2):
+    result = False
     word_letter_amount = {}
 
-    for letter in ''.join(c.lower() for c in input() if c.isalpha()):
+    for letter in string_1:
         word_letter_amount[letter] = word_letter_amount.get(letter, 0) + 1
-    for letter in ''.join(c.lower() for c in input() if c.isalpha()):
+    for letter in string_2:
         if letter not in word_letter_amount:
             break
         else:
@@ -16,12 +16,23 @@ def main():
             if word_letter_amount[letter] == 0:
                 del word_letter_amount[letter]
     else:
-        is_anagram = word_letter_amount == {}
+        result = word_letter_amount == {}
+
+    return result
 
 
-    output = POSITIVE_OUTPUT if is_anagram else NEGATIVE_OUTPUT
+def main():
+    string_1, string_2 = ''.join(c.lower() for c in input() if c.isalpha()), \
+                         ''.join(c.lower() for c in input() if c.isalpha())
+
+    output = POSITIVE_OUTPUT if is_anagram(string_1, string_2) else NEGATIVE_OUTPUT
   
     print(output)
+
+    
+
+
+
 
 
 main()
