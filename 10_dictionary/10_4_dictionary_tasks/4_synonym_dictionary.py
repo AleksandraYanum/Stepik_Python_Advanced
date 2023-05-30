2886aa5a1f677999dc82258b1bc41d1c12ad646c
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-31 00:00:02 +0200

    Returned prev logic with double dict

--- 10_dictionary/10_4_dictionary_tasks/4_synonym_dictionary.py
+++ 10_dictionary/10_4_dictionary_tasks/4_synonym_dictionary.py
@@ -1,26 +1,43 @@
-def find_synonym(synonyms, input_word):
-    input_word_synonym = synonyms.get(input_word)
-
-    if input_word_synonym == None:
-        key_list = list(synonyms.keys())
-        value_list = list(synonyms.values())
-        input_word_synonym = key_list[value_list.index(input_word)]
-
-    return input_word_synonym
-
-
 def main():
 
     synonyms = {}
     for _ in range(int(input())):
         word, synonym = input().split()
         synonyms[word] = synonym
+        synonyms[synonym] = word
 
     input_word = input()
 
-    input_word_synonym = find_synonym(synonyms, input_word)
-
+    input_word_synonym = synonyms[input_word]
     print(input_word_synonym)
 
 
 main()
+
+
+# def find_synonym(synonyms, input_word):
+#     input_word_synonym = synonyms.get(input_word)
+
+#     if input_word_synonym == None:
+#         key_list = list(synonyms.keys())
+#         value_list = list(synonyms.values())
+#         input_word_synonym = key_list[value_list.index(input_word)]
+
+#     return input_word_synonym
+
+
+# def main():
+
+#     synonyms = {}
+#     for _ in range(int(input())):
+#         word, synonym = input().split()
+#         synonyms[word] = synonym
+
+#     input_word = input()
+
+#     input_word_synonym = find_synonym(synonyms, input_word)
+
+#     print(input_word_synonym)
+
+
+# main()
