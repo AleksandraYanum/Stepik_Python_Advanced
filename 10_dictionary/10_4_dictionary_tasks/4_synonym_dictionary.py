2269668498cf7f9a656c8fe2df12242ec75497f7
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-30 11:51:25 +0200

    Added full logic

--- 10_dictionary/10_4_dictionary_tasks/4_synonym_dictionary.py
+++ 10_dictionary/10_4_dictionary_tasks/4_synonym_dictionary.py
@@ -1,6 +1,25 @@
+def find_synonym(synonyms, input_word):
+    input_word_synonym = ''
+    
+    for word, synonym in synonyms.items():
+        if input_word == word:
+            input_word_synonym = synonym
+            break
+        elif input_word == synonym:
+            input_word_synonym = word
+            break
+    
+    return input_word_synonym
+
+
 def main():
-    pass
-    return 0
+    synonyms = dict([[word for word in input().split()] for i in range(int(input()))])
+    input_word = input()
+
+    synonym = find_synonym(synonyms, input_word)
+    
+    print(synonym)
+
 
 
 main()
