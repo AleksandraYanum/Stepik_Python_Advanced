f8cd70eb23a5be756390887684ec80b446a83a13
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-30 12:19:17 +0200

    Changed logic to store dict x2

--- 10_dictionary/10_4_dictionary_tasks/4_synonym_dictionary.py
+++ 10_dictionary/10_4_dictionary_tasks/4_synonym_dictionary.py
@@ -1,25 +1,15 @@
-def find_synonym(synonyms, input_word):
-    input_word_synonym = ''
-    
-    for word, synonym in synonyms.items():
-        if input_word == word:
-            input_word_synonym = synonym
-            break
-        elif input_word == synonym:
-            input_word_synonym = word
-            break
-    
-    return input_word_synonym
-
-
 def main():
-    synonyms = dict([[word for word in input().split()] for i in range(int(input()))])
+
+    synonyms = {}
+    for _ in range(int(input())):
+        word, synonym = input().split()
+        synonyms[word] = synonym
+        synonyms[synonym] = word
+
     input_word = input()
 
-    synonym = find_synonym(synonyms, input_word)
-    
-    print(synonym)
-
+    input_word_synonym = synonyms[input_word]
+    print(input_word_synonym)
 
 
 main()
