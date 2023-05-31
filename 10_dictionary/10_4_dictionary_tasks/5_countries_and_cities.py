7f327f9cf8e50a2c95b52f3366e16b15a2ff2ba3
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-31 10:14:34 +0200

    Added full logic with city-country dict

--- 10_dictionary/10_4_dictionary_tasks/5_countries_and_cities.py
+++ 10_dictionary/10_4_dictionary_tasks/5_countries_and_cities.py
@@ -1,5 +1,18 @@
 def main():
-    return 0
+    input_lists = [input().split() for _ in range(int(input()))]
+
+    city_country_dict = {}
+    for input_list in input_lists:
+        for city in input_list[1:]:
+            city_country_dict[city] = input_list[0]
+
+    country_output_list = []
+    for _ in range(int(input())):
+        input_city = input()
+        country_output_list.append(city_country_dict[input_city])
+        
+    for country in country_output_list:
+        print(country)
 
 
 main()
