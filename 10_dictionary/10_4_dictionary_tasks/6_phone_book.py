32c12cf2b88e9c83588c685c733278d812169d25
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-31 16:31:05 +0200

    Added the rest logic

--- 10_dictionary/10_4_dictionary_tasks/6_phone_book.py
+++ 10_dictionary/10_4_dictionary_tasks/6_phone_book.py
@@ -1,13 +1,20 @@
+NAME_NOT_FOUND = ['абонент не найден']
+
+
 def main():
     phone_book = {}
 
     for _ in range(int(input())):
-        phone_num, name = input().split()
-        phone_book.setdefault(name.lower(), []).append(phone_num)
+        found_phone_num, name = input().split()
+        phone_book.setdefault(name.lower(), []).append(found_phone_num)
 
-    
-    print(phone_book)
+    found_phone_num_list = []
+    for _ in range(int(input())):
+        input_name = input().lower()
+        found_phone_num_list.append(phone_book.get(input_name, NAME_NOT_FOUND))
 
+    for found_phone_num in found_phone_num_list:
+        print(*found_phone_num)
 
 
 main()
