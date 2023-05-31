59afbabdc917f864ac43a8b30df8442e42dfa007
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-05-31 16:17:58 +0200

    Removed unnecessary var i

--- 10_dictionary/10_4_dictionary_tasks/6_phone_book.py
+++ 10_dictionary/10_4_dictionary_tasks/6_phone_book.py
@@ -1,10 +1,11 @@
 def main():
     phone_book = {}
 
-    for i in range(int(input())):
+    for _ in range(int(input())):
         phone_num, name = input().split()
         phone_book.setdefault(name.lower(), []).append(phone_num)
 
+    
     print(phone_book)
 
 
