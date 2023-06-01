a489e256bfc4f0895eb272d766c66ac36d6b17ca
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-06-01 15:04:27 +0200

    10_4_6 Phone book (#71)
    * Created 6_phone_book.py
    
    * Created phone_book dict with setdafault method
    
    * Converted key str to its lowercase equivalent
    
    * Removed unnecessary var i
    
    * Added the rest logic
    
    * Changed default value list -> set to handle duplicates
    
    * Used proper type casting for NAME_NOT_FOUND

---
+++ 10_dictionary/10_4_dictionary_tasks/6_phone_book.py
@@ -0,0 +1,21 @@
+NAME_NOT_FOUND = 'абонент не найден'
+
+
+def main():
+    phone_book = {}
+
+    for _ in range(int(input())):
+        found_phone_num, name = input().split()
+        phone_set = set()
+        phone_book.setdefault(name.lower(), phone_set).add(found_phone_num)
+
+    found_phone_num_list = []
+    for _ in range(int(input())):
+        input_name = input().lower()
+        found_phone_num_list.append(phone_book.get(input_name, {NAME_NOT_FOUND}))
+
+    for found_phone_num in found_phone_num_list:
+        print(*found_phone_num)
+
+
+main()
