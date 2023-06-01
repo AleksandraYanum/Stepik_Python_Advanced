0c257ca92645684a56be9c76f495729f28bca800
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-01 11:51:47 +0200

    Changed default value list -> set to handle duplicates

--- 10_dictionary/10_4_dictionary_tasks/5_countries_and_cities.py
+++ 10_dictionary/10_4_dictionary_tasks/5_countries_and_cities.py
@@ -2,9 +2,12 @@ def main():
     country_table = [input().split() for _ in range(int(input()))]
 
     city_country_dict = {}
+    
+
     for country_row in country_table:
+        country_set = set()
         for city in country_row[1:]:
-            city_country_dict.setdefault(city, []).append(country_row[0])
+            city_country_dict.setdefault(city, country_set).add(country_row[0])
             
     country_output_list = []
     for _ in range(int(input())):
