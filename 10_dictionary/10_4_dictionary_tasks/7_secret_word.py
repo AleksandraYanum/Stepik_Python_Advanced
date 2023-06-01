639282dda0e1d8564f3bfbb0d460d8b99771ce96
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-01 12:22:07 +0200

    Added logic for input and creating dict

--- 10_dictionary/10_4_dictionary_tasks/7_secret_word.py
+++ 10_dictionary/10_4_dictionary_tasks/7_secret_word.py
@@ -1,5 +1,12 @@
 def main():
-    pass
+    secret_word = input()
+
+    letter_amount = {}
+    for _ in range(int(input())):
+        letter, amount = input().split(':')
+        letter_amount[amount] = letter
+
+    print(letter_amount)
 
 
 main()
