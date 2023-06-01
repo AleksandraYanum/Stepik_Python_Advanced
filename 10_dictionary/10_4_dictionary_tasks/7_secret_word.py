ba19372456eb0f0fa5da2b92ba156221a9f30a7a
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-01 14:48:31 +0200

    Added logic to handle inputs

--- 10_dictionary/10_4_dictionary_tasks/7_secret_word.py
+++ 10_dictionary/10_4_dictionary_tasks/7_secret_word.py
@@ -1,12 +1,21 @@
 def main():
     secret_word = input()
+    secret_word_set = set(secret_word)
 
-    letter_amount = {}
+    letters_amount = {}
     for _ in range(int(input())):
         letter, amount = input().split(':')
-        letter_amount[amount] = letter
+        letters_amount[amount] = letter
+    
+    print(secret_word_set)
+    print(letters_amount)    
 
-    print(letter_amount)
+    secret_chars_amount = {}
+    for char in secret_word_set:
+        char_amount = secret_word.count(char)
+        secret_chars_amount[char] = char_amount
+
+    print(secret_chars_amount)
 
 
 main()
