199a633a05374768abbe8711bfd71f558e7748dd
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-06-13 13:41:57 +0200

    10_4_7 Secret word (#72)
    * Created 7_secret_word.py
    
    * Added logic for input and creating dict
    
    * Added logic to handle inputs
    
    * Added logic to decode chars
    
    * Renamed vars to keep consistency
    
    * Optimized char_amount_dict filling
    
    * Optimized version to reduce scan amount
    
    * Optimized dict search - created the third combined dict
    
    * Refactoring: deleted unnecessary commits
    
    * Edited comment

---
+++ 10_dictionary/10_4_dictionary_tasks/7_secret_word.py
@@ -0,0 +1,31 @@
+def main():
+    secret_word = input()
+
+    # {AMOUNT: LETTER}
+    letter_amount_dict = {}
+    for _ in range(int(input())):
+        letter, letter_amount = input().split(':')
+        letter_amount_dict[int(letter_amount)] = letter
+    
+    # {SECRET_CHAR: [AMOUNT]}
+    char_amount_dict = {}
+    for char in secret_word:
+        result_list = char_amount_dict.get(char, [0])
+        result_list[0] += 1
+        char_amount_dict[char] = result_list
+  
+    # Editing dict {SECRET_CHAR: [AMOUNT, LETTER]}
+    for char in char_amount_dict:
+        need_amount = char_amount_dict[char][0]
+        need_letter = letter_amount_dict[need_amount]
+        char_amount_dict[char].append(need_letter)
+
+    decoded_word = ''
+    for char in secret_word:
+        decoded_letter = char_amount_dict[char][1]
+        decoded_word += decoded_letter
+
+    print(decoded_word)
+
+
+main()
