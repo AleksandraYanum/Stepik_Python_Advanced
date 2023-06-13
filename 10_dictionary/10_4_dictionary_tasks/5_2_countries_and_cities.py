d8b9e90a615b8ee788fc1102e6ef82d1afb91175
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-13 16:52:01 +0200

    Added preliminary logic for DefaultDic

--- 10_dictionary/10_4_dictionary_tasks/5_2_countries_and_cities.py
+++ 10_dictionary/10_4_dictionary_tasks/5_2_countries_and_cities.py
@@ -1,5 +1,14 @@
+from collections import defaultdict
+
+
+def def_value():
+    empty_set = set()
+    return empty_set
+
+
 def main():
-    return 0
+    # country_table = [input().split() for _ in range(int(input()))]
+    city_country_dict = defaultdict(def_value)
 
 
 main()
