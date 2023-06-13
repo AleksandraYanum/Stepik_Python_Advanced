0a08d93d127fda81ff8dffce6d936685d3e13b95
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-13 17:06:10 +0200

    Added the remaining logic from the task

--- 10_dictionary/10_4_dictionary_tasks/5_2_countries_and_cities.py
+++ 10_dictionary/10_4_dictionary_tasks/5_2_countries_and_cities.py
@@ -7,8 +7,21 @@ def def_value():
 
 
 def main():
-    # country_table = [input().split() for _ in range(int(input()))]
+    country_table = [input().split() for _ in range(int(input()))]
     city_country_dict = defaultdict(def_value)
+
+    for country_row in country_table:
+        for city in country_row[1:]:
+            city_country_dict[city].add(country_row[0])
+
+    country_output_list = []
+
+    for _ in range(int(input())):
+        input_city = input()
+        country_output_list.append(city_country_dict[input_city])
+
+    for country in country_output_list:
+        print(*country)
 
 
 main()
