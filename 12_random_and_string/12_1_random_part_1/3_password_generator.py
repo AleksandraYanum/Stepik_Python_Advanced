2caa6b7b94cd53f14b8a5bcc172446ffbb176661
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-22 15:51:07 +0200

    Added previous code

--- 12_random_and_string/12_1_random_part_1/3_password_generator.py
+++ 12_random_and_string/12_1_random_part_1/3_password_generator.py
@@ -1,5 +1,28 @@
+from random import randint
+
+FIRST_UPPERCASE_LETTER_CODE = ord('A')
+LAST_UPPERCASE_LETTER_CODE = ord('Z')
+
+FIRST_LOWERCASE_LETTER_CODE = ord('a')
+LAST_LOWERCASE_LETTER_CODE = ord('z')
+
+UPPERCASE_CODE = 1
+LOWERCASE_CODE = 2
+
 def main():
-    pass
+    
+    psw_length = int(input())    # длина пароля
+    psw = ''
+
+    for _ in range(psw_length):
+        letter_case_choise = randint(UPPERCASE_CODE, LOWERCASE_CODE)
+        if letter_case_choise == UPPERCASE_CODE:
+            psw_letter = chr(randint(FIRST_UPPERCASE_LETTER_CODE, LAST_UPPERCASE_LETTER_CODE))
+        else:
+            psw_letter = chr(randint(FIRST_LOWERCASE_LETTER_CODE, LAST_LOWERCASE_LETTER_CODE))
+        psw += psw_letter
+
+    print(psw)
 
 
 main()
