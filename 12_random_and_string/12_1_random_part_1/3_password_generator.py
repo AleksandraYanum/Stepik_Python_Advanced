d92ef63561f120bc2bdb82b10a454310a73b1f7e
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-26 20:08:34 +0200

    Changed input order

--- 12_random_and_string/12_1_random_part_1/3_password_generator.py
+++ 12_random_and_string/12_1_random_part_1/3_password_generator.py
@@ -7,13 +7,13 @@ CHAR_RANGES = [{FIRST_CHAR_KEY: 'A', LAST_CHAR_KEY: 'Z'},
               {FIRST_CHAR_KEY: 'a', LAST_CHAR_KEY: 'z'}]
 
 def main():
+    psw_length = int(input())    # длина пароля
+    psw = ''
+
     possible_psw_chars = []
     for char_range in CHAR_RANGES:
         chars = [chr(c) for c in range(ord(char_range[FIRST_CHAR_KEY]), ord(char_range[LAST_CHAR_KEY]) + 1)]
         possible_psw_chars.extend(chars)
-
-    psw_length = int(input())    # длина пароля
-    psw = ''
 
     for _ in range(psw_length):
         psw_letter = choice(possible_psw_chars)
