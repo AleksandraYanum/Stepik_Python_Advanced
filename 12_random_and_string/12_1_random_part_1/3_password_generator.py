2fd25ab22c9ebe7d105324ce74e9a9036c203f14
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-06-26 20:10:02 +0200

    12_1_3 Password generator (#83)
    * Made an expand seq of pos letters, used choice func
    
    * Introduced const char range
    
    * Implemented const char range
    
    * Changed input order

--- 12_random_and_string/12_1_random_part_1/3_password_generator.py
+++ 12_random_and_string/12_1_random_part_1/3_password_generator.py
@@ -1,25 +1,22 @@
-from random import randint
+from random import choice
 
-FIRST_UPPERCASE_LETTER_CODE = ord('A')
-LAST_UPPERCASE_LETTER_CODE = ord('Z')
+FIRST_CHAR_KEY = 'first'
+LAST_CHAR_KEY = 'last'
 
-FIRST_LOWERCASE_LETTER_CODE = ord('a')
-LAST_LOWERCASE_LETTER_CODE = ord('z')
-
-UPPERCASE_CODE = 1
-LOWERCASE_CODE = 2
+CHAR_RANGES = [{FIRST_CHAR_KEY: 'A', LAST_CHAR_KEY: 'Z'}, 
+              {FIRST_CHAR_KEY: 'a', LAST_CHAR_KEY: 'z'}]
 
 def main():
-    
     psw_length = int(input())    # длина пароля
     psw = ''
 
+    possible_psw_chars = []
+    for char_range in CHAR_RANGES:
+        chars = [chr(c) for c in range(ord(char_range[FIRST_CHAR_KEY]), ord(char_range[LAST_CHAR_KEY]) + 1)]
+        possible_psw_chars.extend(chars)
+
     for _ in range(psw_length):
-        letter_case_choise = randint(UPPERCASE_CODE, LOWERCASE_CODE)
-        if letter_case_choise == UPPERCASE_CODE:
-            psw_letter = chr(randint(FIRST_UPPERCASE_LETTER_CODE, LAST_UPPERCASE_LETTER_CODE))
-        else:
-            psw_letter = chr(randint(FIRST_LOWERCASE_LETTER_CODE, LAST_LOWERCASE_LETTER_CODE))
+        psw_letter = choice(possible_psw_chars)
         psw += psw_letter
 
     print(psw)
