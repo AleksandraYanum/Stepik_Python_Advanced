d6e73b39fc0d5f0b327b8d28e0b90102ded425ac
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-26 15:15:52 +0200

    Implemented const char range

--- 12_random_and_string/12_1_random_part_1/3_password_generator.py
+++ 12_random_and_string/12_1_random_part_1/3_password_generator.py
@@ -6,16 +6,17 @@ LAST_CHAR_KEY = 'last'
 CHAR_RANGES = [{FIRST_CHAR_KEY: 'A', LAST_CHAR_KEY: 'Z'}, 
               {FIRST_CHAR_KEY: 'a', LAST_CHAR_KEY: 'z'}]
 
-POSSIBLE_PSW_LETTERS = [chr(c) for c in range(ord('A'), ord('Z') + 1)] + \
-                       [chr(c) for c in range(ord('a'), ord('z') + 1)]
-
-
 def main():
+    possible_psw_chars = []
+    for char_range in CHAR_RANGES:
+        chars = [chr(c) for c in range(ord(char_range[FIRST_CHAR_KEY]), ord(char_range[LAST_CHAR_KEY]) + 1)]
+        possible_psw_chars.extend(chars)
+
     psw_length = int(input())    # длина пароля
     psw = ''
 
     for _ in range(psw_length):
-        psw_letter = choice(POSSIBLE_PSW_LETTERS)
+        psw_letter = choice(possible_psw_chars)
         psw += psw_letter
 
     print(psw)
