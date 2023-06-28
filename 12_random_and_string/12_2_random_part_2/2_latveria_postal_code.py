cc53308d909ee187fab5c3acfef25ba6ab003ff2
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-28 10:10:13 +0200

    Added full logic

--- 12_random_and_string/12_2_random_part_2/2_latveria_postal_code.py
+++ 12_random_and_string/12_2_random_part_2/2_latveria_postal_code.py
@@ -1,9 +1,34 @@
-def generate_postal_code():
-    pass
+from string import ascii_uppercase
+from random import randint
+from random import choice
+
+
+LIVERIA_POSTAL_CODE_MASK = 'LLn_nLL'
+UPPERCASE_LETTER_CODE = 'L'
+NUM_CODE = 'n'   # from 0 to 99
+FIRST_POS_NUM = 0
+LAST_POS_NUM = 99
+
+def generate_postal_code(postal_code_mask):
+    postal_code = ''
+
+    for c in postal_code_mask:
+        if c == UPPERCASE_LETTER_CODE:
+            postal_code_char = choice(ascii_uppercase)
+        elif c == NUM_CODE:
+            postal_code_char = str(randint(FIRST_POS_NUM, LAST_POS_NUM))
+        else:
+            postal_code_char = c
+        postal_code += postal_code_char
+    
+    return postal_code
+
+    
 
 
 def main():
-    pass
+    liveria_postal_code = generate_postal_code(LIVERIA_POSTAL_CODE_MASK)
+    print(liveria_postal_code)
 
 
 main()
