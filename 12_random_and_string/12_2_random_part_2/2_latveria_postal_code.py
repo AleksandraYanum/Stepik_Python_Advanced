026addc6d42f823ef9a13f6133bf56b4cc2104d4
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-29 15:58:22 +0200

    Added comment about implemented version

--- 12_random_and_string/12_2_random_part_2/2_latveria_postal_code.py
+++ 12_random_and_string/12_2_random_part_2/2_latveria_postal_code.py
@@ -2,6 +2,11 @@ from string import ascii_uppercase
 from random import randint
 from random import choice
 
+# Implemented version formates LATVERIA_POSTAL_CODE_PATTERN from 
+# 'LetterLetterNumber_NumberLetterLetter' to 'lln_nll'. If postal code includes letters 'l', 'n'
+# that should be ignored, it's needed to introduced new chars for encoding which don't occurs 
+# in POSTAL_CODE_PATTERN
+
 
 LATVERIA_POSTAL_CODE_PATTERN = 'LetterLetterNumber_NumberLetterLetter'
 
@@ -10,7 +15,6 @@ UPPERCASE_LETTER_CHAR = 'l'
 
 NUM_WORD = 'number'   # from 0 to 99
 NUM_CHAR = 'n'
-
 
 FIRST_POS_NUM = 0
 LAST_POS_NUM = 99
