09aea243b5e32b9930e0924631736652965f6e10
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-06-29 15:45:43 +0200

    Implemented postal code pattern formatting

--- 12_random_and_string/12_2_random_part_2/2_latveria_postal_code.py
+++ 12_random_and_string/12_2_random_part_2/2_latveria_postal_code.py
@@ -5,8 +5,12 @@ from random import choice
 
 LATVERIA_POSTAL_CODE_PATTERN = 'LetterLetterNumber_NumberLetterLetter'
 
-UPPERCASE_LETTER_CODE = 'letter'    #uppercase letter
-NUM_CODE = 'number'   # from 0 to 99
+UPPERCASE_LETTER_WORD = 'letter'    #uppercase letter
+UPPERCASE_LETTER_CHAR = 'l'
+
+NUM_WORD = 'number'   # from 0 to 99
+NUM_CHAR = 'n'
+
 
 FIRST_POS_NUM = 0
 LAST_POS_NUM = 99
@@ -15,10 +19,16 @@ LAST_POS_NUM = 99
 def generate_postal_code(postal_code_mask):
     postal_code = ''
 
-    for c in postal_code_mask:
-        if c == UPPERCASE_LETTER_CODE:
+    postal_code_mask_formatted = postal_code_mask.lower().\
+        replace(UPPERCASE_LETTER_WORD, UPPERCASE_LETTER_CHAR).\
+        replace(NUM_WORD, NUM_CHAR)
+
+    print(postal_code_mask_formatted)
+
+    for c in postal_code_mask_formatted:
+        if c == UPPERCASE_LETTER_CHAR:
             postal_code_char = choice(ascii_uppercase)
-        elif c == NUM_CODE:
+        elif c == NUM_CHAR:
             postal_code_char = str(randint(FIRST_POS_NUM, LAST_POS_NUM))
         else:
             postal_code_char = c
