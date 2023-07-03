8e5cd2a9c62148e7ce745ecf0606f203dcafc6d0
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-07-03 13:24:19 +0200

    Implemented pattern words processing

--- 12_random_and_string/12_2_random_part_2/2_v2_latveria_postal_code.py
+++ 12_random_and_string/12_2_random_part_2/2_v2_latveria_postal_code.py
@@ -16,6 +16,9 @@ POSTAL_UPPERCASE_LETTER_CHAR = 'l'
 POSTAL_NUM_WORD = 'number'   # from 0 to 99
 POSTAL_NUM_CHAR = 'n'
 
+PATTERN_WORD_CHAR_DIC = {POSTAL_UPPERCASE_LETTER_WORD: POSTAL_UPPERCASE_LETTER_CHAR,
+                         POSTAL_NUM_WORD: POSTAL_NUM_CHAR}
+
 FIRST_POS_NUM = 0
 LAST_POS_NUM = 99
 
@@ -35,30 +38,30 @@ def find_all(source, symb):
 
 # func takes not normalized postal code pattern, brings it into normalized form 
 # (lowercase and single char coded) and calls enerate_postal_code_from_normalized_pattern func
-def generate_postal_code(postal_code_mask):
+def generate_postal_code(postal_code_mask, pattern_word_char_dic):
     
     postal_code_mask_low = postal_code_mask.lower()
 
-    postal_uppercase_letter_idx_list = find_all(postal_code_mask_low, POSTAL_UPPERCASE_LETTER_WORD)
-    postal_number_idx_list = find_all(postal_code_mask_low, POSTAL_NUM_WORD)
+    for pattern_word, pattern_char in pattern_word_char_dic.items():
 
+        pattern_word_idx_list = find_all(postal_code_mask_low, pattern_word)
 
-    first_idx = postal_uppercase_letter_idx_list[0]
-    last_idx = postal_uppercase_letter_idx_list[len(postal_uppercase_letter_idx_list) - 1]
+        first_idx = pattern_word_idx_list[0]
+        last_idx = pattern_word_idx_list[len(pattern_word_idx_list) - 1]
 
-    postal_code_mask_normalized = postal_code_mask_low[:first_idx]
+        postal_code_mask_normalized = postal_code_mask_low[:first_idx]
 
-    for i in range(len(postal_uppercase_letter_idx_list) - 1):
-        current_idx = postal_uppercase_letter_idx_list[i]
-        next_idx = postal_uppercase_letter_idx_list[i + 1]
+        for i in range(len(pattern_word_idx_list) - 1):
+            current_idx = pattern_word_idx_list[i]
+            next_idx = pattern_word_idx_list[i + 1]
 
-        postal_code_mask_normalized += POSTAL_UPPERCASE_LETTER_CHAR
-        postal_code_mask_normalized += postal_code_mask_low[current_idx + len(POSTAL_UPPERCASE_LETTER_WORD) : next_idx] 
+            postal_code_mask_normalized += pattern_char
+            postal_code_mask_normalized += postal_code_mask_low[current_idx + len(pattern_word) : next_idx] 
 
-    postal_code_mask_normalized += POSTAL_UPPERCASE_LETTER_CHAR + postal_code_mask_low[last_idx + len(POSTAL_UPPERCASE_LETTER_WORD):]
+        postal_code_mask_normalized += pattern_char + postal_code_mask_low[last_idx + len(pattern_word):]
     
-    print(postal_code_mask_normalized)
-
+        print(postal_code_mask_normalized)
+        postal_code_mask_low = postal_code_mask_normalized
         
     postal_code = generate_postal_code_from_normalized_pattern(postal_code_mask_normalized)
 
@@ -81,7 +84,8 @@ def generate_postal_code_from_normalized_pattern(postal_code_mask_normalized):
 
 
 def main():
-    latveria_postal_code = generate_postal_code(LATVERIA_POSTAL_CODE_PATTERN)
+    
+    latveria_postal_code = generate_postal_code(LATVERIA_POSTAL_CODE_PATTERN, PATTERN_WORD_CHAR_DIC)
     print(latveria_postal_code)
 
 
