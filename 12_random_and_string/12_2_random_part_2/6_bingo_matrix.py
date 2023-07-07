1b1676f0c101a5381cba92f78ccbc51d851e869c
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-07-07 15:58:24 +0200

    Added full logic

--- 12_random_and_string/12_2_random_part_2/6_bingo_matrix.py
+++ 12_random_and_string/12_2_random_part_2/6_bingo_matrix.py
@@ -1,5 +1,23 @@
+from random import sample
+
+ROW_COUNT = 5
+COL_COUNT = 5
+
+FIRST_POSSIBLE_NUM = 1
+LAST_POSSIBLE_NUM = 75
+CENTER_NUM = 0
+
+
 def main():
-    pass
+    num_amount = ROW_COUNT * COL_COUNT
+    bingo_nums = sample(range(FIRST_POSSIBLE_NUM, LAST_POSSIBLE_NUM), num_amount)
+    bingo_nums[len(bingo_nums) // 2] = CENTER_NUM
+    bingo_matrix = [bingo_nums[i:i+COL_COUNT] for i in range(0, len(bingo_nums), COL_COUNT)]
+
+    for row in range(ROW_COUNT):
+        for col in range(COL_COUNT):
+            print(str(bingo_matrix[row][col]).ljust(3), end='')
+        print()
 
 
 main()
