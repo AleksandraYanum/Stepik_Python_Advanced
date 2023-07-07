6f3e4551cfb3438fe84bacbb9a2a679875cf5e03
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-07-07 16:10:34 +0200

    Change center num filling order

--- 12_random_and_string/12_2_random_part_2/6_bingo_matrix.py
+++ 12_random_and_string/12_2_random_part_2/6_bingo_matrix.py
@@ -11,8 +11,8 @@ CENTER_NUM = 0
 def main():
     num_amount = ROW_COUNT * COL_COUNT
     bingo_nums = sample(range(FIRST_POSSIBLE_NUM, LAST_POSSIBLE_NUM), num_amount)
-    bingo_nums[len(bingo_nums) // 2] = CENTER_NUM
     bingo_matrix = [bingo_nums[i:i+COL_COUNT] for i in range(0, len(bingo_nums), COL_COUNT)]
+    bingo_matrix[ROW_COUNT // 2][COL_COUNT // 2] = CENTER_NUM
 
     for row in range(ROW_COUNT):
         for col in range(COL_COUNT):
