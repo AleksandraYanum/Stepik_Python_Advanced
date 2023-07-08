771106b1bbdde5a622ce5656d932b64ada8dd345
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-07-08 21:40:03 +0200

    Created 8_password_gererator_1.py

---
+++ 12_random_and_string/12_2_random_part_2/8_password_generator_1.py
@@ -0,0 +1,14 @@
+def generate_password_by_len(length):
+    pass
+
+
+def generate_passwords(amount, length):
+    pass
+
+
+def main():
+    password_amount = int(input())
+    password_len = int(input())
+
+
+main()
