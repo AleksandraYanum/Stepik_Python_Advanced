2d81185d60a33ba0635481d531366ec79c22bded
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-07-10 15:36:58 +0200

    Removed unnecessary copy and loop

--- 12_random_and_string/12_2_random_part_2/7_secret_friend.py
+++ 12_random_and_string/12_2_random_part_2/7_secret_friend.py
@@ -4,15 +4,12 @@ def main():
     friends = [input() for _ in range(int(input()))]
     shuffle(friends)
 
-    friends_copy = friends.copy()
-    friends_copy = friends_copy[1:]
-    friends_copy.append(friends[0])
+    friends_copy = friends[1:] + list(friends[0])
 
     secret_friend_list = list(zip(friends, friends_copy))
 
     for pair in secret_friend_list:
-        for i in range(1, len(pair)):
-            print(f"{pair[i - 1]} - {pair[i]}")
+        print(*pair, sep=' - ')
 
 
 main()
