b32f4f92eab16d43425891ecec9d7f08862f6aec
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-07-10 14:22:33 +0200

    Introduced secret_friend_list

--- 12_random_and_string/12_2_random_part_2/7_secret_friend.py
+++ 12_random_and_string/12_2_random_part_2/7_secret_friend.py
@@ -4,6 +4,7 @@ def main():
     friends = [input() for _ in range(int(input()))]
     shuffle(friends)
 
+    secret_friend_list = []
     for i in range(len(friends)):
         print(f"{friends[i - 1]} - {friends[i]}")
 
