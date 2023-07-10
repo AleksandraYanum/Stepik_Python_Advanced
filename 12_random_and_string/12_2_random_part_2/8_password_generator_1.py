879bf3bc141556bbc07ac3d6525c3f282a714b4f
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-07-10 14:11:31 +0200

    Added full logic

--- 12_random_and_string/12_2_random_part_2/8_password_generator_1.py
+++ 12_random_and_string/12_2_random_part_2/8_password_generator_1.py
@@ -1,14 +1,28 @@
-def generate_password_by_len(length):
-    pass
+from string import ascii_letters
+from string import digits
+from random import sample
 
 
-def generate_passwords(amount, length):
-    pass
+EXCEPTION_CHARS = 'lI1oO0'
+PASSWORD_CHARS = ''.join(set(ascii_letters) | set(digits) - set(EXCEPTION_CHARS))
+
+
+def generate_password_by_len(length, chars):
+    password = ''.join(sample(chars, length))
+    return password
+
+
+def generate_passwords(amount, length, chars):
+    password_list = [generate_password_by_len(length, PASSWORD_CHARS) for _ in range(amount)]
+    return password_list
 
 
 def main():
     password_amount = int(input())
     password_len = int(input())
+    passwords = generate_passwords(password_amount, password_len, PASSWORD_CHARS)
+
+    print(*passwords, sep='\n')
 
 
 main()
