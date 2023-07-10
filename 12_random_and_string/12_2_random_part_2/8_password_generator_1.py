1345d97c4807796b835b8a02f8a3b4d69703138b
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-07-10 14:28:04 +0200

    12_2_8 Password generator 1 (#106)
    * Created 8_password_gererator_1.py
    
    * Added full logic
    
    * Changed EXCEPTION_CHARS typy to set
    
    * Used var insted of const in generate_passwords

---
+++ 12_random_and_string/12_2_random_part_2/8_password_generator_1.py
@@ -0,0 +1,28 @@
+from string import ascii_letters
+from string import digits
+from random import sample
+
+
+EXCEPTION_CHARS = set('lI1oO0')
+PASSWORD_CHARS = ''.join(set(ascii_letters) | set(digits) - EXCEPTION_CHARS)
+
+
+def generate_password_by_len(length, chars):
+    password = ''.join(sample(chars, length))
+    return password
+
+
+def generate_passwords(amount, length, chars):
+    password_list = [generate_password_by_len(length, chars) for _ in range(amount)]
+    return password_list
+
+
+def main():
+    password_amount = int(input())
+    password_len = int(input())
+    passwords = generate_passwords(password_amount, password_len, PASSWORD_CHARS)
+
+    print(*passwords, sep='\n')
+
+
+main()
