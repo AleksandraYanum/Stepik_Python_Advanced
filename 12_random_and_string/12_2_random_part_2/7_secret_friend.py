7986c11d38218d9db9ba0aa0bd0bac103f75d3bf
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-07-10 15:21:55 +0200

    Added business logic to create friend pairs

--- 12_random_and_string/12_2_random_part_2/7_secret_friend.py
+++ 12_random_and_string/12_2_random_part_2/7_secret_friend.py
@@ -4,9 +4,15 @@ def main():
     friends = [input() for _ in range(int(input()))]
     shuffle(friends)
 
-    secret_friend_list = []
-    for i in range(len(friends)):
-        print(f"{friends[i - 1]} - {friends[i]}")
+    friends_copy = friends.copy()
+    friends_copy = friends_copy[1:]
+    friends_copy.append(friends[0])
+
+    secret_friend_list = list(zip(friends, friends_copy))
+
+    for pair in secret_friend_list:
+        for i in range(1, len(pair)):
+            print(f"{pair[i - 1]} - {pair[i]}")
 
 
 main()
