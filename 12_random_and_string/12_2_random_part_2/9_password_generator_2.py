bb76dc76a6808d0e10b1e3b258c1c76b7c36fb7f
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-07-11 09:46:08 +0200

    Added additional consts for password chars

--- 12_random_and_string/12_2_random_part_2/9_password_generator_2.py
+++ 12_random_and_string/12_2_random_part_2/9_password_generator_2.py
@@ -1,10 +1,14 @@
-from string import ascii_letters
+from string import ascii_lowercase
+from string import ascii_uppercase
 from string import digits
 from random import sample
 
 
 EXCEPTION_CHARS = set('lI1oO0')
-PASSWORD_CHARS = ''.join((set(ascii_letters) | set(digits)) - EXCEPTION_CHARS)
+PASSWORD_DIGITS = set(digits) - EXCEPTION_CHARS
+PASSWORD_LOWERCASE = set(ascii_lowercase) - EXCEPTION_CHARS
+PASSWORD_UPPERCASE = set(ascii_uppercase) - EXCEPTION_CHARS
+ALL_PASSWORD_CHARS = ''.join(PASSWORD_DIGITS | PASSWORD_LOWERCASE |PASSWORD_UPPERCASE)
 
 
 def generate_password_by_len(length, chars):
@@ -20,7 +24,7 @@ def generate_passwords(amount, length, chars):
 def main():
     password_amount = int(input())
     password_len = int(input())
-    passwords = generate_passwords(password_amount, password_len, PASSWORD_CHARS)
+    passwords = generate_passwords(password_amount, password_len, ALL_PASSWORD_CHARS)
 
     print(*passwords, sep='\n')
 
