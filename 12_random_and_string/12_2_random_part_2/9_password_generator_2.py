57d5652ae005a7c36dedc9ccff7b77202203132e
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-07-11 10:34:21 +0200

    Moved const PASSWORD_ALL_CHARS to var all_chars

--- 12_random_and_string/12_2_random_part_2/9_password_generator_2.py
+++ 12_random_and_string/12_2_random_part_2/9_password_generator_2.py
@@ -12,11 +12,11 @@ EXCEPTION_CHARS = set('lI1oO0')
 PASSWORD_DIGITS = set(digits) - EXCEPTION_CHARS
 PASSWORD_LOWERCASE = set(ascii_lowercase) - EXCEPTION_CHARS
 PASSWORD_UPPERCASE = set(ascii_uppercase) - EXCEPTION_CHARS
-PASSWORD_ALL_CHARS = ''.join(PASSWORD_DIGITS | PASSWORD_LOWERCASE | PASSWORD_UPPERCASE)
 MIN_PASSWORD_LENGTH = 3
 
 
-def generate_password_by_len(length, digits, lowercase, uppercase, all_chars):
+def generate_password_by_len(length, digits, lowercase, uppercase):
+    all_chars = ''.join(digits | lowercase | uppercase)
     password = sample(digits, 1) + sample(lowercase, 1) + sample(uppercase, 1) + \
                        sample(all_chars, length - MIN_PASSWORD_LENGTH)
     shuffle(password)
@@ -25,8 +25,8 @@ def generate_password_by_len(length, digits, lowercase, uppercase, all_chars):
     return password
 
 
-def generate_passwords(amount, length, digits, lowercase, uppercase, all_chars):
-    password_list = [generate_password_by_len(length, digits, lowercase, uppercase, all_chars) \
+def generate_passwords(amount, length, digits, lowercase, uppercase):
+    password_list = [generate_password_by_len(length, digits, lowercase, uppercase) \
                      for _ in range(amount)]
     return password_list
 
@@ -35,7 +35,7 @@ def main():
     password_amount = int(input())
     password_len = int(input())
     passwords = generate_passwords(password_amount, password_len, PASSWORD_DIGITS, \
-                                   PASSWORD_LOWERCASE, PASSWORD_UPPERCASE, PASSWORD_ALL_CHARS)
+                                   PASSWORD_LOWERCASE, PASSWORD_UPPERCASE)
 
     print(*passwords, sep='\n')
 
