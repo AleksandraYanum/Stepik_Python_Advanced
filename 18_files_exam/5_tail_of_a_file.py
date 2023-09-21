fc554075a78c942a445d76f980f927b0fb867004
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-09-21 12:57:39 +0200

    Used effective data structure: deque insteadf of list

--- 18_files_exam/5_tail_of_a_file.py
+++ 18_files_exam/5_tail_of_a_file.py
@@ -1,12 +1,32 @@
-LINE_AMOUNT = 10
+# LINE_AMOUNT = 10
 
-file_tail = []
+# file_tail = []
 
-with open(input(),  encoding='utf-8') as file:
-        for line in file:
-                if len(file_tail) == LINE_AMOUNT:
-                        file_tail.pop(0)
-                file_tail.append(line.strip())
+# with open(input(),  encoding='utf-8') as file:
+#         for line in file:
+#                 if len(file_tail) == LINE_AMOUNT:
+#                         file_tail.pop(0)
+#                 file_tail.append(line.strip())
 
                 
+# print(*file_tail, sep='\n')
+
+#***************************************************************************************************************
+
+# Deque is preferred over a list in the cases where we need quicker append or pop operations from 
+# both the ends of the container. This is possible because Python's deque is implemented as a doubly linked list. 
+
+from collections import deque as de
+
+
+LINE_AMOUNT = 10
+
+file_tail = de([])
+
+with open(r'C:\Users\Aleksandra\Downloads\text.txt',  encoding='utf-8') as file:
+        for line in file:
+                if len(file_tail) == LINE_AMOUNT:
+                        file_tail.popleft()
+                file_tail.append(line.strip())
+
 print(*file_tail, sep='\n')
