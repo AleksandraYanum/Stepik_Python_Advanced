39c8988e04e3a548bbcbecd64891a9b673e5fdee
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-09-21 12:12:06 +0200

    Deleted "if True then True" pattern

--- 18_files_exam/3_goooood_students.py
+++ 18_files_exam/3_goooood_students.py
@@ -6,7 +6,7 @@ good_student_amount = 0
 with open('grades.txt',  encoding='utf-8') as file:
     for line in file:
         name, *scores = line.split()
-        if all(map(lambda x: True if x >= MIN_GOOD_SCORE else False, map(int, scores))):
+        if all(map(lambda x: x >= MIN_GOOD_SCORE, map(int, scores))):
                 good_student_amount += 1
 
 print(good_student_amount)
