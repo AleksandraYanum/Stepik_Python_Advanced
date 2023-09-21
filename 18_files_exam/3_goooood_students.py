f2c522e18aad09859c460903886d939f2cbcf7d3
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-09-21 12:15:01 +0200

    18_3 Goooood students (#145)
    * Created 3_goooood students.py
    
    * Deleted "if True then True" pattern

---
+++ 18_files_exam/3_goooood_students.py
@@ -0,0 +1,12 @@
+MIN_GOOD_SCORE = 65
+
+
+good_student_amount = 0
+
+with open('grades.txt',  encoding='utf-8') as file:
+    for line in file:
+        name, *scores = line.split()
+        if all(map(lambda x: x >= MIN_GOOD_SCORE, map(int, scores))):
+                good_student_amount += 1
+
+print(good_student_amount)
