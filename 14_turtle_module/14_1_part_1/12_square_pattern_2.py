eb61fb3ed279c6fabcff8d95f4bec951dac49053
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-09-24 16:31:08 +0200

    Replaced i -> side

--- 14_turtle_module/14_1_part_1/12_square_pattern_2.py
+++ 14_turtle_module/14_1_part_1/12_square_pattern_2.py
@@ -2,9 +2,9 @@ import turtle as t
 
 
 def square_pattern(smallest_side, biggest_side, step):
-  for i in range(smallest_side, biggest_side, step):
+  for side in range(smallest_side, biggest_side, step):
     t.left(90)
-    t.forward(i)
+    t.forward(side)
     
     
 def main():
