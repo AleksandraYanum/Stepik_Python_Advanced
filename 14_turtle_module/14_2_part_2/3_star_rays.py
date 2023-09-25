3adbcaff0272857d9343157fba4d49ae020830b1
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-09-25 13:10:55 +0200

    Created 3_star_rays.py

---
+++ 14_turtle_module/14_2_part_2/3_star_rays.py
@@ -0,0 +1,22 @@
+import turtle as t
+
+
+def star_rays(ray_size, ray_amount=12):
+  turn_angle = 360 / ray_amount
+  for i in range(ray_amount):
+      t.forward(ray_size)
+      t.stamp()
+      t.backward(ray_size)
+      t.right(turn_angle)
+
+
+def main():
+  t.shape('triangle')
+  ray_amount = int(input())
+  ray_size = 120
+  t.dot(20)
+  star_rays(ray_size, ray_amount)
+
+
+
+main()
