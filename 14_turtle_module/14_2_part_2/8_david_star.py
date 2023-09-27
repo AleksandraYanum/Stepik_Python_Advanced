b01927015b98137017450760821a6e732ab393aa
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-09-27 05:41:11 +0200

    Created 8_david_star.py

---
+++ 14_turtle_module/14_2_part_2/8_david_star.py
@@ -0,0 +1,31 @@
+import turtle as t
+
+
+def david_star(triangle_side):
+  angle_amount = 3
+  shape(triangle_side, angle_amount)
+  
+  t.penup()
+  t.goto(0, 60)
+  t.pendown()
+
+  is_up = 0
+  shape(triangle_side, angle_amount, is_up)
+
+
+def shape(side, angle_amount, is_up=1):
+  turn_angle = 360 / angle_amount
+  for i in range(angle_amount):
+    t.forward(side)
+    if is_up:
+      t.left(turn_angle)
+    else:
+      t.right(turn_angle)
+      
+
+def main():
+  triangle_side = 100
+  david_star(triangle_side)
+  
+
+main()
