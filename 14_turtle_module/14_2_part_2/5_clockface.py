7dd62b5d808ba16b6bb27e99617a923c38e5b112
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-09-28 12:58:43 +0200

    14_2_5 Clockface (#193)
    * Created 5_clockface.py
    
    * Added bg color
    
    * Removed unused var i

---
+++ 14_turtle_module/14_2_part_2/5_clockface.py
@@ -0,0 +1,35 @@
+import turtle as t
+
+
+def turtle_circle(ray_size, ray_amount=12):
+  t.Screen().bgcolor('lightblue')
+  t.penup()
+  t.shape('turtle')
+  t.shapesize(1)
+  t.pensize(5)
+  t.stamp()
+
+  turn_angle = 360 / ray_amount
+  line_len = 20
+  empty_line_len = 30
+  bakward_len_dif = line_len + empty_line_len
+  for _ in range(ray_amount):
+      t.forward(ray_size)
+      t.pendown()
+      t.forward(line_len)
+      t.penup()
+      t.forward (empty_line_len)
+
+      t.stamp()
+      t.backward(ray_size + bakward_len_dif)
+      t.right(turn_angle)
+
+
+def main():
+  ray_amount = 12
+  ray_size = 120
+  turtle_circle(ray_size, ray_amount)
+
+
+
+main()
