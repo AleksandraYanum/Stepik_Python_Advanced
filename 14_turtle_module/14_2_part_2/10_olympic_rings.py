87a05fa0dc7eeb9f71071b707645ba641c84efcf
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-02 21:12:51 +0200

    Added initial colorful drawing

--- 14_turtle_module/14_2_part_2/10_olympic_rings.py
+++ 14_turtle_module/14_2_part_2/10_olympic_rings.py
@@ -1,14 +1,16 @@
 import turtle as t
+from itertools import cycle
 
 
-def circle_line(radius, amount, start_x, start_y):
+def circle_line(radius, amount, start_x, start_y, colors):
     x_cor, y_cor = start_x, start_y
     diameter = 2 * radius
     t.hideturtle()
-    for _ in range(amount):
+    for _, color in zip(range(amount), cycle(colors)):
         t.penup()
         t.goto(x_cor, y_cor)
         t.pendown()
+        t.pencolor(color)
         t.circle(radius)
         x_cor = x_cor + diameter
 
@@ -17,22 +19,22 @@ def circle_line(radius, amount, start_x, start_y):
 def olympic_rings(radius, start_x, start_y):
     diameter = 2 * radius
     t.speed(10)
-    # colors = ['chartreuse', 'red', 'black', 'DeepSkyBlue', 'yellow']
+    colors = ['chartreuse', 'DeepSkyBlue', 'black', 'red', 'yellow']
 
 
     # green circle
     circle_amount = 1
     x_cor, y_cor = start_x + radius, start_y - radius
-    circle_line(radius, circle_amount,  x_cor, y_cor)
+    circle_line(radius, circle_amount,  x_cor, y_cor, colors[0:1])
 
     # all top circles
     circle_amount = 3
     x_cor, y_cor = x_cor - diameter - radius, y_cor + radius
-    circle_line(radius, 3,  x_cor, y_cor)
+    circle_line(radius, 3,  x_cor, y_cor, colors[1:4])
 
     # yellow circle
     x_cor, y_cor = x_cor + radius, y_cor - radius
-    circle_line(radius, 1,  x_cor, y_cor)
+    circle_line(radius, 1,  x_cor, y_cor, colors[4:5])
 
 
 def main():
