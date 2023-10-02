f4bd3e797c998a6404df6483cad9b049632cb39b
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-02 15:06:09 +0200

    Added logic for triangle start calc

--- 14_turtle_module/14_2_part_2/8_david_star.py
+++ 14_turtle_module/14_2_part_2/8_david_star.py
@@ -1,18 +1,26 @@
 # TODO: make david star drawing with perfect positions
 
 import turtle as t
+from math import cos, pi
 
 
-def david_star(triangle_side, compression_percent=0.6):
-  angle_amount = 3
-  shape(triangle_side, angle_amount)
-  
+def david_star(triangle_side, angle_amount=6):
+  turn_angle = 360 / angle_amount
+  radius = int(triangle_side / 2 / cos(turn_angle / 2 * pi / 180))
+
+  shape(triangle_side, int(angle_amount / 2))
+
+  t.hideturtle()
   t.penup()
-  t.goto(0, triangle_side * compression_percent)
+  t.left(turn_angle / 2)
+  t.forward(radius)  
+  t.left(turn_angle * 2)
+  t.forward(radius)  
+  t.right(turn_angle * 2 + turn_angle / 2)
   t.pendown()
+  t.showturtle()
 
-  is_up = 0
-  shape(triangle_side, angle_amount, is_up)
+  shape(triangle_side, int(angle_amount / 2), 0)
 
 
 def shape(side, angle_amount, is_up=1):
