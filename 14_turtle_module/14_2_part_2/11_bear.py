332e56f053d609ce8d79f435687583b8be6a4cf7
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-02 23:26:53 +0200

    Changed draw logic with starting x, y coords

--- 14_turtle_module/14_2_part_2/11_bear.py
+++ 14_turtle_module/14_2_part_2/11_bear.py
@@ -1,62 +1,76 @@
 import turtle as t
 
 
-def draw_simple_bear(size):
-    t.pencolor('SaddleBrown')
+def draw_simple_bear(start_x, start_y, head_radius):
+    t.penup()
+    t.goto(start_x, start_y)
     t.hideturtle()
-    t.speed(10)
+    t.speed(20)
+    t.pencolor('SaddleBrown')
+    t.pendown()
 
     # Head
-    t.penup()
-    t.setpos(0, -size * 1.8)
-    t.pendown()
     t.fillcolor('BurlyWood')
     t.begin_fill()
-    t.circle(size * 2)
+    t.circle(head_radius)
     t.end_fill()
 
     # Face
+    face_radius = head_radius / 2
     t.fillcolor('NavajoWhite')
     t.begin_fill()
-    t.circle(size * 1.2)
+    t.circle(face_radius)
     t.end_fill()
 
-    # Mouth
+    # Nose and mouth
+    nose_radius = head_radius / 10
     t.fillcolor('Maroon')
     t.penup()
-    t.setpos(0, -size / 10)
+    t.setpos(start_x, start_y + face_radius)
     t.pendown()
     t.begin_fill()
-    t.circle(size / 5)
+    t.circle(nose_radius)
     t.end_fill()
-    t.setpos(0, -size)
+    t.setpos(start_x, start_y + nose_radius)
 
     # Eyes
-    for eye_pos in [(-size, size / 3), (size, size / 3)]:
-        t.color('Black', 'Black')
+    eye_raduis = nose_radius
+    eye_x_cor_1 = start_x - face_radius
+    eye_x_cor_2 = start_x + face_radius
+    eye_y_cor = start_y + head_radius
+
+    for eye_pos in [(eye_x_cor_1, eye_y_cor), (eye_x_cor_2, eye_y_cor)]:
+        t.color('Black')
         t.penup()
         t.setpos(eye_pos)
         t.pendown()
         t.begin_fill()
-        t.circle(size // 5)
+        t.circle(eye_raduis)
         t.end_fill()
 
     # Ears
-    for ear_pos in [(-size * 1.7, size * 1.5), (size * 1.7, size * 1.5)]:
+    ear_x_cor_1 = start_x - head_radius
+    ear_x_cor_2 = start_x + head_radius
+    ear_y_cor = start_y + head_radius * 1.5
+
+    for ear_pos in [(ear_x_cor_1, ear_y_cor), (ear_x_cor_2, ear_y_cor)]:
         t.fillcolor('LightPink')
         t.penup()
         t.setpos(ear_pos)
         t.pendown()
         t.begin_fill()
-        t.circle(size // 2)
+        t.circle(face_radius / 2)
         t.end_fill()
 
     t.penup()
-    t.setpos(0, -size * 3)
+    input()
+    t.setpos(0, -head_radius * 3)
 
 
 def main():
-    draw_simple_bear(50)
+    head_radius = 100
+    start_x, start_y = 0, -150
+    draw_simple_bear(start_x, start_y, head_radius)
 
 
 main()
