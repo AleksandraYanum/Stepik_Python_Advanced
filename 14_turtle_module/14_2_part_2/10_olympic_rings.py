d188d5b526a06c217e6c017ce0ba5f6564938add
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-02 19:43:46 +0200

    Added initial logic for olympic_rings()

--- 14_turtle_module/14_2_part_2/10_olympic_rings.py
+++ 14_turtle_module/14_2_part_2/10_olympic_rings.py
@@ -14,25 +14,31 @@ def circle_line(radius, amount, start_x, start_y):
 
 
 
-# def olympic_rings(radius, start_x, start_y):
-#     t.pensize(7)
-#     t.speed(10)
+def olympic_rings(radius, start_x, start_y):
+    diameter = 2 * radius
+    # t.speed(10)
+    # colors = ['chartreuse', 'red', 'black', 'DeepSkyBlue', 'yellow']
 
-#     colors = ['chartreuse', 'red', 'black', 'DeepSkyBlue', 'yellow']
-    
-#     for color, position in zip(colors, positions):
-#         t.penup()
-#         t.goto(position)
-#         t.pencolor(color)
-#         t.pendown()
-#         t.circle(radius)
+    # green circle
+    x_cor, y_cor = start_x + radius, start_y - radius
+    circle_line(radius, 1,  x_cor, y_cor)
+
+    # all top circles
+    x_cor, y_cor = x_cor - diameter - radius, y_cor + radius
+    circle_line(radius, 3,  x_cor, y_cor)
+
+    # yellow circle
+    x_cor, y_cor = x_cor + radius, y_cor - radius
+    circle_line(radius, 1,  x_cor, y_cor)
 
 
 def main():
-    radius = 50
+    radius = 70
+
+    # Coordinate of the point of the two lower circles contact
     start_x, start_y = 0, 0
-    # olympic_rings(radius, start_x, start_y)
-    circle_line(radius, 4, start_x, start_y)
+    olympic_rings(radius, start_x, start_y)
+    # circle_line(radius, 4, start_x, start_y)
 
 
 main()
