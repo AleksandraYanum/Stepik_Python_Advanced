efcb82b6a88cc0d9e588b86088849d5e5880dce1
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-03 10:57:33 +0200

    Parameterized drawing

--- 14_turtle_module/14_2_part_2/9_lines.py
+++ 14_turtle_module/14_2_part_2/9_lines.py
@@ -1,26 +1,38 @@
 import turtle as t
 
 
-def lines(line_len, line_color='MediumAquamarine', dot_color='Blue', top_dot_color='Orange'):
+def lines(start_x, start_y, line_amount, height, dot_distance, 
+          line_color='MediumAquamarine', dot_color='Blue', top_dot_color='Orange'):
+  
+  t.hideturtle(), t.speed(20)
   t.penup()
-  t.goto(0, 100)
-  for x_cor in range(-200, 201, 40):
+  t.goto(start_x, start_y)
+  end_y = start_y - height
+  dots_len = (line_amount - 1) * dot_distance
+  x_cor = int(start_x - dots_len / 2)
+  t.color(line_color)
+  
+  for _ in range(line_amount):
     t.pendown()
     t.color(line_color)
-    t.goto(x_cor, -line_len)
+    t.goto(x_cor, end_y)
     t.color(dot_color)
     t.dot()
     t.penup()
-    t.goto(0, 100)
-    
+    t.goto(start_x, start_y)
+    x_cor += dot_distance
+
+
   t.color(top_dot_color)
   t.dot()
-  t.hideturtle()
-  
-  
+
+
 def main():
-  line_len = 150
-  lines(line_len)
+  height = 300
+  line_amount = 10
+  dot_distance = 50
+  start_x, start_y = -50, 200
+  lines(start_x, start_y, line_amount, height, dot_distance)
   
   
 main()
