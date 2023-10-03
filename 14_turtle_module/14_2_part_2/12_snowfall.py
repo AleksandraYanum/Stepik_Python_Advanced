f6b2273a4fa5fa079e1b701530bab1fe1391ce2f
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-03 16:29:53 +0200

    Added initial branch drawing func

--- 14_turtle_module/14_2_part_2/12_snowfall.py
+++ 14_turtle_module/14_2_part_2/12_snowfall.py
@@ -1,8 +1,45 @@
 import turtle as t
 
 
+def branch(start_x, start_y, branch_len, leave_amount):
+    t.left(90)
+    t.penup()
+    t.goto(start_x, start_y)
+    t.pendown()
+
+    leave_distance = branch_len // (leave_amount + 2)
+    leave_len = 50
+    leave_angle = 45
+
+    t.forward(branch_len)
+
+    for _ in range(leave_amount):
+        t.penup()
+        t.backward(leave_distance)
+        t.left(leave_angle)
+        t.pendown()
+        t.forward(leave_len)
+        t.penup()
+        t.backward(leave_len)
+        t.right(leave_angle * 2)
+        t.pendown()
+        t.forward(leave_len)
+        t.penup()
+        t.backward(leave_len)
+        t.left(leave_angle)
+
+
+
+    
+
+
+
 def main():
-    pass
+    start_x, start_y = 0, 0
+    branch_len = 200
+    leave_amount = 2
+    branch(start_x, start_y, branch_len, leave_amount)
+    input()
 
 
 main()
