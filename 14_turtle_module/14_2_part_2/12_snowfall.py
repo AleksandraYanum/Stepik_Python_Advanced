f4cdc0b068a5dc3bf920e0448b0631fcd8dbf170
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-03 23:30:26 +0200

    Added lambdda func for wrapper

--- 14_turtle_module/14_2_part_2/12_snowfall.py
+++ 14_turtle_module/14_2_part_2/12_snowfall.py
@@ -81,6 +81,9 @@ def circle_ray(start_x, start_y, ray_len):
 def five_circle_core(start_x, start_y, biggest_radius):
     circle_core(start_x, start_y, biggest_radius, circle_amount=5, circle_radius_delta=10)
 
+# five_circle_core_lambda = lambda start_x, start_y, biggest_radius: \
+# circle_core(start_x, start_y, biggest_radius, circle_amount=5, circle_radius_delta=10)
+
 
 def circle_core(start_x, start_y, radius, circle_amount=1, circle_radius_delta=0):
     t.penup()
@@ -103,6 +106,9 @@ def main():
     radius = 200
 
     snowflake(start_x, start_y, ray_amount, radius, branch_ray_two_leaves, five_circle_core)
+    # snowflake(start_x, start_y, ray_amount, radius, branch_ray_two_leaves, lambda start_x, start_y, biggest_radius: \
+    # circle_core(start_x, start_y, biggest_radius, circle_amount=5, circle_radius_delta=10))
+
 
     # snowflake(start_x, start_y, ray_amount, radius, circle_ray, circle_core)
     input()
