7a7bb3d77bf786f7cb1cc2f66dd6d8494e86ce96
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-10 14:58:52 +0200

    Added border check while drawing

--- 14_turtle_module/14_2_part_2/12_snowfall.py
+++ 14_turtle_module/14_2_part_2/12_snowfall.py
@@ -8,8 +8,8 @@ GEAR_INNER_RADIUS_PERCENTAGE = 0.7
 MIN_POSSIBLE_SNOWFLAKE_RADIUS = 50
 MAX_POSSIBLE_SNOWFLAKE_RADIUS = 200
 
-MIN_SNOWFLAKE_AMOUNT = 2
-MAX_SNOWFLAKE_AMOUNT = 15
+MIN_SNOWFLAKE_AMOUNT = 1
+MAX_SNOWFLAKE_AMOUNT = 10
 
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 650
@@ -162,19 +162,28 @@ def gear_circle_base(start_x, start_y, radius, ray_amount, mid_radius=None):
     input()
 
 
+def is_within_screen(start_x, start_y, radius):
+    result = (start_x + radius <= SCREEN_WIDTH and start_x - radius >= 0) and \
+             (start_y + radius <= SCREEN_HEIGHT and start_y - radius >= 0)
+    return result
+
+
 def main():
     t.Screen().setup(SCREEN_WIDTH, SCREEN_HEIGHT)
 
     random_snowflake_amount = randint(MIN_SNOWFLAKE_AMOUNT, MAX_SNOWFLAKE_AMOUNT)
     ray_amount = 8
 
-    for _ in range(random_snowflake_amount):
+    drawn_snowflake_amount = 0
+
+    while drawn_snowflake_amount != random_snowflake_amount:
         random_radius = randint(MIN_POSSIBLE_SNOWFLAKE_RADIUS, MAX_POSSIBLE_SNOWFLAKE_RADIUS)
         random_start_x = randint(0, SCREEN_WIDTH)
         random_start_y = randint(0, SCREEN_HEIGHT)
-    
-      
-        snowflake(random_start_x, random_start_y, ray_amount, random_radius, branch_ray_two_leaves, five_circle_core)
+
+        if is_within_screen(random_start_x, random_start_y, random_radius):
+            snowflake(random_start_x, random_start_y, ray_amount, random_radius, branch_ray_two_leaves, five_circle_core)
+            drawn_snowflake_amount += 1
     input()
 
  
