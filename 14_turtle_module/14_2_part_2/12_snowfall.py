4e5bf4c4ccb93427b1b054dd70a1c0d718855af5
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-11 13:45:36 +0200

    Fixed bool logic in is_not_overlapped()

--- 14_turtle_module/14_2_part_2/12_snowfall.py
+++ 14_turtle_module/14_2_part_2/12_snowfall.py
@@ -186,9 +186,9 @@ def is_not_overlapped(curr_x, curr_y, curr_radius, drawn_snowflake_info_list):
     for snowflake_info in drawn_snowflake_info_list:
         prev_x, prev_y, prev_radius = snowflake_info
         snowflake_min_distance = curr_radius + prev_radius
-        curr_distance = sqrt(abs(pow((curr_x - prev_x), 2) - pow((curr_y - prev_y), 2)))
+        curr_distance = sqrt(pow((curr_x - prev_x), 2) + pow((curr_y - prev_y), 2))
 
-        result = curr_distance < snowflake_min_distance
+        result = not curr_distance < snowflake_min_distance
         if not result:
             break
 
