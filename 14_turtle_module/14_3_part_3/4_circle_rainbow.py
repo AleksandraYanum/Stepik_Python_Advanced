5e6ae5e94ceb80583059ea0f66017ea2f232eda5
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-12 16:21:19 +0200

    Reafctoring: extracted vars, added filling color set up

--- 14_turtle_module/14_3_part_3/4_circle_rainbow.py
+++ 14_turtle_module/14_3_part_3/4_circle_rainbow.py
@@ -1,22 +1,28 @@
 import turtle as t
+from itertools import cycle
 
 
-DRAWING_SPEED = 10
+DRAWING_SPEED = 50
+RAINBOW_COLORS = ['red', 'orange', 'yellow', 'green', 'LightGreen', 'cyan', 'CornflowerBlue', 'blue', 'magenta', 'maroon1']
 
 
-def ten_circle_rainbow(start_x, start_y, biggest_radius):
-    circle_core(start_x, start_y, biggest_radius, circle_amount=7, circle_radius_delta=10)
+def ten_circle_rainbow(start_x, start_y, biggest_radius, colors):
+    circle_amount = 10
+    circle_radius_delta = biggest_radius // circle_amount
+    circle_core(start_x, start_y, biggest_radius, colors, circle_amount, circle_radius_delta)
 
   
-def circle_core(start_x, start_y, radius, circle_amount=1, circle_radius_delta=0):
+def circle_core(start_x, start_y, radius, colors, circle_amount=1, circle_radius_delta=0):
     x, y = start_x, start_y - radius
     t.penup()
     t.goto(x, y)
     t.pendown()
 
     y_cor = y
-    for _ in range(circle_amount):
+    for _, color in zip(range(circle_amount), cycle(colors)):
+        t.fillcolor(color), t.begin_fill()
         t.circle(radius)
+        t.end_fill()
         radius -= circle_radius_delta
         t.penup()
         y_cor += circle_radius_delta
@@ -26,10 +32,10 @@ def circle_core(start_x, start_y, radius, circle_amount=1, circle_radius_delta=0
 def main():
     t.hideturtle(), t.speed(DRAWING_SPEED)
 
-    bigest_radius = 300
+    bigest_radius = 200
     start_x, start_y = 0, - 50
 
-    circle_core(start_x, start_y, bigest_radius)
+    ten_circle_rainbow(start_x, start_y, bigest_radius, RAINBOW_COLORS)
 
     input()
 
