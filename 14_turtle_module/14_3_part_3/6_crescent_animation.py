66389b82dc55bd4b54f1cb267c90e0a07ff22ff7
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-13 16:46:10 +0200

    Added initial version of the 2nd part of animation

--- 14_turtle_module/14_3_part_3/6_crescent_animation.py
+++ 14_turtle_module/14_3_part_3/6_crescent_animation.py
@@ -3,29 +3,43 @@ import turtle as t
 
 CRESCENT_COLOR = 'orange'
 SKY_COLOR = 'blue'
-SPEED_DELTA = 5
+SPEED_DELTA = 20
 
 
 def main():
+    t.hideturtle(), t.penup()
 
     start_x, start_y = 0, -50
     crescent_diameter = 400
-    t.penup(), t.hideturtle()
-    t.goto(start_x, start_y)
-    t.dot(crescent_diameter, CRESCENT_COLOR)
-    
-
     t.bgcolor(SKY_COLOR)
 
-    x = start_x + crescent_diameter
-    t.goto(x, start_y)
+    while True:
 
-    for i in range(crescent_diameter // SPEED_DELTA):
-        x -= SPEED_DELTA
-        t.goto(x, start_y)
-        t.dot(crescent_diameter, SKY_COLOR)
+        t.goto(start_x, start_y)
+        t.dot(crescent_diameter, CRESCENT_COLOR)         
+
+        x = start_x + crescent_diameter    
+        
+        for _ in range(crescent_diameter // SPEED_DELTA):
+            x -= SPEED_DELTA
+            t.goto(x, start_y)
+            t.dot(crescent_diameter, SKY_COLOR)
+
+        t.goto(start_x, start_y)
+        t.dot(crescent_diameter, CRESCENT_COLOR)  
+
+        x = start_x - crescent_diameter  
+
+        for _ in range(crescent_diameter // SPEED_DELTA):
+            x += SPEED_DELTA
+            t.goto(x, start_y)
+            t.dot(crescent_diameter, SKY_COLOR)      
+
+
 
     input()
 
 
 main()
+
+
