3c3a2522cff0aca48665ed3f1e5890548fbadf05
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-13 14:57:42 +0200

    Added logic for the 1st part of animation

--- 14_turtle_module/14_3_part_3/6_crescent_animation.py
+++ 14_turtle_module/14_3_part_3/6_crescent_animation.py
@@ -1,8 +1,31 @@
 import turtle as t
 
 
+CRESCENT_COLOR = 'orange'
+SKY_COLOR = 'blue'
+SPEED_DELTA = 5
+
+
 def main():
-    pass
+
+    start_x, start_y = 0, -50
+    crescent_diameter = 400
+    t.penup(), t.hideturtle()
+    t.goto(start_x, start_y)
+    t.dot(crescent_diameter, CRESCENT_COLOR)
+    
+
+    t.bgcolor(SKY_COLOR)
+
+    x = start_x + crescent_diameter
+    t.goto(x, start_y)
+
+    for i in range(crescent_diameter // SPEED_DELTA):
+        x -= SPEED_DELTA
+        t.goto(x, start_y)
+        t.dot(crescent_diameter, SKY_COLOR)
+
+    input()
 
 
 main()
