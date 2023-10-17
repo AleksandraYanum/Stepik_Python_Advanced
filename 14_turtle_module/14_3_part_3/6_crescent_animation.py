6dbf35c773d86bcded61b8aadd48b35dcc82f769
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-10-17 15:45:10 +0200

    Added logic to draw growing cersent

--- 14_turtle_module/14_3_part_3/6_crescent_animation.py
+++ 14_turtle_module/14_3_part_3/6_crescent_animation.py
@@ -3,43 +3,40 @@ import turtle as t
 
 CRESCENT_COLOR = 'orange'
 SKY_COLOR = 'blue'
-SPEED_DELTA = 20
+SPEED_DELTA = 5
 
+
+def moon(start_x, start_y, radius):
+    t.penup()
+    t.goto(start_x, start_y)
+    t.fillcolor(CRESCENT_COLOR)
+    t.begin_fill()
+    t.circle(radius)
+    t.end_fill()
+ 
+
+def moon_shadow(start_x, start_y, radius, i): 
+    x = start_x + radius * 2
+    t.goto(x - i, start_y) 
+    t.fillcolor(SKY_COLOR)
+    t.begin_fill()
+    t.circle(radius)
+    t.end_fill()
 
 def main():
-    t.hideturtle(), t.penup()
-
-    start_x, start_y = 0, -50
-    crescent_diameter = 400
+    t.hideturtle(), t.penup(), t.tracer(0)
     t.bgcolor(SKY_COLOR)
 
+    start_x, start_y = 0, -50
+    crescent_radius = 120
+
     while True:
-
-        t.goto(start_x, start_y)
-        t.dot(crescent_diameter, CRESCENT_COLOR)         
-
-        x = start_x + crescent_diameter    
+        for i in range(0, crescent_radius * 4, SPEED_DELTA):
+            moon(start_x, start_y, crescent_radius)
+            moon_shadow(start_x, start_y, crescent_radius, i)
+            t.update()
         
-        for _ in range(crescent_diameter // SPEED_DELTA):
-            x -= SPEED_DELTA
-            t.goto(x, start_y)
-            t.dot(crescent_diameter, SKY_COLOR)
-
-        t.goto(start_x, start_y)
-        t.dot(crescent_diameter, CRESCENT_COLOR)  
-
-        x = start_x - crescent_diameter  
-
-        for _ in range(crescent_diameter // SPEED_DELTA):
-            x += SPEED_DELTA
-            t.goto(x, start_y)
-            t.dot(crescent_diameter, SKY_COLOR)      
-
-
-
-    input()
-
-
+    
 main()
 
 
