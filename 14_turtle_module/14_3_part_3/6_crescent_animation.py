1531039d0474c4da0f3d7d1d1efbf051ffe77612
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-10-20 16:51:17 +0200

    14_3_6 Crescent animation (#223)
    * Created 6_crescent_animation.py
    
    * Added logic for the 1st part of animation
    
    * Added initial version of the 2nd part of animation
    
    * Added logic to draw growing cersent
    
    * Removed duplicate function moon_shadow
    
    * Added shadow_start_x, shadow_x

---
+++ 14_turtle_module/14_3_part_3/6_crescent_animation.py
@@ -0,0 +1,39 @@
+import turtle as t
+
+
+CRESCENT_COLOR = 'orange'
+SKY_COLOR = 'blue'
+SPEED_DELTA = 5
+
+
+def moon(start_x, start_y, radius, color):
+    t.penup()
+    t.goto(start_x, start_y)
+    t.fillcolor(color)
+    t.begin_fill()
+    t.circle(radius)
+    t.end_fill()
+
+
+def main():
+    t.hideturtle(), t.penup(), t.tracer(0)
+    t.bgcolor(SKY_COLOR)
+
+    start_x, start_y = 0, -50
+    crescent_radius = 120
+    crescent_diameter = crescent_radius * 2
+    shadow_start_x = start_x + crescent_diameter
+
+    while True:
+        for i in range(0, crescent_radius * 4, SPEED_DELTA):
+            shadow_x = shadow_start_x - i
+
+            moon(start_x, start_y, crescent_radius, CRESCENT_COLOR)
+            moon(shadow_x, start_y, crescent_radius, SKY_COLOR)
+
+            t.update()
+        
+    
+main()
+
+
