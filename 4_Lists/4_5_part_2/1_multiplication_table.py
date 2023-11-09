80fece46e1e7b4d8cd027256457bb4d3e794d308
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-11-09 15:59:21 +0100

    4_5_1 Multiplication table improvement (#227)
    * Created 1_multiplication_table.py
    
    * Removed unnecessary calc of matrix elems
    
    * Replaced mult with sum
    
    * Replaced mult with sum [2] + fixed prev replacement
    
    * Added ljust method

---
+++ 4_Lists/4_5_part_2/1_multiplication_table.py
@@ -0,0 +1,41 @@
+ALIGNMENT_LENGTH = 3
+
+# row_count, col_count = int(input()), int(input())
+row_count, col_count = 4, 5
+
+mult_table = []
+
+# for row in range(row_count):
+#     curr_row = []
+#     curr_elem = 0
+#     for col in range(col_count):
+#         curr_row.append(str(curr_elem).ljust(ALIGNMENT_LENGTH))
+#         curr_elem += row 
+#     mult_table.append(curr_row)
+
+
+# left_col - elems before main diag
+# right_col - elems of main diag and after it
+
+for row in range(min(row_count, col_count)):
+    curr_row = []
+    curr_elem = pow(row, 2)
+    for left_col in range(row):
+        curr_row.append(mult_table[left_col][row])
+    for right_col in range(row, col_count):
+        curr_row.append(str(curr_elem).ljust(ALIGNMENT_LENGTH))
+        curr_elem += row
+    mult_table.append(curr_row)
+
+if col_count < row_count:
+    curr_elem = 0
+    for row in range(col_count, row_count):
+        curr_elem = 0
+        curr_row = []
+        for col in range(col_count):
+            curr_row.append(str(curr_elem).ljust(ALIGNMENT_LENGTH))
+            curr_elem += row
+        mult_table.append(curr_row)
+
+for row in mult_table:
+    print(*row)
