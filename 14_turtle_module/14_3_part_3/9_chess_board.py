9df6863757a5a7f68f7e6994cf34eac611049d2f
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-11-14 13:27:59 +0100

    Added colors for drawing

--- 14_turtle_module/14_3_part_3/9_chess_board.py
+++ 14_turtle_module/14_3_part_3/9_chess_board.py
@@ -1,10 +1,9 @@
 import turtle as t
 from itertools import cycle
 
-CHESS_BOARD_SIZE = 200
-SIZE_SQUARE_AMOUNT = 5
-SQUARE_SIZE = CHESS_BOARD_SIZE / SIZE_SQUARE_AMOUNT
-# ALL_SQUARE_AMOUN = pow(SIZE_SQUARE_AMOUNT, 2)
+CHESS_BOARD_SIZE = 300
+SIZE_SQUARE_AMOUNT = 8
+SQUARE_SIZE = CHESS_BOARD_SIZE // SIZE_SQUARE_AMOUNT
 
 CHESS_COLORS = ['black', 'white']
 
@@ -17,28 +16,27 @@ def shape(side, angle_count=4):
 
 
 def main():
-    chess_board_half_size = CHESS_BOARD_SIZE / 2
+    chess_board_half_size = CHESS_BOARD_SIZE // 2
     start_x, start_y = -chess_board_half_size, chess_board_half_size
-    SQUARE_SIZE = CHESS_BOARD_SIZE / SIZE_SQUARE_AMOUNT
 
+    t.speed(100), t.hideturtle()
 
-    t.speed(10), t.hideturtle()
     x, y = start_x, start_y
+    colors = CHESS_COLORS
 
     for _ in range(SIZE_SQUARE_AMOUNT):
-        for _ in range(SIZE_SQUARE_AMOUNT):
+        for color, _ in zip(cycle(colors), range(SIZE_SQUARE_AMOUNT)):
             t.penup(), t.goto(x, y), t.pendown()
+            t.fillcolor(color)
+            t.begin_fill()
             shape(SQUARE_SIZE)
+            t.end_fill()
             x += SQUARE_SIZE
         x = start_x
         y -= SQUARE_SIZE
-
+        colors = colors[::-1]
 
     input()
-
-
-
-
 
 
 main()
