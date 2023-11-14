158f494fd5ad40f6e199964d6f890da113c1e0cf
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-11-14 14:27:47 +0100

    14_3_9 Chess board (#231)
    * Created 9_chess_board.py
    
    * Added squares drawing
    
    * Added colors for drawing
    
    * Added iterator for odd row colors

---
+++ 14_turtle_module/14_3_part_3/9_chess_board.py
@@ -0,0 +1,42 @@
+import turtle as t
+from itertools import cycle
+
+CHESS_BOARD_SIZE = 300
+SIZE_SQUARE_AMOUNT = 6
+SQUARE_SIZE = CHESS_BOARD_SIZE // SIZE_SQUARE_AMOUNT
+
+CHESS_COLORS = ['black', 'white']
+
+
+def shape(side, angle_count=4):
+    turn_angle = 360 / angle_count
+    for _ in range(angle_count):
+        t.forward(side)
+        t.right(turn_angle)
+
+
+def main():
+    chess_board_half_size = CHESS_BOARD_SIZE // 2
+    start_x, start_y = -chess_board_half_size, chess_board_half_size
+
+    t.speed(100), t.hideturtle()
+
+    x, y = start_x, start_y
+    even_colors = CHESS_COLORS
+    odd_colors = CHESS_COLORS[::-1]
+
+    for colors, _ in zip(cycle([even_colors, odd_colors]), range(SIZE_SQUARE_AMOUNT)):
+        for color, _ in zip(cycle(colors), range(SIZE_SQUARE_AMOUNT)):
+            t.penup(), t.goto(x, y), t.pendown()
+            t.fillcolor(color)
+            t.begin_fill()
+            shape(SQUARE_SIZE)
+            t.end_fill()
+            x += SQUARE_SIZE
+        x = start_x
+        y -= SQUARE_SIZE
+
+    input()
+
+
+main()
