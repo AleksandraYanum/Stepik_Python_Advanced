a61e5368ecb398fdaef47979f9e618a5429eba0e
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-11-14 12:03:25 +0100

    Created 9_chess_board.py

---
+++ 14_turtle_module/14_3_part_3/9_chess_board.py
@@ -0,0 +1,15 @@
+import turtle as t
+from itertools import cycle
+
+CHESS_BOARD_SIZE = 200
+SIZE_SQUARE_AMOUNT = 5
+# ALL_SQUARE_AMOUN = pow(SIZE_SQUARE_AMOUNT, 2)
+
+CHESS_COLORS = ['black', 'white']
+
+
+def main():
+    pass
+
+
+main()
