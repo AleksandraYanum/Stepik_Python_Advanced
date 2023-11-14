9e0194f4951412b5ee2ec40de38b1fad67a0d810
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2023-11-14 11:11:12 +0100

    14_3_12 Stop (#229)
    * Created 12_stop.py
    
    * Fixed 8-polygon drawing
    
    * Fixed text printing
    
    * Replaced 8 with const
    
    * Fixed text alignment

---
+++ 14_turtle_module/14_3_part_3/12_stop.py
@@ -0,0 +1,58 @@
+import turtle as t
+from math import sin, pi, sqrt
+
+EXTERNAL_BORDER_COLOR = 'black'
+INTERNAL_BORDER_COLOR = 'white'
+STOP_COLOR = 'red'
+TEXT_COLOR = 'white'
+COLORS = [EXTERNAL_BORDER_COLOR, INTERNAL_BORDER_COLOR, STOP_COLOR]
+
+STOP_TEXT = 'STOP'
+
+STOP_SIDE_LEN = 150
+STOP_SIZE_DIF = 10
+STOP_SIGH_ANGLE_AMOUNT = 8
+
+
+def shape(side, angle_count=6):
+    turn_angle = 360 / angle_count
+    for _ in range(angle_count):
+        t.forward(side)
+        t.right(turn_angle)
+
+
+def main():
+
+    t.speed(10), t.hideturtle(), t.up()
+    start_x, start_y = 0, 0
+
+    side_len = STOP_SIDE_LEN
+    for color in range(len(COLORS)):
+        radius = int(side_len / (2 * sin(180 / STOP_SIGH_ANGLE_AMOUNT * pi / 180)))
+        mid_height = sqrt(pow(radius, 2) - pow(side_len / 2, 2))
+
+        t.up(), t.goto(start_x - side_len // 2, start_y + mid_height), t.down()
+        t.pencolor(COLORS[color])
+        
+        t.fillcolor(COLORS[color])
+        t.begin_fill()
+        shape(side_len, STOP_SIGH_ANGLE_AMOUNT)
+        t.end_fill() 
+
+        side_len -= STOP_SIZE_DIF
+      
+
+    side_len += STOP_SIZE_DIF
+
+    t.up()
+    text_size = int(mid_height * 2 / len(STOP_TEXT))
+    t.goto(start_x, start_y - mid_height // 3)
+
+    t.pencolor(TEXT_COLOR)
+    t.write(STOP_TEXT, align='center', font=('Arial', text_size, 'normal'))
+
+    input()
+
+
+main()
+
