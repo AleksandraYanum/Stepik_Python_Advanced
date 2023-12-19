4acca2c291d4e3640fe254abb28c38bd4393b5b0
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-12-19 09:59:44 +0100

    Created 14_3_11_solar_system.py

---
+++ 14_turtle_module/14_3_part_3/14_3_11_solar_system.py
@@ -0,0 +1,18 @@
+SOLAR_SYSTEM_OBJECTS = {'sun': {'relative_radius': 1, 'color': 'yellow', 'text': 'Солнце'},
+                        'mercury': {'relative_radius': 0.3, 'color': 'peru', 'text': 'Меркурий'},
+                        'venus': {'relative_radius': 0.4, 'color': 'peru', 'text': 'Венера'},
+                        'earth': {'relative_radius': 0.3, 'color': 'spring green', 'text': 'Земля'},
+                        'mars': {'relative_radius': 0.2, 'color': 'dark orange', 'text': 'Марс'},
+                        'jupiter': {'relative_radius': 0.6, 'color': 'peru', 'text': 'Юпитер'},
+                        'saturn': {'relative_radius': 0.6, 'color': 'peru', 'is_ring': True, 'text': 'Сатурн'},
+                        'uranus': {'relative_radius': 0.5, 'color': 'sky blue', 'text': 'Уран'},
+                        'neptune': {'relative_radius': 0.5, 'color': 'blue', 'text': 'Нептун'},
+                        'pluto': {'relative_radius': 0.1, 'color': 'peru', 'text': 'Плутон'}
+                        }
+
+DISTANCE = 50
+
+def main():
+    pass
+
+main() 
