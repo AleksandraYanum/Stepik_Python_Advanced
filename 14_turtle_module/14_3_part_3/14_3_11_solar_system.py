2ce6b97b8a0fb63cd049f4616f01c2865b2f8206
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-12-19 11:26:12 +0100

    Added initial version of drawing

--- 14_turtle_module/14_3_part_3/14_3_11_solar_system.py
+++ 14_turtle_module/14_3_part_3/14_3_11_solar_system.py
@@ -1,3 +1,6 @@
+import turtle as t
+
+
 SOLAR_SYSTEM_OBJECTS = {'sun': {'relative_radius': 1, 'color': 'yellow', 'text': 'Солнце'},
                         'mercury': {'relative_radius': 0.3, 'color': 'peru', 'text': 'Меркурий'},
                         'venus': {'relative_radius': 0.4, 'color': 'peru', 'text': 'Венера'},
@@ -10,9 +13,43 @@ SOLAR_SYSTEM_OBJECTS = {'sun': {'relative_radius': 1, 'color': 'yellow', 'text':
                         'pluto': {'relative_radius': 0.1, 'color': 'peru', 'text': 'Плутон'}
                         }
 
-DISTANCE = 50
+PLANET_DISTANCE = 30
+TEXT_DISTANCE = 20
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
 
 def main():
-    pass
+    t.Screen().colormode(255)
+    t.Screen().setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+    t.hideturtle(), t.penup()
+
+    sun_radius = 100
+
+    # left central coords
+    x = -SCREEN_WIDTH // 2 + PLANET_DISTANCE
+    y = 0
+
+
+    for planet_info in SOLAR_SYSTEM_OBJECTS.values():
+        planet_radius = sun_radius * planet_info['relative_radius']
+        planet_color = planet_info['color']
+        planet_name = planet_info['text']
+
+        start_x = x + planet_radius
+        start_y = y - planet_radius
+
+        t.goto(start_x, start_y)
+        t.pendown()
+        t.fillcolor(planet_color)
+        t.begin_fill()
+        t.circle(planet_radius)
+        t.end_fill()
+        t.penup()
+        t.goto(start_x, start_y - TEXT_DISTANCE)
+        t.write(planet_name, align='center', font=('Arial', 10, 'normal'))
+        x = start_x + planet_radius + PLANET_DISTANCE
+
+
+    input()
 
 main() 
