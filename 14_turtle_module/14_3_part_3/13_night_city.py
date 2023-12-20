be098bd56263af9283e56b731f9edb16492f8235
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-12-20 13:00:06 +0100

    Added initial house drawing

--- 14_turtle_module/14_3_part_3/13_night_city.py
+++ 14_turtle_module/14_3_part_3/13_night_city.py
@@ -1,10 +1,12 @@
 import turtle as t
+from random import randint, randrange
+from itertools import cycle
 
 #*******************************************************************************************************************
 
 SKY_COLOR = 'midnight blue'
 STAR_COLOR = 'gold'
-HOUSE_COLOR = 'blue'
+HOUSE_COLOR = 'RoyalBlue4'
 LIGHT_WINDOW_COLOR = 'gold'
 NO_LIGHT_WINDOW_COLOR = 'medium blue'
 
@@ -18,11 +20,12 @@ ALL_ONE_WINDOW_SPACE = WINDOW_SIZE * 2
 SCREEN_WIDTH = 30 * ALL_ONE_WINDOW_SPACE
 SCREEN_HEIGHT = 20 * ALL_ONE_WINDOW_SPACE
 
+MIN_HOUSE_WIDTH = 1 * ALL_ONE_WINDOW_SPACE
 MIN_HOUSE_HEIGHT = 1 * ALL_ONE_WINDOW_SPACE
 MAX_HOUSE_HEIGHT = SCREEN_HEIGHT - ALL_ONE_WINDOW_SPACE
 
 MIN_HOUSE_AMOUNT = 3
-MAX_HOUSE_AMOUNT = 10
+MAX_HOUSE_AMOUNT = 15
 
 #*******************************************************************************************************************
 
@@ -31,9 +34,47 @@ def main():
 
     t.Screen().colormode(255)
     t.Screen().bgcolor(SKY_COLOR)
-    t.hideturtle()
+    t.hideturtle(), t.tracer(0)
     
+    start_x = - SCREEN_WIDTH // 2
+    start_y = - SCREEN_HEIGHT // 2
+
+    house_amount = randint(MIN_HOUSE_AMOUNT, MAX_HOUSE_AMOUNT)
+    house_height = 600
+    drawn_house_width = 0
+
+
+    for i in range(1, house_amount):
+        max_house_width = SCREEN_WIDTH - (house_amount - i)
+
+        t.penup()
+        t.goto(start_x, start_y)
+        t.pendown()
+
+        house_width = randrange(MIN_HOUSE_WIDTH, max_house_width, ALL_ONE_WINDOW_SPACE)
+        rectangle(house_width, house_height, fill_color=HOUSE_COLOR)
+
+        drawn_house_width += house_width
+        start_x = start_x + house_width
+
+    t.penup()
+    t.goto(start_x, start_y)
+    t.pendown()
+    
+    last_house_width = SCREEN_WIDTH - drawn_house_width
+    rectangle(last_house_width, house_height, fill_color=HOUSE_COLOR)
+
+
     input()
+
+
+def rectangle(width, height, *, border_color='black', fill_color=''):
+    t.color(border_color, fill_color)
+    t.begin_fill()
+    for step, _ in zip(cycle([width, height]), range(4)):
+        t.forward(step)
+        t.left(90)
+    t.end_fill()
 
 
 main()
