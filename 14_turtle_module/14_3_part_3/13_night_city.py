379382d071bbadc1acf5b0a5525a6c576aacd07d
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2023-12-21 23:27:01 +0100

    Extracted buildings, windows drawing funcs

--- 14_turtle_module/14_3_part_3/13_night_city.py
+++ 14_turtle_module/14_3_part_3/13_night_city.py
@@ -6,7 +6,7 @@ from itertools import cycle
 
 SKY_COLOR = 'midnight blue'
 STAR_COLOR = 'gold'
-HOUSE_COLOR = 'RoyalBlue4'
+BUILDING_COLOR = 'RoyalBlue4'
 LIGHT_WINDOW_COLOR = 'gold'
 NO_LIGHT_WINDOW_COLOR = 'medium blue'
 
@@ -20,12 +20,12 @@ ALL_ONE_WINDOW_SPACE = WINDOW_SIZE + WINDOW_DISTANCE * 2
 SCREEN_WIDTH = 15 * ALL_ONE_WINDOW_SPACE
 SCREEN_HEIGHT = 10 * ALL_ONE_WINDOW_SPACE
 
-MIN_HOUSE_WIDTH = 1 * ALL_ONE_WINDOW_SPACE
-MIN_HOUSE_HEIGHT = 1 * ALL_ONE_WINDOW_SPACE
-MAX_HOUSE_HEIGHT = SCREEN_HEIGHT - ALL_ONE_WINDOW_SPACE
+MIN_BUILDING_WIDTH = 1 * ALL_ONE_WINDOW_SPACE
+MIN_BUILDING_HEIGHT = 1 * ALL_ONE_WINDOW_SPACE
+MAX_BUILDING_HEIGHT = SCREEN_HEIGHT - ALL_ONE_WINDOW_SPACE
 
-MIN_HOUSE_AMOUNT = 5
-MAX_HOUSE_AMOUNT = 15
+MIN_BUILDING_AMOUNT = 5
+MAX_BUILDING_AMOUNT = 15
 
 #*******************************************************************************************************************
 
@@ -36,61 +36,77 @@ def main():
     t.Screen().bgcolor(SKY_COLOR)
     t.hideturtle(), t.tracer(0)
     
-    house_start_x = - SCREEN_WIDTH // 2
-    house_start_y = - SCREEN_HEIGHT // 2 
+    building_start_x = - SCREEN_WIDTH // 2
+    building_start_y = - SCREEN_HEIGHT // 2 
 
-    house_amount = 5
-    # randint(MIN_HOUSE_AMOUNT, MAX_HOUSE_AMOUNT)
-    drawn_house_width = 0
+    building_amount = 5
+    # randint(MIN_BUILDING_AMOUNT, MAX_BUILDING_AMOUNT)
 
-    for i in range(house_amount - 1):
-        max_curr_house_width = SCREEN_WIDTH - drawn_house_width - (house_amount - i - 1) * MIN_HOUSE_WIDTH
-        curr_house_width = randrange(MIN_HOUSE_WIDTH, max_curr_house_width + 1, ALL_ONE_WINDOW_SPACE)
-        curr_house_height = randrange(MIN_HOUSE_HEIGHT, MAX_HOUSE_HEIGHT, ALL_ONE_WINDOW_SPACE)
+    drawn_building_width = 0
 
-        t.penup()
-        t.goto(house_start_x, house_start_y)
-        t.pendown()
-        rectangle(curr_house_width, curr_house_height, HOUSE_COLOR)
+# BUILDINGS DRAWING
+    for i in range(building_amount - 1):
+        max_curr_building_width = SCREEN_WIDTH - drawn_building_width - (building_amount - i - 1) * MIN_BUILDING_WIDTH
 
-#*******************************************************************************************************************
+        curr_building_width, curr_building_height = \
+            draw_random_building(building_start_x, building_start_y, MIN_BUILDING_WIDTH, max_curr_building_width, ALL_ONE_WINDOW_SPACE,
+                                 MIN_BUILDING_HEIGHT, MAX_BUILDING_HEIGHT, ALL_ONE_WINDOW_SPACE, BUILDING_COLOR)
 
-        window_amount_in_height = curr_house_height // ALL_ONE_WINDOW_SPACE
-        window_amount_in_width = curr_house_width // ALL_ONE_WINDOW_SPACE
+# WINDOWS DRAWING
+        window_amount_in_height = curr_building_height // ALL_ONE_WINDOW_SPACE
+        window_amount_in_width = curr_building_width // ALL_ONE_WINDOW_SPACE
 
-        window_start_x = house_start_x + WINDOW_DISTANCE
-        curr_window_start_y = house_start_y + WINDOW_DISTANCE
+        window_start_x = building_start_x + WINDOW_DISTANCE
+        curr_window_start_y = building_start_y + WINDOW_DISTANCE
 
+        window_possible_colors = [LIGHT_WINDOW_COLOR, NO_LIGHT_WINDOW_COLOR]
 
-        for _ in range(window_amount_in_height):
-            curr_window_start_x = window_start_x
+        draw_building_windows(window_start_x, curr_window_start_y, window_amount_in_height, window_amount_in_width,
+                              WINDOW_SIZE, WINDOW_DISTANCE, window_possible_colors)
 
-            for _ in range(window_amount_in_width):
-                t.penup()
-                t.goto(curr_window_start_x, curr_window_start_y)
-                t.pendown()
+        drawn_building_width += curr_building_width
+        building_start_x += curr_building_width
 
-                window_color = choice([LIGHT_WINDOW_COLOR, NO_LIGHT_WINDOW_COLOR])
-                square(WINDOW_SIZE, window_color)
-                curr_window_start_x += WINDOW_SIZE + WINDOW_DISTANCE * 2
-            
-            curr_window_start_y += WINDOW_SIZE + WINDOW_DISTANCE * 2
-
-        drawn_house_width += curr_house_width
-        house_start_x += curr_house_width
-
-    t.penup()
-    t.goto(house_start_x, house_start_y)
-    t.pendown()
-    
-    curr_house_width = SCREEN_WIDTH - drawn_house_width
-    curr_house_height = randrange(MIN_HOUSE_HEIGHT, MAX_HOUSE_HEIGHT, ALL_ONE_WINDOW_SPACE)
-
-    rectangle(curr_house_width, curr_house_height, fill_color=HOUSE_COLOR)
-
+# LAST BUILDING DRAWING
+    curr_building_width = SCREEN_WIDTH - drawn_building_width
+    curr_building_height = randrange(MIN_BUILDING_HEIGHT, MAX_BUILDING_HEIGHT, ALL_ONE_WINDOW_SPACE)
+    draw_building(building_start_x, building_start_y, curr_building_width, curr_building_height, BUILDING_COLOR)
 
 
     input()
+
+
+def draw_building_windows(start_x, start_y, amount_in_height, amount_in_width, size, distance, possible_colors):
+
+    curr_start_y = start_y
+    for _ in range(amount_in_height):
+            curr_start_x = start_x
+
+            for _ in range(amount_in_width):
+                t.penup()
+                t.goto(curr_start_x, curr_start_y)
+                t.pendown()
+                color = choice(possible_colors)
+                square(size, color)
+                curr_start_x += size + distance * 2
+
+            curr_start_y += size + distance * 2
+
+
+def draw_random_building(start_x, start_y, min_width, max_width, step_width, min_height, max_height, step_height, color):
+
+    random_width = randrange(min_width, max_width + 1, step_width)
+    random_height = randrange(min_height, max_height, step_height)
+    draw_building(start_x, start_y, random_width, random_height, color)
+
+    return random_width, random_height
+
+
+def draw_building(start_x, start_y, width, height, color):
+    t.penup()
+    t.goto(start_x, start_y)
+    t.pendown()
+    rectangle(width, height, color)
 
 
 def rectangle(width, height, fill_color='black'):
