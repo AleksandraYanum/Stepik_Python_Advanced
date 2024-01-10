674d1e269bcae6215fdf8c01b1eebc13ea014cd8
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-01-10 12:56:46 +0100

    Added initial stars drawing

--- 14_turtle_module/14_3_part_3/13_night_city.py
+++ 14_turtle_module/14_3_part_3/13_night_city.py
@@ -13,9 +13,9 @@ BUILDING_POSSIBLE_COLORS = ['RoyalBlue4', 'SlateBlue4', 'SlateGray', 'DarkOliveG
 LIGHT_WINDOW_COLOR = 'gold'
 NO_LIGHT_WINDOW_COLOR = 'medium blue'
 
-MIN_STAR_DIAMETER = 2
-MAX_STAR_DIAMETER = 7
-STAR_AMOUNT = 20
+MIN_STAR_DIAMETER = 4
+MAX_STAR_DIAMETER = 8
+STAR_AMOUNT = 100
 
 WINDOW_SIZE = 30
 WINDOW_DISTANCE = WINDOW_SIZE // 2
@@ -40,8 +40,8 @@ def main():
     t.Screen().bgcolor(SKY_COLOR)
     t.hideturtle(), t.tracer(0)
     
-    curr_building_start_x = - SCREEN_WIDTH // 2
-    curr_building_start_y = - SCREEN_HEIGHT // 2 
+    building_start_x = - SCREEN_WIDTH // 2
+    building_start_y = - SCREEN_HEIGHT // 2 
 
     # building_amount = 5
     building_amount = randint(MIN_BUILDING_AMOUNT, MAX_BUILDING_AMOUNT)
@@ -50,6 +50,10 @@ def main():
     window_possible_colors = [LIGHT_WINDOW_COLOR, NO_LIGHT_WINDOW_COLOR]
     building_possible_colors = cycle(BUILDING_POSSIBLE_COLORS)
     drawn_building_info = []      # upper x1, x2 y coords
+    curr_building_start_x, curr_building_start_y = building_start_x, building_start_y
+
+# STARS DRAWING
+    draw_random_stars(STAR_AMOUNT, MIN_STAR_DIAMETER, MAX_STAR_DIAMETER, STAR_COLOR, building_start_x, -building_start_x, building_start_y, -building_start_y)
 
 # BUILDINGS DRAWING
     for i in range(building_amount - 1):
@@ -92,8 +96,50 @@ def main():
     draw_building_windows(window_start_x, curr_window_start_y, window_amount_in_height, window_amount_in_width,
                               WINDOW_SIZE, WINDOW_DISTANCE, window_possible_colors)
 
+
+    # draw_random_stars_1(STAR_AMOUNT, MIN_STAR_DIAMETER, MAX_STAR_DIAMETER, STAR_COLOR, building_start_x, -building_start_x,
+    #                   building_start_y, -building_start_y, drawn_building_info)
+
     input()
 
+
+def draw_random_stars(amount, min_diameter, max_diameter, color, left_x_border, right_x_border, lower_y_border, upper_y_border):
+    for _ in range(amount):
+        random_diameter = randint(min_diameter, max_diameter)
+        x, y = generate_random_within_screen_coords(left_x_border, right_x_border, lower_y_border, upper_y_border)
+        t.up()
+        t.goto(x, y)
+        t.dot(random_diameter, color)
+
+def draw_random_stars_1(amount, min_diameter, max_diameter, color, left_x_border, right_x_border, lower_y_border, upper_y_border,  unavailable_coords):
+
+    for _ in range(amount):
+        random_diameter = randint(min_diameter, max_diameter)
+        x, y = generate_random_within_screen_coords(left_x_border, right_x_border, lower_y_border, upper_y_border)
+        result = is_not_overlapping_building(x, y, random_diameter, unavailable_coords)
+        while result != True:
+            x, y = generate_random_within_screen_coords(left_x_border, right_x_border, lower_y_border, upper_y_border)
+            result = is_not_overlapping_building(x, y, random_diameter, unavailable_coords)
+        t.up()
+        t.goto(x, y)
+        t.dot(random_diameter, color)
+
+
+
+def generate_random_within_screen_coords(left_x_border, right_x_border, lower_y_border, upper_y_border):
+    x = randint(left_x_border, right_x_border)
+    y = randint(lower_y_border, upper_y_border)
+    return x, y
+
+
+def is_not_overlapping_building(x_dot, y_dot, dot_diameter, unavailable_coords):
+    dot_radius = dot_diameter // 2
+    for coords in unavailable_coords:
+        left_x, right_x, upper_y = coords
+        result = ((x_dot < left_x - dot_radius) or (x_dot > right_x + dot_radius)) and (y_dot > upper_y + dot_radius)
+        if not result:
+            break
+    return result
 
 def draw_building_windows(start_x, start_y, amount_in_height, amount_in_width, size, distance, possible_colors):
 
