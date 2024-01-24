541f2dd6e568eea924275a9e30e6c61d8230f27b
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-01-24 14:06:31 +0100

    Extracted get_polygon_info_by_side_amount()

--- 14_turtle_module/14_3_part_3/8_regular_polygons.py
+++ 14_turtle_module/14_3_part_3/8_regular_polygons.py
@@ -12,11 +12,13 @@ POLYGON_AMOUNT_IN_ROW = 5
 POLYGON_AMOUNT_IN_COL = 5
 POLYGON_MIN_POSSIBLE_SIDE_AMOUNT = 3
 POLYGON_MAX_POSSIBLE_SIDE_AMOUNT = 7
+POLYGON_HEIGHT = 70
 
 
-def draw_polygon(start_x, start_y, side_amount, side, color):
+def draw_polygon(start_x, start_y, start_turn_angle, side_amount, side, color):
     turn_angle = 360 / side_amount
     penup() 
+    setheading(start_turn_angle)
     setposition(start_x, start_y)
     fillcolor(color)
     pendown()
@@ -27,12 +29,30 @@ def draw_polygon(start_x, start_y, side_amount, side, color):
     end_fill()
 
 
+def get_polygon_info_by_side_amount(center_x, center_y, side_amount, height):
+    # For polygons with an odd number of sides (3, 5, 7), the starting point is the center of the bottom border
+    # For polygons with an even number of sides (4, 6) - the leftmost point lying on the upper boundary
+    
+    turn_angle = 360 / side_amount
+
+    if side_amount % 2 == 1:
+        сircumcircle_radius = height / (cos(pi / side_amount) + 1) # радиус описанной окружности
+        side = 2 * сircumcircle_radius * sin(pi / side_amount) 
+        start_turn_angle = 180 - turn_angle / 2
+        start_x, start_y = center_x, center_y - height / 2  # drawing sratrting coordinate
+    else:
+        side = 2 * (height / 2) * tan(pi / side_amount)
+        start_turn_angle = 0
+        start_x, start_y = center_x - side / 2, center_y + height / 2
+
+    return start_x, start_y, side, start_turn_angle
+
+
 def main():
     Screen().setup(SCREEN_WIDTH, SCREEN_HEIGHT)
     speed(0) 
 
-    polygon_height = 70
-    polygon_center_distance = polygon_height + POLYGON_DISTANCE
+    polygon_center_distance = POLYGON_HEIGHT + POLYGON_DISTANCE
     first_polygon_center_x = - (SCREEN_WIDTH // 2 - BORDER_DISTANCE)  #center
     first_polygon_center_y = SCREEN_HEIGHT // 2 - BORDER_DISTANCE   #center
 
@@ -41,28 +61,13 @@ def main():
         for polygon_in_col in range(POLYGON_AMOUNT_IN_COL): 
             curr_polygon_side_amount = randint(POLYGON_MIN_POSSIBLE_SIDE_AMOUNT, POLYGON_MAX_POSSIBLE_SIDE_AMOUNT)
             curr_polygon_color = POLYGON_POSSIBLE_COLORS[polygon_in_col]
-
             curr_polygon_center_x = first_polygon_center_x + polygon_center_distance * polygon_in_col 
             curr_polygon_center_y = first_polygon_center_y - polygon_center_distance * polygon_in_row
-            curr_polygon_turn_angle = 360 / curr_polygon_side_amount
             
-
-            # For polygons with an odd number of sides (3, 5, 7), the starting point is the center of the bottom border
-            # For polygons with an even number of sides (4, 6) - the leftmost point lying on the upper boundary
-
-            if curr_polygon_side_amount % 2 == 1:
-                curr_сircumcircle_radius = polygon_height / (cos(pi / curr_polygon_side_amount) + 1) # радиус описанной окружности
-                curr_polygon_side = 2 * curr_сircumcircle_radius * sin(pi / curr_polygon_side_amount) 
-                curr_start_x, curr_start_y = curr_polygon_center_x, curr_polygon_center_y - polygon_height / 2  # drawing sratrting coordinate
-                curr_start_turn_angle = 180 - curr_polygon_turn_angle / 2
-                setheading(curr_start_turn_angle)
-            else:
-                curr_polygon_side = 2 * (polygon_height / 2) * tan(pi / curr_polygon_side_amount)
-                curr_start_x, curr_start_y = curr_polygon_center_x - curr_polygon_side / 2, curr_polygon_center_y + polygon_height / 2
-                setheading(0)
-            
-            # DRAWING 
-            draw_polygon(curr_start_x, curr_start_y, curr_polygon_side_amount, curr_polygon_side, curr_polygon_color)
+            curr_start_x,  curr_start_y, curr_polygon_side, curr_start_turn_angle = \
+            get_polygon_info_by_side_amount(curr_polygon_center_x, curr_polygon_center_y, curr_polygon_side_amount, POLYGON_HEIGHT)
+                       
+            draw_polygon(curr_start_x, curr_start_y, curr_start_turn_angle, curr_polygon_side_amount, curr_polygon_side, curr_polygon_color)
 
     hideturtle()
     input()
