8ce1126002686b8498b8abb32c7c00aac1256fea
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-01-26 12:21:52 +0100

    Moved drawing calc to draw_polygon()

--- 14_turtle_module/14_3_part_3/8_regular_polygons.py
+++ 14_turtle_module/14_3_part_3/8_regular_polygons.py
@@ -19,8 +19,17 @@ POLYGON_MIN_POSSIBLE_SIDE_AMOUNT = 3
 POLYGON_MAX_POSSIBLE_SIDE_AMOUNT = 7
 
 
-def draw_polygon(start_x, start_y, start_turn_angle, side_amount, side, color):
+def draw_polygon(center_x, center_y, side_amount, side, color):
     turn_angle = 360 / side_amount
+    excircle_radius = side / (2 * sin(pi / side_amount))
+    height = excircle_radius * (cos(pi / side_amount) + 1)
+    if side_amount % 2 == 1:
+        start_turn_angle = 180 - turn_angle / 2
+        start_x, start_y = center_x, center_y - height / 2  
+    else:
+        start_turn_angle = 0
+        start_x, start_y = center_x - side / 2, center_y + height / 2
+
     penup() 
     setheading(start_turn_angle)
     setposition(start_x, start_y)
@@ -32,35 +41,17 @@ def draw_polygon(start_x, start_y, start_turn_angle, side_amount, side, color):
         right(turn_angle)
     end_fill()
 
-
-def calculate_polygon_dimensions_by_height(center_x, center_y, side_amount, height):
-
-    # For polygons with an odd number of sides (3, 5, 7), the starting point is the center of the bottom border
-    # For polygons with an even number of sides (4, 6) - the leftmost point lying on the upper boundary
-    
-    turn_angle = 360 / side_amount
-    if side_amount % 2 == 1:
-        excircle_radius = height / (cos(pi / side_amount) + 1) # радиус описанной окружности
-        side = 2 * excircle_radius * sin(pi / side_amount) 
-        start_turn_angle = 180 - turn_angle / 2
-        start_x, start_y = center_x, center_y - height / 2  # drawing sratrting coordinate
-    else:
-        side = 2 * (height / 2) * tan(pi / side_amount)
-        start_turn_angle = 0
-        start_x, start_y = center_x - side / 2, center_y + height / 2
-
-    return start_x, start_y, side, start_turn_angle
-
   
 def draw_random_polygon(center_x, center_y, height, min_possible_side_amount, max_possible_side_amount, color):
     side_amount = randint(min_possible_side_amount, max_possible_side_amount)
-    start_x, start_y, side, start_turn_angle = calculate_polygon_dimensions_by_height(center_x, center_y, side_amount, height)
-    draw_polygon(start_x=start_x, 
-                 start_y=start_y, 
-                 start_turn_angle=start_turn_angle, 
-                 side_amount=side_amount, 
-                 side=side, 
-                 color=color)
+    
+    if side_amount % 2 == 1:
+        excircle_radius = height / (cos(pi / side_amount) + 1) # радиус описанной окружности
+        side = 2 * excircle_radius * sin(pi / side_amount) 
+    else:
+        side = 2 * (height / 2) * tan(pi / side_amount)
+
+    draw_polygon(center_x, center_y, side_amount, side, color)
 
     return side
 
@@ -73,7 +64,7 @@ def main():
     first_polygon_center_y = SCREEN_HEIGHT // 2 - BORDER_DISTANCE   #center
     curr_center_x, curr_center_y = first_polygon_center_x, first_polygon_center_y
    
-    for _  in range(POLYGON_AMOUNT_IN_ROW):
+    for _ in range(POLYGON_AMOUNT_IN_ROW):
         shuffle(POLYGON_POSSIBLE_COLORS)
         for polygon_in_row in range(POLYGON_AMOUNT_IN_COL): 
             draw_random_polygon(center_x=curr_center_x, 
