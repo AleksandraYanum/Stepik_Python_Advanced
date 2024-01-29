12ac9f0194e6fee78a0c8c892cb47fdf23121130
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2024-01-29 16:34:18 +0100

    14_3_8 Regular polygons (#255)
    * Created 8_regular_polygons.py
    
    * Implemented initial working version
    
    * Renamed var
    
    * Extracted draw_polygon()
    
    * Extracted get_polygon_info_by_side_amount()
    
    * Added named parameters to funcs
    
    * Extracted var to const
    
    * Moved logic of calculating coords to drawing func
    
    * Changed logic for not repeating colors in a row
    
    * calculate_polygon_dimensions -> calculate_polygon_dimensions_by_height
    
    * сircumcircle_radius -> excircle_radius
    
    * Moved drawing calc to draw_polygon()
    
    * Added get_start_drawing_coords_angle()
    
    * Added height calc to get_start_drawing_coords_angle()
    
    * Added draw_polygon_by_height()
    
    * Changed params order
    
    * draw_polygon() -> draw_polygon_by_side()
    
    * calculate_polygon_dimensions() -> get_drawing_init_values()
    
    * Moved func description to a high level
    
    * Changed return params order
    
    * circumcircle_radius -> excircle_radius
    
    * Set init pos restore
    
    * Deleted empty lines
    
    * polygon_in_row -> polygon_idx
    
    * Reverted shuffle to choice
    
    * Added draw mode consts
    
    * draw_random_polygon() -> draw_random_polygon_by_height()
    
    * Added draw_random_polygon_by_area()
    
    * Added drawing modesprocessing
    
    * Added draw_random_polygon_matrix_by_area(), draw_random_polygon_matrix_by_height()
    
    * Refactoring

---
+++ 14_turtle_module/14_3_part_3/8_regular_polygons.py
@@ -0,0 +1,177 @@
+from turtle import *
+from random import choice, randint
+from math import sin, cos, tan, pi, sqrt, radians
+
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+
+POLYGON_HEIGHT = 70
+POLYGON_AREA = 5000
+
+
+POLYGON_POSSIBLE_COLORS = ['yellow', 'lightblue', 'violet', 'orange', 'red', 'blue', 'green']
+
+BORDER_DISTANCE = 50
+POLYGON_DISTANCE = 50
+POLYGON_CENTER_DISTANCE = POLYGON_HEIGHT + POLYGON_DISTANCE
+POLYGON_AMOUNT_IN_ROW = 5
+POLYGON_AMOUNT_IN_COL = 5
+POLYGON_MIN_POSSIBLE_SIDE_AMOUNT = 3
+POLYGON_MAX_POSSIBLE_SIDE_AMOUNT = 7
+
+EQUAL_HEIGHT = 1
+EQUAL_AREA = 2
+# Choose one of the options above
+DRAW_MODE = EQUAL_HEIGHT
+
+
+def get_start_drawing_coords_angle(center_x, center_y, side_amount, side):
+    turn_angle = 360 / side_amount
+    excircle_radius = side / (2 * sin(pi / side_amount))
+    if side_amount % 2 == 1:
+        height = excircle_radius * (cos(pi / side_amount) + 1)
+        start_turn_angle = 180 - turn_angle / 2
+        start_x, start_y = center_x, center_y - height / 2  
+    else:
+        height = 2 * (side / (2* tan(pi / side_amount)))
+        start_turn_angle = 0
+        start_x, start_y = center_x - side / 2, center_y + height / 2
+    
+    return start_x, start_y, start_turn_angle
+
+
+# For polygons with an odd number of sides (3, 5, 7), the starting point is the center of the bottom border
+# For polygons with an even number of sides (4, 6) - the leftmost point lying on the upper boundary
+
+def get_drawing_init_values(center_x, center_y, height, side_amount):
+   
+    turn_angle = 360 / side_amount
+    if side_amount % 2 == 1:
+        excircle_radius = height / (cos(pi / side_amount) + 1) # радиус описанной окружности
+        side = 2 * excircle_radius * sin(pi / side_amount) 
+        start_turn_angle = 180 - turn_angle / 2
+        start_x, start_y = center_x, center_y - height / 2  # drawing starting coordinate
+    else:
+        side = 2 * (height / 2) * tan(pi / side_amount)
+        start_turn_angle = 0
+        start_x, start_y = center_x - side / 2, center_y + height / 2
+
+    return start_x, start_y, start_turn_angle, side
+
+
+def draw_polygon_by_side(center_x, center_y, side, side_amount, color):
+    turn_angle = 360 / side_amount
+    start_x, start_y, start_turn_angle = get_start_drawing_coords_angle(center_x, center_y, side_amount, side)
+
+    penup() 
+    setheading(start_turn_angle)
+    setposition(start_x, start_y)
+    fillcolor(color)
+    pendown()
+    begin_fill()
+    for _ in range(side_amount): 
+        forward(side)
+        right(turn_angle)
+    end_fill()
+
+
+def draw_polygon_by_height(center_x, center_y, height, side_amount, color):
+    turn_angle = 360 / side_amount
+    start_x, start_y, start_turn_angle, side = get_drawing_init_values(center_x, center_y, height, side_amount)
+
+    init_x, init_y = pos()
+    penup() 
+    setheading(start_turn_angle)
+    setposition(start_x, start_y)
+    fillcolor(color)
+    pendown()
+    begin_fill()
+    for _ in range(side_amount): 
+        forward(side)
+        right(turn_angle)
+    end_fill()
+    penup()
+    setposition(init_x, init_y) 
+
+
+def draw_random_polygon_by_height(center_x, center_y, height, min_possible_side_amount, max_possible_side_amount, color):
+    side_amount = randint(min_possible_side_amount, max_possible_side_amount)
+    
+    draw_polygon_by_height(center_x, center_y, height, side_amount, color)
+
+
+def draw_random_polygon_by_area(center_x, center_y, area, min_possible_side_amount, max_possible_side_amount, color):
+    side_amount = randint(min_possible_side_amount, max_possible_side_amount)
+    side = sqrt(area * 4 * tan(radians(180) / side_amount) / side_amount)
+
+    draw_polygon_by_side(center_x, center_y, side, side_amount, color)
+
+
+def draw_random_polygon_matrix_by_area(first_center_x, first_center_y, amount_in_row, amount_in_col, area, min_possible_side_amount, 
+                                       max_possible_side_amount, center_distance, colors):
+    curr_center_x, curr_center_y = first_center_x, first_center_y
+
+    for _ in range(amount_in_row):
+        for _ in range(amount_in_col): 
+                draw_random_polygon_by_area(center_x=curr_center_x, 
+                                            center_y=curr_center_y, 
+                                            area=area, 
+                                            min_possible_side_amount=min_possible_side_amount, 
+                                            max_possible_side_amount=max_possible_side_amount, 
+                                            color=choice(colors))  
+                curr_center_x += center_distance
+        curr_center_x = first_center_x
+        curr_center_y -= center_distance
+
+
+def draw_random_polygon_matrix_by_height(first_center_x, first_center_y, amount_in_row, amount_in_col, height, min_possible_side_amount, 
+                                         max_possible_side_amount, center_distance, colors):
+    curr_center_x, curr_center_y = first_center_x, first_center_y
+
+    for _ in range(amount_in_row):
+        for _ in range(amount_in_col): 
+                draw_random_polygon_by_height(center_x=curr_center_x, 
+                                            center_y=curr_center_y, 
+                                            height=height, 
+                                            min_possible_side_amount=min_possible_side_amount, 
+                                            max_possible_side_amount=max_possible_side_amount, 
+                                            color=choice(colors))  
+                curr_center_x += center_distance
+        curr_center_x = first_center_x
+        curr_center_y -= center_distance
+
+
+def main():
+    Screen().setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+    speed(0) 
+
+    first_polygon_center_x = - (SCREEN_WIDTH // 2 - BORDER_DISTANCE)  #center
+    first_polygon_center_y = SCREEN_HEIGHT // 2 - BORDER_DISTANCE   #center
+  
+    if DRAW_MODE == EQUAL_AREA:
+        draw_random_polygon_matrix_by_area(first_center_x=first_polygon_center_x,
+                                           first_center_y=first_polygon_center_y,
+                                           amount_in_row=POLYGON_AMOUNT_IN_ROW,
+                                           amount_in_col=POLYGON_AMOUNT_IN_COL,
+                                           area=POLYGON_AREA,
+                                           min_possible_side_amount=POLYGON_MIN_POSSIBLE_SIDE_AMOUNT, 
+                                           max_possible_side_amount=POLYGON_MAX_POSSIBLE_SIDE_AMOUNT, 
+                                           center_distance=POLYGON_CENTER_DISTANCE,
+                                           colors=POLYGON_POSSIBLE_COLORS)
+    elif DRAW_MODE == EQUAL_HEIGHT:
+        draw_random_polygon_matrix_by_height(first_center_x=first_polygon_center_x,
+                                             first_center_y=first_polygon_center_y,
+                                             amount_in_row=POLYGON_AMOUNT_IN_ROW,
+                                             amount_in_col=POLYGON_AMOUNT_IN_COL,
+                                             height=POLYGON_HEIGHT,
+                                             min_possible_side_amount=POLYGON_MIN_POSSIBLE_SIDE_AMOUNT, 
+                                             max_possible_side_amount=POLYGON_MAX_POSSIBLE_SIDE_AMOUNT, 
+                                             center_distance=POLYGON_CENTER_DISTANCE,
+                                             colors=POLYGON_POSSIBLE_COLORS)
+
+    hideturtle()
+    input()
+
+
+main()
+
