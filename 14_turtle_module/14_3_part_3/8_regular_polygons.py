dcbaeecc423eb7dfa5dbffc0dae25c6316ab0c35
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-01-29 14:25:12 +0100

    polygon_in_row -> polygon_idx

--- 14_turtle_module/14_3_part_3/8_regular_polygons.py
+++ 14_turtle_module/14_3_part_3/8_regular_polygons.py
@@ -104,13 +104,13 @@ def main():
    
     for _ in range(POLYGON_AMOUNT_IN_ROW):
         shuffle(POLYGON_POSSIBLE_COLORS)
-        for polygon_in_row in range(POLYGON_AMOUNT_IN_COL): 
+        for polygon_idx in range(POLYGON_AMOUNT_IN_COL): 
             draw_random_polygon(center_x=curr_center_x, 
                                 center_y=curr_center_y, 
                                 height=POLYGON_HEIGHT, 
                                 min_possible_side_amount=POLYGON_MIN_POSSIBLE_SIDE_AMOUNT, 
                                 max_possible_side_amount=POLYGON_MAX_POSSIBLE_SIDE_AMOUNT, 
-                                color=POLYGON_POSSIBLE_COLORS[polygon_in_row])
+                                color=POLYGON_POSSIBLE_COLORS[polygon_idx])
             curr_center_x += POLYGON_CENTER_DISTANCE
         curr_center_x = first_polygon_center_x
         curr_center_y -= POLYGON_CENTER_DISTANCE
