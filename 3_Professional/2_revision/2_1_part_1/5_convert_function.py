613263ba45abfd9219eb05e257fb3b8dbf8ab35c
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-01-31 17:07:55 +0100

    Added comments to each version

--- 3_Professional/2_revision/2_1_part_1/5_convert_function.py
+++ 3_Professional/2_revision/2_1_part_1/5_convert_function.py
@@ -1,3 +1,4 @@
+# Code is loger, but with 1 loop
 def convert(string):
     lower_alpha_count = 0
     upper_alpha_count = 0
@@ -13,13 +14,13 @@ def convert(string):
 
     return edited_string
 
-
+# Shorter, but with 2 loops
 def convert_v2(string):
     edited_string = string.lower() if sum(map(str.islower, string)) >= sum(map(str.isupper, string)) else string.upper() 
 
     return edited_string
 
-
+# Short and with 1 loop, but complicated
 def convert_v3(string):
     edited_string = string.lower() if sum(map(lambda c: (1 if c.isupper() else -1) if c.isalpha() else 0, string)) <= 0 else string.upper()
     
