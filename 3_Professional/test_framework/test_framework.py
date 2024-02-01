a5982d25fefe055bd2a3f26ecede790b068f1957
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-02-01 12:46:17 +0100

    Added sample function

--- 3_Professional/test_framework/test_framework.py
+++ 3_Professional/test_framework/test_framework.py
@@ -5,3 +5,14 @@
 # 4. Compare #3 with the expected output file and write comparison results
 
 
+HIDING_SYM = '*'
+ALL_DIGIT_AMOUNT = 16
+NOT_HIDED_DIGIT_AMOUNT = 4
+HIDING_SYM_AMOUNT = ALL_DIGIT_AMOUNT - NOT_HIDED_DIGIT_AMOUNT
+
+
+def hide_card(card_number):
+    card_number_only_digit = card_number.replace(' ', '')
+    hided_card_number = HIDING_SYM * HIDING_SYM_AMOUNT + card_number_only_digit[- NOT_HIDED_DIGIT_AMOUNT:]
+    return hided_card_number
+
