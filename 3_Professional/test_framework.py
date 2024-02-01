b178a95baa8fafe057bd70210a24cbcdfd8a0ab2
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-02-01 12:43:50 +0100

    Moved test_framework.py to external folder

--- 3_Professional/test_framework.py
+++
@@ -1,7 +0,0 @@
-# Implement test func:
-# 1. Read input file with test data from Stepik
-# 2. Merge #1 with file to test and save to new file
-# 3. Execute #2 file and save output to result file
-# 4. Compare #3 with the expected output file and write comparison results
-
-
