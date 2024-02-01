656fa16a0308a78afb506d72914167425f3edacb
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2024-02-01 12:16:56 +0100

    2_1_5 Convert function (#265)
    * Created 5_convert_function.py with the initial version
    
    * Added convert_v2()
    
    * Added convert_v3() and main()
    
    * Added comments to each version
    
    * Fixed typo

---
+++ 3_Professional/2_revision/2_1_part_1/5_convert_function.py
@@ -0,0 +1,34 @@
+# Code is longer, but with 1 loop
+def convert(string):
+    lower_alpha_count = 0
+    upper_alpha_count = 0
+    
+    for sym in string:
+        if sym.isalpha():
+            if sym.islower():
+                lower_alpha_count += 1
+            else:
+                upper_alpha_count += 1
+      
+    edited_string = string.lower() if lower_alpha_count >= upper_alpha_count else string.upper()
+
+    return edited_string
+
+# Shorter, but with 2 loops
+def convert_v2(string):
+    edited_string = string.lower() if sum(map(str.islower, string)) >= sum(map(str.isupper, string)) else string.upper() 
+
+    return edited_string
+
+# Short and with 1 loop, but complicated
+def convert_v3(string):
+    edited_string = string.lower() if sum(map(lambda c: (1 if c.isupper() else -1) if c.isalpha() else 0, string)) <= 0 else string.upper()
+    
+    return edited_string
+
+
+def main():
+    print(convert('pi31415!'))
+
+
+main()
