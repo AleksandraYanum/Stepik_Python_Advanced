5790d1765f3eaf43655ab2d1180c751955b596ed
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-02-20 17:11:16 +0100

    Deleted unnecessary line and comment

--- 3_Professional/2_revision/2_2_part_2/8_corporate_mail.py
+++ 3_Professional/2_revision/2_2_part_2/8_corporate_mail.py
@@ -1,29 +1,3 @@
-# from string import digits
-
-# MAIL_DOMAIN = '@beegeek.bzz'
-
-
-# existing_names = []
-# new_names = []
-
-# for _ in range(int(input())):
-#     name, _ = input().split('@')
-#     existing_names.append(name)
-
-# for _ in range(int(input())):
-#     name = input()
-#     name_count = 0
-#     while name in existing_names:
-#         name_count += 1
-#         name = name.rstrip(digits) + str(name_count)
-#     existing_names.append(name)
-#     new_names.append(name)
-
-# all_emails = list(map(lambda c: c + MAIL_DOMAIN, new_names))
-
-# print(*all_emails, sep='\n')
-
-
 from collections import defaultdict
 
 
@@ -50,7 +24,6 @@ for _ in range(int(input())):
     if existing_name_num_list:
         while num in existing_name_num_list:
             num += 1
-        num_to_add = num
 
     num_to_add = num if num else ''
 
