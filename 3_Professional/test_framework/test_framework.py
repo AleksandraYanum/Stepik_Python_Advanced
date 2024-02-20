96e3af7b41030cf4a0cc4a1a87494ceac45faec4
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-02-20 14:57:53 +0100

    Added file_test const

--- 3_Professional/test_framework/test_framework.py
+++ 3_Professional/test_framework/test_framework.py
@@ -4,15 +4,17 @@
 # 3. Execute #2 file and save output to result file
 # 4. Compare #3 with the expected output file and write comparison results
 
+FILE_PATH = 'C:\\Users\\Aleksandra\\src\\Stepik_Python_Advanced\\3_Professional\\test_framework\\'
+
 
 def test():
-    with open('code.py', encoding='utf-8') as code_file, \
-     open('to_be_tested.py', 'w', encoding='utf-8') as to_be_tested_file:
+    with open(FILE_PATH + 'code.py', encoding='utf-8') as code_file, \
+     open(FILE_PATH + 'to_be_tested.py', 'w', encoding='utf-8') as to_be_tested_file:
         for line in code_file:
             to_be_tested_file.write(line)
 
-    with open('input.txt', encoding='utf-8') as input_file, \
-     open('to_be_tested.py', 'w', encoding='utf-8') as to_be_tested_file:
+    with open(FILE_PATH + 'input.txt', encoding='utf-8') as input_file, \
+     open(FILE_PATH + 'to_be_tested.py', 'w', encoding='utf-8') as to_be_tested_file:
         for line in input_file:
             to_be_tested_file.write(line)        
 
