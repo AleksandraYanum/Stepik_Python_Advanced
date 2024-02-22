91b737dcaa6d4b3f85a2ce1d9eaa1e931443e97d
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-02-22 12:20:55 +0100

    converted_size ->byte_size

--- 3_Professional/2_revision/2_2_part_2/9_files_in_files.py
+++ 3_Professional/2_revision/2_2_part_2/9_files_in_files.py
@@ -1,7 +1,17 @@
+from collections import defaultdict
+
+FILE_INFO = defaultdict(lambda: defaultdict(list))
+TOTAL_SIZE_CODE = 'total_size'
+NAMES_CODE = 'names'
+SIZES_CODE = 'sizes'
+UNIT_CODE = 'unit'
+FILE_INFO_VALUE_CODES = [NAMES_CODE, SIZES_CODE]
+UNIT_CONVERTION_DICT = {'B': 1, 'KB': 1024, 'MB': 1024 ** 2, 'GB': 1024 ** 3}
+FILE_PATH = 'C:\\Users\\Aleksandra\\Downloads\\'
+FILE_NAME = 'files.txt'
 STRING_DELIMITER = '-' * 10
 TOTAL_SIZE_STRING = 'Summary: {size} {unit}'
-UNIT_CONVERTION_DICT = {'B': 1, 'KB': 1024, 'MB': 1024 ** 2, 'GB': 1024 ** 3}
-UNIT_DICT = {0: 'В', 1: 'КВ', 2: 'MB', 3: 'GB'}
+
 
 
 def convert_to_biggest_unit(size):
@@ -17,25 +27,29 @@ def convert_to_biggest_unit(size):
     
 def main():
     
-    file_info = dict()
-    
-    with open('C:\\Users\\Aleksandra\\Downloads\\files.txt', encoding='utf-8') as file:
+    with open(FILE_PATH + FILE_NAME, encoding='utf-8') as file:
 
         for line in file:
             full_name, size, unit = line.split()
             name, extension = full_name.split('.')
-            file_info.setdefault(extension, []).append((name, int(size) * UNIT_CONVERTION_DICT[unit]))
+            byte_size =  int(size) * UNIT_CONVERTION_DICT[unit]
+            for code, value in zip(FILE_INFO_VALUE_CODES, [name, byte_size]):
+                FILE_INFO[extension][code].append(value)
 
-    for extension in sorted(file_info):
-        total_size = 0
-        for name, size in sorted(file_info[extension]):
+    for extension in FILE_INFO:
+        total_size = sum(FILE_INFO[extension][SIZES_CODE])
+        byte_size, converted_unit = convert_to_biggest_unit(total_size)
+
+        FILE_INFO[extension][TOTAL_SIZE_CODE].append(byte_size)
+        FILE_INFO[extension][UNIT_CODE].append(converted_unit)
+
+    for extension in sorted(FILE_INFO):
+        for name in sorted(FILE_INFO[extension][NAMES_CODE]):
             print(name + '.' + extension)
-            total_size += size
-            
-        converted_size, converted_unit = convert_to_biggest_unit(total_size)
         print(STRING_DELIMITER)
-        print(TOTAL_SIZE_STRING.format(size=converted_size, unit=converted_unit))
+        print(TOTAL_SIZE_STRING.format(size=FILE_INFO[extension][TOTAL_SIZE_CODE][0], 
+                                        unit=FILE_INFO[extension][UNIT_CODE][0]))
         print()
-        
+
 
 main()
