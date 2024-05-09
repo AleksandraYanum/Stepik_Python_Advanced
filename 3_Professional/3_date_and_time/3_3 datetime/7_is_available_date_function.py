87f1b4446831e2e9b9d179c0d3e1ad666c2b5594
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-09 14:13:11 +0200

    Extracted funcs from is_available_date()

--- 3_Professional/3_date_and_time/3_3 datetime/7_is_available_date_function.py
+++ 3_Professional/3_date_and_time/3_3 datetime/7_is_available_date_function.py
@@ -4,30 +4,33 @@ from datetime import datetime, timedelta
 DATE_FORMAT = '%d.%m.%Y'
 DATE_DIVIDER = '-'
 
-def is_available_date(booked_dates, date_for_booking):
 
-    def parse_date(date_str):
-        parsed_date = datetime.strptime(date_str, DATE_FORMAT)
-        return parsed_date
+def parse_date(date_str):
+    parsed_date = datetime.strptime(date_str, DATE_FORMAT)
+    return parsed_date
 
-    def is_booked(date, booked_ranges):
-        is_booked = False
+
+def is_booked(date, booked_ranges):
+    is_booked = False
+
+    for booked_range in booked_ranges:
         
-        for booked_range in booked_ranges:
+        if DATE_DIVIDER in booked_range:
+            start, end = booked_range.split(DATE_DIVIDER)
+            start_date = parse_date(start)
+            end_date = parse_date(end)
             
-            if DATE_DIVIDER in booked_range:
-                start, end = booked_range.split(DATE_DIVIDER)
-                start_date = parse_date(start)
-                end_date = parse_date(end)
+            if start_date <= date <= end_date:
+                is_booked = True
+        else:
+            if parse_date(booked_range) == date:
+                is_booked = True
                 
-                if start_date <= date <= end_date:
-                    is_booked = True
-            else:
-                if parse_date(booked_range) == date:
-                    is_booked = True
-                    
-        return is_booked
+    return is_booked
 
+
+def is_available_date(booked_dates, date_for_booking):
+    
     # Divide the date for booking into a period, if necessary
     if DATE_DIVIDER in date_for_booking:
         start, end = date_for_booking.split(DATE_DIVIDER)
