18f5337228b44ecf30f302a6e80275ee8937e3d6
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-09 14:22:30 +0200

    Removed date_divider check and addep map func for date parsing

--- 3_Professional/3_date_and_time/3_3 datetime/7_is_available_date_function.py
+++ 3_Professional/3_date_and_time/3_3 datetime/7_is_available_date_function.py
@@ -30,21 +30,31 @@ def is_booked(date, booked_ranges):
 
 
 def is_available_date(booked_dates, date_for_booking):
-    
-    # Divide the date for booking into a period, if necessary
-    if DATE_DIVIDER in date_for_booking:
-        start, end = date_for_booking.split(DATE_DIVIDER)
-        start_date = parse_date(start)
-        end_date = parse_date(end)
-        booking_dates = [start_date + timedelta(days=i) for i in range((end_date - start_date).days + 1)]
-    else:
-        booking_dates = [parse_date(date_for_booking)]
 
-    # Check each booking date
-    are_free_dates = True
-    for booking_date in booking_dates:
-        if is_booked(booking_date, booked_dates):
-            are_free_dates = False
-            break
+    dates_for_booking = date_for_booking.split(DATE_DIVIDER)
+    start = dates_for_booking[0]
+    end = dates_for_booking[1] if len(dates_for_booking) == 2 else start
+    start_date, end_date = map(parse_date, [start, end])
+
+
+    start_date = parse_date(start)
+    end_date = parse_date(end)
+#     booking_dates = [start_date + timedelta(days=i) for i in range((end_date - start_date).days + 1)]
+# else:
+#     booking_dates = [parse_date(date_for_booking)]
+
+    # # Check each booking date
+    # are_free_dates = True
+    # for booking_date in booking_dates:
+    #     if is_booked(booking_date, booked_dates):
+    #         are_free_dates = False
+    #         break
     
-    return are_free_dates
+    # return are_free_dates
+
+
+
+
+dates = ['04.11.2021', '05.11.2021-09.11.2021']
+some_date = '01.11.2021'
+print(is_available_date(dates, some_date))
