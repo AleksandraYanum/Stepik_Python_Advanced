061d4c536fccbf41a2ffb1a66ccaf815538cff1e
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-10 15:30:39 +0200

    Added defaultdict

--- 3_Professional/3_date_and_time/3_3_datetime/6_cosmonauts_diary.py
+++ 3_Professional/3_date_and_time/3_3_datetime/6_cosmonauts_diary.py
@@ -1,45 +1,30 @@
 from datetime import datetime
+from collections import defaultdict
 
 
 DATETIME_FORMAT = '%d.%m.%Y; %H:%M'
 DATETIME_IDX = 0
 DIARY_TEXT_IDX = 1
 
-diary_entries = {}  # Use a dictionary instead of a list
+diary_entries = defaultdict(list)  # Use a dictionary instead of a list
 
 with open('C:\\Users\\Aleksandra\\Downloads\\diary.txt', 'r', encoding='utf-8') as file:
     is_first_line = True
 
     for line in file:
+        line = line.strip()
         if line:
-            line = line.strip()
             if is_first_line:
                 date_time = datetime.strptime(line, DATETIME_FORMAT)
                 is_first_line = False
             else:
                 diary_entries[date_time].append(line)  # Store entry in the dictionary
         else:
-            is_first_line = False
-
-
-
-    #     line = line.strip()
-    #     if line:
-    #         entry_lines.append(line)
-    #     else:
-    #         date_time = datetime.strptime(entry_lines[0], DATETIME_FORMAT)
-    #         text = '\n'.join(entry_lines[1:])
-    #         diary_entries[date_time] = text  # Store entry in the dictionary
-    #         entry_lines = []
-
-    # if entry_lines:
-    #     date_time = datetime.strptime(entry_lines[0], DATETIME_FORMAT)
-    #     text = '\n'.join(entry_lines[1:])
-    #     diary_entries[date_time] = text  # Store entry in the dictionary
+            is_first_line = True
 
 sorted_entries = sorted(diary_entries.items(), key=lambda x: x[0])
 
-for entry in sorted_entries:
-    print(entry[0].strftime(DATETIME_FORMAT))
-    print(entry[1])
+for date, entries in sorted_entries:
+    print(date.strftime(DATETIME_FORMAT))
+    print(*entries, sep='\n')
     print()
