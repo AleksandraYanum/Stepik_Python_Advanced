d87063e4cffcfc081702ea5b3cb86ee577612a34
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-15 19:54:45 +0200

    Added 8_is_it_python.py with full logic

---
+++ 3_Professional/8_is_it_python.py
@@ -0,0 +1,26 @@
+from sys import stdin
+from datetime import datetime
+
+
+ASCENDING_MSG = 'ASC'
+DESCENDING_MSG = 'DESC'
+MIXED_MSG = 'MIX'
+DATE_FROMAT = '%d.%m.%Y'
+
+def main():
+    
+    prev_date = datetime.strptime(stdin.readline().strip(), DATE_FROMAT)
+    order_count = 0
+    date_count = 0
+
+    for line in stdin:
+        curr_date = datetime.strptime(line.strip(), DATE_FROMAT)
+        order_count += 1 if prev_date < curr_date else (-1 if prev_date > curr_date else 0)
+        date_count +=1
+
+    output = ASCENDING_MSG if order_count == date_count else (DESCENDING_MSG if order_count == -date_count else MIXED_MSG)
+
+    print(output)
+
+
+main()
