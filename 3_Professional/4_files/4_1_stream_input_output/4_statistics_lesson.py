35f31a10795ac776c4eae59d93ea6a4ef0cd95d5
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-16 02:33:21 +0200

    Added main()

--- 3_Professional/4_files/4_1_stream_input_output/4_statistics_lesson.py
+++ 3_Professional/4_files/4_1_stream_input_output/4_statistics_lesson.py
@@ -7,29 +7,33 @@ SHORTEST_HEIGHT_MSG = 'Рост самого низкого ученика: {}'
 AVERAGE_HEIGHT_MSG = 'Средний рост: {}'
 
 
-min_height, max_height = 1000, -1
-total_height_sum = 0
-student_count = 0
+def main():
+    min_height, max_height = 1000, -1
+    total_height_sum = 0
+    student_count = 0
 
-for line in sys.stdin:
-    current_height = int(line)
+    for line in stdin:
+        current_height = int(line)
 
-    if current_height < min_height:
-        min_height = current_height
-    if current_height > max_height:
-        max_height = current_height
+        if current_height < min_height:
+            min_height = current_height
+        if current_height > max_height:
+            max_height = current_height
 
-    total_height_sum += current_height
-    student_count += 1
+        total_height_sum += current_height
+        student_count += 1
 
-output = EMPTY_INPUT_MSG
+    output = EMPTY_INPUT_MSG
 
-if student_count:
+    if student_count:
 
-    average_height = total_height_sum / student_count
+        average_height = total_height_sum / student_count
 
-    output = '\n'.join([SHORTEST_HEIGHT_MSG.format(min_height), 
-                    TALLEST_HEIGHT_MSG.format(max_height), 
-                    AVERAGE_HEIGHT_MSG.format(average_height)])
+        output = '\n'.join([SHORTEST_HEIGHT_MSG.format(min_height), 
+                        TALLEST_HEIGHT_MSG.format(max_height), 
+                        AVERAGE_HEIGHT_MSG.format(average_height)])
 
-print(output)
+    print(output)
+
+
+    main()
