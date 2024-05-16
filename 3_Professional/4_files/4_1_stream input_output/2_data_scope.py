ad62c8e42b5a1b932109da97d87a1095abe11454
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-16 02:19:19 +0200

    Removed all items saving

--- 3_Professional/4_files/4_1_stream input_output/2_data_scope.py
+++ 3_Professional/4_files/4_1_stream input_output/2_data_scope.py
@@ -1,11 +1,17 @@
 import sys
-from datetime import datetime
+from datetime import date
 
 
-dates = []
+min_date = date.max
+max_date = date.min
 
 for line in sys.stdin:
-    dates.append(datetime.fromisoformat(line.strip()))
+    curr_date = date.fromisoformat(line.strip())
+    if curr_date > max_date:
+        max_date = curr_date
+    if curr_date < min_date:
+        min_date = curr_date
 
-max_min_delta = max(dates) - min(dates)
-print(max_min_delta.days)
+max_min_delta = (max_date - min_date).days
+
+print(max_min_delta)
