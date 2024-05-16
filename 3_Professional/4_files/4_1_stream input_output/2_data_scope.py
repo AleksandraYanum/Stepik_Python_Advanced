59a0ee0e76662d8ab9444350ae6a2e1cc445ae14
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-16 02:20:39 +0200

    Imported only stdin from sys

--- 3_Professional/4_files/4_1_stream input_output/2_data_scope.py
+++ 3_Professional/4_files/4_1_stream input_output/2_data_scope.py
@@ -1,12 +1,12 @@
-import sys
+from sys import stdin
 from datetime import date
 
 def main():
-    
+
     min_date = date.max
     max_date = date.min
 
-    for line in sys.stdin:
+    for line in stdin:
         curr_date = date.fromisoformat(line.strip())
         if curr_date > max_date:
             max_date = curr_date
