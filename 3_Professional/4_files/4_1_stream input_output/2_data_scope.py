005737f45c4c8bf322f101baaa73cf4a897e902f
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-16 02:19:51 +0200

    Added main()

--- 3_Professional/4_files/4_1_stream input_output/2_data_scope.py
+++ 3_Professional/4_files/4_1_stream input_output/2_data_scope.py
@@ -1,17 +1,21 @@
 import sys
 from datetime import date
 
+def main():
+    
+    min_date = date.max
+    max_date = date.min
 
-min_date = date.max
-max_date = date.min
+    for line in sys.stdin:
+        curr_date = date.fromisoformat(line.strip())
+        if curr_date > max_date:
+            max_date = curr_date
+        if curr_date < min_date:
+            min_date = curr_date
 
-for line in sys.stdin:
-    curr_date = date.fromisoformat(line.strip())
-    if curr_date > max_date:
-        max_date = curr_date
-    if curr_date < min_date:
-        min_date = curr_date
+    max_min_delta = (max_date - min_date).days
 
-max_min_delta = (max_date - min_date).days
+    print(max_min_delta)
 
-print(max_min_delta)
+
+main()
