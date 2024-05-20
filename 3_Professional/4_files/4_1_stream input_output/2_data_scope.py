d5f8414b49db9886f7d5dc6bc0215b2c518c5ff1
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-20 15:49:41 +0200

    Assigned first date value min and max date vars

--- 3_Professional/4_files/4_1_stream input_output/2_data_scope.py
+++ 3_Professional/4_files/4_1_stream input_output/2_data_scope.py
@@ -2,15 +2,14 @@ from sys import stdin
 from datetime import date
 
 def main():
-
-    min_date = date.max
-    max_date = date.min
+    сurr_date = date.fromisoformat(stdin.readline().strip())
+    min_date, max_date = сurr_date, сurr_date
 
     for line in stdin:
         curr_date = date.fromisoformat(line.strip())
         if curr_date > max_date:
             max_date = curr_date
-        if curr_date < min_date:
+        elif curr_date < min_date:
             min_date = curr_date
 
     max_min_delta = (max_date - min_date).days
