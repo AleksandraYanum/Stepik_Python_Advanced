b3a86d7569d3d7f46fe9055cbbbd0937261cdb83
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-20 15:14:47 +0200

    Removed all spaces after reading lines using map()

--- 3_Professional/4_files/4_1_stream_input_output/7_panoramic_agency.py
+++ 3_Professional/4_files/4_1_stream_input_output/7_panoramic_agency.py
@@ -10,13 +10,13 @@ LINE_IDX = 0
 
 def main():
 
-    news = [line.strip().split(DIVIDER) for line in sys.stdin]
+    news = [list(map(str.strip, line.split(DIVIDER))) for line in sys.stdin]
 
     needed_category = news.pop()[0]
-    category_news = [one_news_info for one_news_info in news if one_news_info[CATEGORY_IDX].strip() == needed_category]
+    category_news = [one_news_info for one_news_info in news if one_news_info[CATEGORY_IDX] == needed_category]
 
-    for news in sorted(category_news, key=lambda x: (float(x[RELIABILITY_IDX]), x[LINE_IDX].strip())):
-        print(news[LINE_IDX].strip())
+    for news in sorted(category_news, key=lambda x: (float(x[RELIABILITY_IDX]), x[LINE_IDX])):
+        print(news[LINE_IDX])
 
 
 main()
