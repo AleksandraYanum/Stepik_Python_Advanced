f8a7a2aa26f708f625e52da4e93ffcd638388e8e
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-20 21:10:49 +0200

    Added reading lines condition

--- 3_Professional/4_files/4_1_stream_input_output/8_is_it_python.py
+++ 3_Professional/4_files/4_1_stream_input_output/8_is_it_python.py
@@ -20,17 +20,19 @@ def main():
     date_order = ASC_ORDER if curr_date > prev_date else (DESC_ORDER if curr_date < prev_date else MIX_ORDER)
     prev_date = curr_date
 
-    for line in stdin:
-        curr_date = datetime.strptime(line.strip(), DATE_FROMAT)
+    if date_order != MIX_ORDER:
 
-        if curr_date == prev_date or \
-            (curr_date < prev_date and date_order == ASC_ORDER) or \
-            (curr_date > prev_date and date_order == DESC_ORDER):
+        for line in stdin:
+            curr_date = datetime.strptime(line.strip(), DATE_FROMAT)
+
+            if curr_date == prev_date or \
+                (curr_date < prev_date and date_order == ASC_ORDER) or \
+                (curr_date > prev_date and date_order == DESC_ORDER):
+                
+                date_order = MIX_ORDER
+                break
             
-            date_order = MIX_ORDER
-            break
-        
-        prev_date = curr_date
+            prev_date = curr_date
 
     output = ASCENDING_MSG if date_order == ASC_ORDER else (DESCENDING_MSG if date_order == DESC_ORDER else MIXED_MSG)
 
