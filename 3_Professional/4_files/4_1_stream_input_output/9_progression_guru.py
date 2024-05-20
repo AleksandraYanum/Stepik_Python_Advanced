1bced2eb544543ba9ee7be3cf6b20d385352f158
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-20 22:39:54 +0200

    Added hypothesis check after reading first three nums

--- 3_Professional/4_files/4_1_stream_input_output/9_progression_guru.py
+++ 3_Professional/4_files/4_1_stream_input_output/9_progression_guru.py
@@ -1,6 +1,10 @@
 from sys import stdin
 
 
+ARITHMETIC_PROGRESSION = 1
+GEOMETRIC_PROGRESSION = 2
+NOT_PROGRESSION = 3
+
 ARITHMETIC_PROGRESSION_MSG = 'Арифметическая прогрессия'
 GEOMETRIC_PROGRESSION_MSG = 'Геометрическая прогрессия'
 NOT_PROGRESSION_MSG = 'Не прогрессия'
@@ -8,22 +12,31 @@ NOT_PROGRESSION_MSG = 'Не прогрессия'
 
 def main():
 
-    prev_num, curr_num = int(stdin.readline()), int(stdin.readline())
+    prev_prev_num, prev_num, curr_num = int(stdin.readline()), int(stdin.readline()), int(stdin.readline())
 
-    difference = curr_num - prev_num
-    quotient = curr_num / prev_num
-    line_count = 0
-    progression_count = 0
-    
-    for line in stdin:
-        prev_num = curr_num
-        curr_num = int(line)
+    progression = NOT_PROGRESSION
+    difference, quotient = None, None
 
-        progression_count += 1 if curr_num - prev_num == difference else (-1 if curr_num / prev_num == quotient else 0)
-        line_count += 1
+    if curr_num - prev_num == prev_num - prev_prev_num:
+        progression = ARITHMETIC_PROGRESSION
+        difference = curr_num - prev_num
+    elif curr_num / prev_num == prev_num / prev_prev_num:
+        progression = GEOMETRIC_PROGRESSION
+        quotient = curr_num / prev_num
 
-    output = ARITHMETIC_PROGRESSION_MSG if progression_count == line_count else \
-            (GEOMETRIC_PROGRESSION_MSG if progression_count == -line_count else NOT_PROGRESSION_MSG)
+    if progression != NOT_PROGRESSION: 
+            
+            for line in stdin:
+                prev_num = curr_num
+                curr_num = int(line)
+
+                if  (curr_num - prev_num != difference and progression == ARITHMETIC_PROGRESSION) or \
+                    (curr_num / prev_num != quotient and progression == GEOMETRIC_PROGRESSION):
+                     progression = NOT_PROGRESSION
+                     break
+
+    output = ARITHMETIC_PROGRESSION_MSG if progression == ARITHMETIC_PROGRESSION else \
+            (GEOMETRIC_PROGRESSION_MSG if progression == GEOMETRIC_PROGRESSION else NOT_PROGRESSION_MSG)
     
     print(output)
 
