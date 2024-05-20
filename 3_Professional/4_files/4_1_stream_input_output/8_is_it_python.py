857908eb9a2f5620f5adc9422859a54cdd0c5057
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-20 16:56:44 +0200

    Added hypothesis check after reading first two dates

--- 3_Professional/4_files/4_1_stream_input_output/8_is_it_python.py
+++ 3_Professional/4_files/4_1_stream_input_output/8_is_it_python.py
@@ -2,24 +2,37 @@ from sys import stdin
 from datetime import datetime
 
 
+ASC_ORDER = 1
+DESC_ORDER = 2
+MIX_ORDER = 3
+
 ASCENDING_MSG = 'ASC'
 DESCENDING_MSG = 'DESC'
 MIXED_MSG = 'MIX'
 DATE_FROMAT = '%d.%m.%Y'
 
+
 def main():
 
     prev_date = datetime.strptime(stdin.readline().strip(), DATE_FROMAT)
-    order_count = 0
-    date_count = 0
+    curr_date = datetime.strptime(stdin.readline().strip(), DATE_FROMAT)
+
+    date_order = ASC_ORDER if curr_date > prev_date else (DESC_ORDER if curr_date < prev_date else MIX_ORDER)
+    prev_date = curr_date
 
     for line in stdin:
         curr_date = datetime.strptime(line.strip(), DATE_FROMAT)
-        order_count += 1 if prev_date < curr_date else (-1 if prev_date > curr_date else 0)
-        date_count +=1
+
+        if curr_date == prev_date or \
+            (curr_date < prev_date and date_order == ASC_ORDER) or \
+            (curr_date > prev_date and date_order == DESC_ORDER):
+            
+            date_order = MIX_ORDER
+            break
+        
         prev_date = curr_date
 
-    output = ASCENDING_MSG if order_count == date_count else (DESCENDING_MSG if order_count == -date_count else MIXED_MSG)
+    output = ASCENDING_MSG if date_order == ASC_ORDER else (DESCENDING_MSG if date_order == DESC_ORDER else MIXED_MSG)
 
     print(output)
 
