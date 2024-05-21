231c933ba54803e1036c37321f98bb4827965345
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-22 01:19:32 +0200

    Simplified logic

--- 3_Professional/4_files/4_1_stream_input_output/9_progression_guru.py
+++ 3_Professional/4_files/4_1_stream_input_output/9_progression_guru.py
@@ -29,24 +29,19 @@ def main():
     prev_prev_num, prev_num, curr_num = int(stdin.readline()), int(stdin.readline()), int(stdin.readline())
 
     progression_type = NOT_PROGRESSION
-    difference, quotient = None, None
+    step = None
 
     if curr_num - prev_num == prev_num - prev_prev_num:
         progression_type = ARITHMETIC_PROGRESSION
-        difference = curr_num - prev_num
+        step = curr_num - prev_num
     elif curr_num / prev_num == prev_num / prev_prev_num:
         progression_type = GEOMETRIC_PROGRESSION
-        quotient = curr_num / prev_num
+        step = curr_num / prev_num
 
     if progression_type != NOT_PROGRESSION: 
             
-            if progression_type == ARITHMETIC_PROGRESSION:
-                if not is_right_progression_in_stdin(ARITHMETIC_PROGRESSION, curr_num, difference):
-                    progression_type = NOT_PROGRESSION
-
-            if progression_type == GEOMETRIC_PROGRESSION:
-                if not is_right_progression_in_stdin(GEOMETRIC_PROGRESSION, curr_num, quotient):
-                    progression_type = NOT_PROGRESSION
+        if not is_right_progression_in_stdin(progression_type, curr_num, step):
+            progression_type = NOT_PROGRESSION
           
     output = ARITHMETIC_PROGRESSION_MSG if progression_type == ARITHMETIC_PROGRESSION else \
             (GEOMETRIC_PROGRESSION_MSG if progression_type == GEOMETRIC_PROGRESSION else NOT_PROGRESSION_MSG)
