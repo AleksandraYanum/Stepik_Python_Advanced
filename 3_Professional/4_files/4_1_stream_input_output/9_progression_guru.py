2e42f544ce3fae6ee520a63413e5ab73bdea7aec
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-22 00:19:28 +0200

    progression -> progression_type

--- 3_Professional/4_files/4_1_stream_input_output/9_progression_guru.py
+++ 3_Professional/4_files/4_1_stream_input_output/9_progression_guru.py
@@ -14,29 +14,29 @@ def main():
 
     prev_prev_num, prev_num, curr_num = int(stdin.readline()), int(stdin.readline()), int(stdin.readline())
 
-    progression = NOT_PROGRESSION
+    progression_type = NOT_PROGRESSION
     difference, quotient = None, None
 
     if curr_num - prev_num == prev_num - prev_prev_num:
-        progression = ARITHMETIC_PROGRESSION
+        progression_type = ARITHMETIC_PROGRESSION
         difference = curr_num - prev_num
     elif curr_num / prev_num == prev_num / prev_prev_num:
-        progression = GEOMETRIC_PROGRESSION
+        progression_type = GEOMETRIC_PROGRESSION
         quotient = curr_num / prev_num
 
-    if progression != NOT_PROGRESSION: 
+    if progression_type != NOT_PROGRESSION: 
             
             for line in stdin:
                 prev_num = curr_num
                 curr_num = int(line)
 
-                if  (curr_num - prev_num != difference and progression == ARITHMETIC_PROGRESSION) or \
-                    (curr_num / prev_num != quotient and progression == GEOMETRIC_PROGRESSION):
-                     progression = NOT_PROGRESSION
+                if  (curr_num - prev_num != difference and progression_type == ARITHMETIC_PROGRESSION) or \
+                    (curr_num / prev_num != quotient and progression_type == GEOMETRIC_PROGRESSION):
+                     progression_type = NOT_PROGRESSION
                      break
 
-    output = ARITHMETIC_PROGRESSION_MSG if progression == ARITHMETIC_PROGRESSION else \
-            (GEOMETRIC_PROGRESSION_MSG if progression == GEOMETRIC_PROGRESSION else NOT_PROGRESSION_MSG)
+    output = ARITHMETIC_PROGRESSION_MSG if progression_type == ARITHMETIC_PROGRESSION else \
+            (GEOMETRIC_PROGRESSION_MSG if progression_type == GEOMETRIC_PROGRESSION else NOT_PROGRESSION_MSG)
     
     print(output)
 
