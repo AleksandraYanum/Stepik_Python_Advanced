93f4c8ad5264ceb3faf44106efe452b0b9858983
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-28 12:43:06 +0200

    Renamed vars

--- 3_Professional/4_files/4_1_stream_input_output/9_progression_guru.py
+++ 3_Professional/4_files/4_1_stream_input_output/9_progression_guru.py
@@ -4,12 +4,12 @@ from operator import truediv, sub
 
 ARITHMETIC_PROGRESSION_FUNC = sub
 GEOMETRIC_PROGRESSION_FUNC = truediv
-NOT_PROGRESSION = 1
+NOT_PROGRESSION_FUNC = None
 
 PROGRESSION_OUTPUT = {
     ARITHMETIC_PROGRESSION_FUNC: 'Арифметическая прогрессия',
     GEOMETRIC_PROGRESSION_FUNC: 'Геометрическая прогрессия',
-    NOT_PROGRESSION:  'Не прогрессия'
+    NOT_PROGRESSION_FUNC:  'Не прогрессия'
 }
                       
 
@@ -17,7 +17,7 @@ def is_right_progression_in_stdin(progression_func, init_num, step):
 
     curr_num = init_num
     result = False
-    
+
     for line in stdin:
         prev_num = curr_num
         curr_num = int(line)
@@ -33,22 +33,22 @@ def main():
 
     prev_prev_num, prev_num, curr_num = int(stdin.readline()), int(stdin.readline()), int(stdin.readline())
 
-    progression_type = NOT_PROGRESSION
+    progression_func = NOT_PROGRESSION_FUNC
     step = None
 
     if curr_num - prev_num == prev_num - prev_prev_num:
-        progression_type = ARITHMETIC_PROGRESSION_FUNC
+        progression_func = ARITHMETIC_PROGRESSION_FUNC
         step = curr_num - prev_num
     elif curr_num / prev_num == prev_num / prev_prev_num:
-        progression_type = GEOMETRIC_PROGRESSION_FUNC
+        progression_func = GEOMETRIC_PROGRESSION_FUNC
         step = curr_num / prev_num
 
-    if progression_type != NOT_PROGRESSION: 
+    if progression_func != NOT_PROGRESSION_FUNC:
             
-        if not is_right_progression_in_stdin(progression_type, curr_num, step):
-            progression_type = NOT_PROGRESSION
+        if not is_right_progression_in_stdin(progression_func, curr_num, step):
+            progression_func = NOT_PROGRESSION_FUNC
           
-    output = PROGRESSION_OUTPUT[progression_type]
+    output = PROGRESSION_OUTPUT[progression_func]
     
     print(output)
 
