7adaf99aa41b652215a09edef88474fc281d4948
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-05-28 12:28:23 +0200

    Renamed vars

--- 3_Professional/4_files/4_1_stream_input_output/9_progression_guru.py
+++ 3_Professional/4_files/4_1_stream_input_output/9_progression_guru.py
@@ -2,23 +2,24 @@ from sys import stdin
 from operator import truediv, sub
 
 
-ARITHMETIC_PROGRESSION = sub
-GEOMETRIC_PROGRESSION = truediv
+ARITHMETIC_PROGRESSION_FUNC = sub
+GEOMETRIC_PROGRESSION_FUNC = truediv
 NOT_PROGRESSION = 1
 
 PROGRESSION_OUTPUT = {
-    ARITHMETIC_PROGRESSION: 'Арифметическая прогрессия',
-    GEOMETRIC_PROGRESSION: 'Геометрическая прогрессия',
+    ARITHMETIC_PROGRESSION_FUNC: 'Арифметическая прогрессия',
+    GEOMETRIC_PROGRESSION_FUNC: 'Геометрическая прогрессия',
     NOT_PROGRESSION:  'Не прогрессия'
 }
                       
 
-def is_right_progression_in_stdin(progression_type, curr_num, step):
+def is_right_progression_in_stdin(progression_func, init_num, step):
 
+    curr_num = init_num
     for line in stdin:
         prev_num = curr_num
         curr_num = int(line)
-        result = progression_type(curr_num, prev_num) == step
+        result = progression_func(curr_num, prev_num) == step
 
         if not result:
             break
@@ -34,10 +35,10 @@ def main():
     step = None
 
     if curr_num - prev_num == prev_num - prev_prev_num:
-        progression_type = ARITHMETIC_PROGRESSION
+        progression_type = ARITHMETIC_PROGRESSION_FUNC
         step = curr_num - prev_num
     elif curr_num / prev_num == prev_num / prev_prev_num:
-        progression_type = GEOMETRIC_PROGRESSION
+        progression_type = GEOMETRIC_PROGRESSION_FUNC
         step = curr_num / prev_num
 
     if progression_type != NOT_PROGRESSION: 
