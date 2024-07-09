90067dc93d0c127c51ad7412dc9e9ceba67855ba
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-07-09 15:01:11 +0200

    Fixed non input logic

--- 3_Professional/4_files/4_1_stream_input_output/4_statistics_lesson.py
+++ 3_Professional/4_files/4_1_stream_input_output/4_statistics_lesson.py
@@ -8,32 +8,36 @@ AVERAGE_HEIGHT_MSG = 'Средний рост: {}'
 
 
 def main():
-    curr_height = int(stdin.readline().strip())
-    min_height, max_height = curr_height, curr_height
-    total_height_sum = curr_height
-    student_count = 1
+    line = stdin.readline().strip()
 
-    for line in stdin:
-        current_height = int(line)
+    if line:
 
-        if current_height > max_height:
-            max_height = current_height
-        elif current_height < min_height:
-            min_height = current_height
+        curr_height = int(line)
+        min_height, max_height = curr_height, curr_height
+        total_height_sum = curr_height
+        student_count = 1
+
+        for line in stdin:
+            current_height = int(line)
+
+            if current_height > max_height:
+                max_height = current_height
+            elif current_height < min_height:
+                min_height = current_height
+            
+            total_height_sum += current_height
+            student_count += 1
         
-        total_height_sum += current_height
-        student_count += 1
-
-    output = EMPTY_INPUT_MSG
-
-    if student_count:
-
         average_height = total_height_sum / student_count
 
         output = '\n'.join([SHORTEST_HEIGHT_MSG.format(min_height), 
-                        TALLEST_HEIGHT_MSG.format(max_height), 
-                        AVERAGE_HEIGHT_MSG.format(average_height)])
+                            TALLEST_HEIGHT_MSG.format(max_height), 
+                            AVERAGE_HEIGHT_MSG.format(average_height)])
+    
+    else:
 
+        output = EMPTY_INPUT_MSG
+      
     print(output)
 
 
