ef7b6250792bb2aab89ac9d60cd8376e434cd34f
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-07-09 14:38:45 +0200

    Added compare_func

--- 3_Professional/4_files/4_1_stream_input_output/8_is_it_python.py
+++ 3_Professional/4_files/4_1_stream_input_output/8_is_it_python.py
@@ -12,12 +12,24 @@ MIX_MSG = 'MIX'
 DATE_FROMAT = '%d.%m.%Y'
 
 
+def is_asc_order(prev_value, curr_value):
+    result = curr_value > prev_value
+    return result
+
+
+def is_desc_order(prev_value, curr_value):
+    result = curr_value < prev_value
+    return result
+
+
 def main():
 
     prev_date = datetime.strptime(stdin.readline().strip(), DATE_FROMAT)
     curr_date = datetime.strptime(stdin.readline().strip(), DATE_FROMAT)
 
     date_order = ASC_ORDER if curr_date > prev_date else (DESC_ORDER if curr_date < prev_date else MIX_ORDER)
+    compare_func = is_asc_order if date_order == ASC_ORDER else is_desc_order
+
     prev_date = curr_date
 
     if date_order != MIX_ORDER:
@@ -25,10 +37,7 @@ def main():
         for line in stdin:
             curr_date = datetime.strptime(line.strip(), DATE_FROMAT)
 
-            if curr_date == prev_date or \
-                (curr_date < prev_date and date_order == ASC_ORDER) or \
-                (curr_date > prev_date and date_order == DESC_ORDER):
-                
+            if not compare_func(prev_date, curr_date):
                 date_order = MIX_ORDER
                 break
             
