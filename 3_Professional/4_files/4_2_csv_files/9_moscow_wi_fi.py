b3252ca92ac2b7a3668717520d80d1fc9f6d6f53
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-09-13 16:21:24 +0200

    Renamed reader -> rows

--- 3_Professional/4_files/4_2_csv_files/9_moscow_wi_fi.py
+++ 3_Professional/4_files/4_2_csv_files/9_moscow_wi_fi.py
@@ -9,10 +9,10 @@ HOTSPOT_POS = 3
 district_hotspot_dict = defaultdict(int)
 
 with open('wifi.csv', encoding='utf-8') as file:
-    reader = csv.reader(file, delimiter=';')
-    next(reader)  # skip title
+    rows = csv.reader(file, delimiter=';')
+    next(rows)  # skip title
 
-    for row in reader:
+    for row in rows:
         district = row[DISTRICT_POS]
         hotspot_amount = int(row[HOTSPOT_POS])
         district_hotspot_dict[district] += hotspot_amount
