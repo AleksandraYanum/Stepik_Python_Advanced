df42d377fa34754ad8dd1d9bed354c21e435af00
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-09-13 11:52:12 +0200

    Removed join usage

--- 3_Professional/3_date_and_time/3_4_timedelta/7_productivity.py
+++ 3_Professional/3_date_and_time/3_4_timedelta/7_productivity.py
@@ -1,10 +1,11 @@
-#Using one-liner
-
 from datetime import datetime, timedelta
 
 DATE_PATTERN = '%d.%m.%Y'
 DAY_AMOUNT = 10
 
 start_date = datetime.strptime(input(), DATE_PATTERN)
+curr_date = start_date
 
-print("\n".join([(start_date + timedelta(days=sum(range(2, i+2)))).strftime(DATE_PATTERN) for i in range(DAY_AMOUNT)]))
+for i in range(1, DAY_AMOUNT + 1):
+    print(curr_date.strftime(DATE_PATTERN))
+    curr_date = curr_date + timedelta(days=i + 1)
