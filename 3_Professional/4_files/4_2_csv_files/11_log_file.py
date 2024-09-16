55db7a16cc993858ecec44e4c7bdbb9133d9c8d9
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-09-16 13:06:46 +0200

    Renamed vars, to-do comments added

--- 3_Professional/4_files/4_2_csv_files/11_log_file.py
+++ 3_Professional/4_files/4_2_csv_files/11_log_file.py
@@ -9,25 +9,28 @@ DATETIME_TITLE = 'dtime'
 DATETIME_PATTERN = '%d/%m/%Y %H:%M'
 
 
-logs = {}
+latest_name_changes = {}
 
 with open('name_log.csv', 'r', encoding='utf-8') as file_read:
-    rows = DictReader(file_read, delimiter=DELIMITER_SYM)
+    name_changes = DictReader(file_read, delimiter=DELIMITER_SYM)
     
-    for row in rows:
-        email = row[EMAIL_TITLE]
-        current_time = datetime.strptime(row[DATETIME_TITLE], DATETIME_PATTERN)
+    for name_change in name_changes:
+        email = name_change[EMAIL_TITLE]
+        change_time = datetime.strptime(name_change[DATETIME_TITLE], DATETIME_PATTERN)
 
-        if email in logs:
-            existing_time = datetime.strptime(logs[email][DATETIME_TITLE], DATETIME_PATTERN)
-            if current_time > existing_time:
-                logs[email] = row
+        # Use dafaultdict
+        if email in latest_name_changes:
+            existing_time = datetime.strptime(latest_name_changes[email][DATETIME_TITLE], DATETIME_PATTERN)
+            if change_time > existing_time:
+
+                # Add datetime value separately
+                latest_name_changes[email] = name_change
         else:
-            logs[email] = row
+            latest_name_changes[email] = name_change
 
 with open('new_name_log.csv', 'w', encoding='utf-8', newline='') as file_write:
     writer = DictWriter(file_write, fieldnames=[USERNAME_TITLE, EMAIL_TITLE, DATETIME_TITLE])
     writer.writeheader()
     
-    for log in sorted(logs.values(), key=lambda x: x[EMAIL_TITLE]):
+    for log in sorted(latest_name_changes.values(), key=lambda x: x[EMAIL_TITLE]):
         writer.writerow(log)
