9ad98c23921cc90ccfa75f9bda46912ea593f7b4
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-09-19 14:58:30 +0200

    Added defaultdict

--- 3_Professional/4_files/4_2_csv_files/11_log_file.py
+++ 3_Professional/4_files/4_2_csv_files/11_log_file.py
@@ -1,5 +1,6 @@
 from csv import DictReader, DictWriter
 from datetime import datetime
+from collections import defaultdict
 
 
 DELIMITER_SYM = ','
@@ -9,7 +10,8 @@ DATETIME_COL_NAME = 'dtime'
 DATETIME_PATTERN = '%d/%m/%Y %H:%M'
 
 
-latest_name_changes = {}
+latest_name_changes = defaultdict(lambda: None)
+
 
 with open('name_log.csv', 'r', encoding='utf-8') as file_read:
     name_changes = DictReader(file_read, delimiter=DELIMITER_SYM)
@@ -18,15 +20,10 @@ with open('name_log.csv', 'r', encoding='utf-8') as file_read:
         email = name_change[EMAIL_COL_NAME]
         change_time = datetime.strptime(name_change[DATETIME_COL_NAME], DATETIME_PATTERN)
 
-        # Use dafaultdict
-        if email in latest_name_changes:
-            existing_time = datetime.strptime(latest_name_changes[email][DATETIME_COL_NAME], DATETIME_PATTERN)
-            if change_time > existing_time:
-
-                # Add datetime value separately
-                latest_name_changes[email] = name_change
-        else:
+        # if no record yet or if new change_time is later than the stored one
+        if latest_name_changes[email] is None or change_time > datetime.strptime(latest_name_changes[email][DATETIME_COL_NAME], DATETIME_PATTERN):
             latest_name_changes[email] = name_change
+
 
 with open('new_name_log.csv', 'w', encoding='utf-8', newline='') as file_write:
     writer = DictWriter(file_write, fieldnames=[USERNAME_COL_NAME, EMAIL_COL_NAME, DATETIME_COL_NAME])
