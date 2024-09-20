0f08555362cf1e43f26450e8130ac2b2fe2daa46
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-09-20 12:20:13 +0200

    Imported only needed object

--- 3_Professional/4_files/4_2_csv_files/6_sorting_by_col.py
+++ 3_Professional/4_files/4_2_csv_files/6_sorting_by_col.py
@@ -1,9 +1,9 @@
-import csv
+from csv import reader
 
 col_num = int(input()) - 1
 
 with open('deniro.csv', 'r', encoding='utf-8') as file:
-    data = list(csv.reader(file))
+    data = list(reader(file))
 
 data.sort(key=lambda x: int(x[col_num]) if x[col_num].isdigit() else x[col_num])
 
