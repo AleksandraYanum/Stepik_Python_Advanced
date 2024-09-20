f026089de3c6a937d1c33205c8e6240c0013cb02
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-09-20 16:32:23 +0200

    Deleted unused const

--- 3_Professional/4_files/4_2_csv_files/10_log_file.py
+++ 3_Professional/4_files/4_2_csv_files/10_log_file.py
@@ -7,12 +7,10 @@ DELIMITER_SYM = ','
 USERNAME_COL_NAME = 'username'
 EMAIL_COL_NAME = 'email'
 DATETIME_COL_NAME = 'dtime'
-CHANGE_LOG_COL_NAME = 'change'
-CHANGE_TIME = 'time'
 DATETIME_PATTERN = '%d/%m/%Y %H:%M'
 
 
-latest_name_changes = defaultdict(lambda: {CHANGE_LOG_COL_NAME: None, CHANGE_TIME: None})
+latest_name_changes = defaultdict(lambda: {USERNAME_COL_NAME: None, EMAIL_COL_NAME: None, DATETIME_COL_NAME: None})
 
 with open('name_log.csv', 'r', encoding='utf-8') as file_read:
     name_changes = DictReader(file_read, delimiter=DELIMITER_SYM)
@@ -22,13 +20,17 @@ with open('name_log.csv', 'r', encoding='utf-8') as file_read:
         change_time = datetime.strptime(name_change[DATETIME_COL_NAME], DATETIME_PATTERN)
 
         # if no record yet or if new change_time is later than the stored one
-        if latest_name_changes[email][CHANGE_TIME] is None or change_time > latest_name_changes[email][CHANGE_TIME]:
-            latest_name_changes[email] = {CHANGE_LOG_COL_NAME: name_change, CHANGE_TIME: change_time}
-
-
+        if latest_name_changes[email][DATETIME_COL_NAME] is None or change_time > latest_name_changes[email][DATETIME_COL_NAME]:
+            latest_name_changes[email] = {
+                USERNAME_COL_NAME: name_change[USERNAME_COL_NAME],
+                EMAIL_COL_NAME: email,
+                DATETIME_COL_NAME: change_time
+            }
+ 
 with open('new_name_log.csv', 'w', encoding='utf-8', newline='') as file_write:
     writer = DictWriter(file_write, fieldnames=[USERNAME_COL_NAME, EMAIL_COL_NAME, DATETIME_COL_NAME])
     writer.writeheader()
     
-    for log in sorted(latest_name_changes.values(), key=lambda x: x[CHANGE_LOG_COL_NAME][EMAIL_COL_NAME]):
-        writer.writerow(log[CHANGE_LOG_COL_NAME])
+    for log in sorted(latest_name_changes.values(), key=lambda x: x[EMAIL_COL_NAME]):
+        log[DATETIME_COL_NAME] = log[DATETIME_COL_NAME].strftime(DATETIME_PATTERN)
+        writer.writerow(log)
