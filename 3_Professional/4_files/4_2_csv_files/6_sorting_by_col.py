38f44a79f9304c8e964c8653c912743f8ee28919
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-09-20 12:21:36 +0200

    Added DELIMITER_SYM const

--- 3_Professional/4_files/4_2_csv_files/6_sorting_by_col.py
+++ 3_Professional/4_files/4_2_csv_files/6_sorting_by_col.py
@@ -1,5 +1,7 @@
 from csv import reader
 
+DELIMITER_SYM = ','
+
 col_num = int(input()) - 1
 
 with open('deniro.csv', 'r', encoding='utf-8') as file:
@@ -8,4 +10,4 @@ with open('deniro.csv', 'r', encoding='utf-8') as file:
 data.sort(key=lambda x: int(x[col_num]) if x[col_num].isdigit() else x[col_num])
 
 for lst in data:
-    print(*lst, sep=',')
+    print(*lst, sep=DELIMITER_SYM)
