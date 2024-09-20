487698be25d59b6f6f1ebac4f8727888353484ee
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2024-09-20 21:32:07 +0200

    4_2_10 Log file (#321)
    * Created 4_2_11_log_file
    
    * Renamed vars, to-do comments added
    
    * Renamed vars
    
    * Added defaultdict
    
    * Optimizeв datetime parsing by storing parsed value separately
    
    * Renamed file
    
    * Deleted unused const
    
    * Deleted unnecessary value from dict
    
    * Added new var formatted_datetime

---
+++ 3_Professional/4_files/4_2_csv_files/10_log_file.py
@@ -0,0 +1,40 @@
+from csv import DictReader, DictWriter
+from datetime import datetime
+from collections import defaultdict
+
+
+DELIMITER_SYM = ','
+USERNAME_COL_NAME = 'username'
+EMAIL_COL_NAME = 'email'
+DATETIME_COL_NAME = 'dtime'
+DATETIME_PATTERN = '%d/%m/%Y %H:%M'
+
+
+latest_name_changes = defaultdict(lambda: {USERNAME_COL_NAME: None, EMAIL_COL_NAME: None, DATETIME_COL_NAME: None})
+
+with open('name_log.csv', 'r', encoding='utf-8') as file_read:
+    name_changes = DictReader(file_read, delimiter=DELIMITER_SYM)
+    
+    for name_change in name_changes:
+        email = name_change[EMAIL_COL_NAME]
+        change_time = datetime.strptime(name_change[DATETIME_COL_NAME], DATETIME_PATTERN)
+
+        # if no record yet or if new change_time is later than the stored one
+        if latest_name_changes[email][DATETIME_COL_NAME] is None or change_time > latest_name_changes[email][DATETIME_COL_NAME]:
+            latest_name_changes[email] = {
+                USERNAME_COL_NAME: name_change[USERNAME_COL_NAME],
+                DATETIME_COL_NAME: change_time
+            }
+ 
+with open('new_name_log.csv', 'w', encoding='utf-8', newline='') as file_write:
+    writer = DictWriter(file_write, fieldnames=[USERNAME_COL_NAME, EMAIL_COL_NAME, DATETIME_COL_NAME])
+    writer.writeheader()
+    
+    for email, log in sorted(latest_name_changes.items(), key=lambda x: x[0]):
+        
+        formatted_datetime = log[DATETIME_COL_NAME].strftime(DATETIME_PATTERN)
+        writer.writerow({
+            USERNAME_COL_NAME: log[USERNAME_COL_NAME],
+            EMAIL_COL_NAME: email, 
+            DATETIME_COL_NAME: formatted_datetime
+        })
