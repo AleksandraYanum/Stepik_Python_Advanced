dd367241741246ac724ccc030943e3717ed15c4a
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-09-20 09:53:04 +0200

    Optimizeв datetime parsing by storing parsed value separately

--- 3_Professional/4_files/4_2_csv_files/11_log_file.py
+++ 3_Professional/4_files/4_2_csv_files/11_log_file.py
@@ -7,10 +7,12 @@ DELIMITER_SYM = ','
 USERNAME_COL_NAME = 'username'
 EMAIL_COL_NAME = 'email'
 DATETIME_COL_NAME = 'dtime'
+CHANGE_LOG_COL_NAME = 'change'
+CHANGE_TIME = 'time'
 DATETIME_PATTERN = '%d/%m/%Y %H:%M'
 
 
-latest_name_changes = defaultdict(lambda: None)
+latest_name_changes = defaultdict(lambda: {CHANGE_LOG_COL_NAME: None, CHANGE_TIME: None})
 
 
 with open('name_log.csv', 'r', encoding='utf-8') as file_read:
@@ -21,13 +23,13 @@ with open('name_log.csv', 'r', encoding='utf-8') as file_read:
         change_time = datetime.strptime(name_change[DATETIME_COL_NAME], DATETIME_PATTERN)
 
         # if no record yet or if new change_time is later than the stored one
-        if latest_name_changes[email] is None or change_time > datetime.strptime(latest_name_changes[email][DATETIME_COL_NAME], DATETIME_PATTERN):
-            latest_name_changes[email] = name_change
+        if latest_name_changes[email][CHANGE_TIME] is None or change_time > latest_name_changes[email][CHANGE_TIME]:
+            latest_name_changes[email] = {CHANGE_LOG_COL_NAME: name_change, CHANGE_TIME: change_time}
 
 
 with open('new_name_log.csv', 'w', encoding='utf-8', newline='') as file_write:
     writer = DictWriter(file_write, fieldnames=[USERNAME_COL_NAME, EMAIL_COL_NAME, DATETIME_COL_NAME])
     writer.writeheader()
     
-    for log in sorted(latest_name_changes.values(), key=lambda x: x[EMAIL_COL_NAME]):
-        writer.writerow(log)
+    for log in sorted(latest_name_changes.values(), key=lambda x: x[CHANGE_LOG_COL_NAME][EMAIL_COL_NAME]):
+        writer.writerow(log[CHANGE_LOG_COL_NAME])
