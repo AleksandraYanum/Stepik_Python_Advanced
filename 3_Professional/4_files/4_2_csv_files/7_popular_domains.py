4661ccb31070fbf93bec86166bc2084fc96fd286
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-09-20 15:22:44 +0200

    Renamed var

--- 3_Professional/4_files/4_2_csv_files/7_popular_domains.py
+++ 3_Professional/4_files/4_2_csv_files/7_popular_domains.py
@@ -15,17 +15,17 @@ def get_domain(email):
 
 
 def count_domain_usage(input_file, output_file):
-    domain_count = defaultdict(int)
+    domain_count_dict = defaultdict(int)
     
     with open(input_file, encoding='utf-8') as file:
         user_data = DictReader(file)
         
         for user in user_data:
             domain = get_domain(user[EMAIL_COL_NAME])
-            domain_count[domain] += 1 
+            domain_count_dict[domain] += 1 
     
     # Sort by amount and alphabet
-    sorted_domains = sorted(domain_count.items(), key=lambda x: (x[1], x[0]))
+    sorted_domains = sorted(domain_count_dict.items(), key=lambda x: (x[1], x[0]))
     
     with open(output_file, 'w', encoding='utf-8', newline='') as file:
         headers = [DOMAIN_COL_NAME, DOMAIN_AMOUNT_COL_NAME]
