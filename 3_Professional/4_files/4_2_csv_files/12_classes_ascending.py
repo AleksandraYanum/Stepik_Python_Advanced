f476abd59a25af564ca814bc600d18830cea6776
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-09-27 11:55:54 +0200

    Renamed var

--- 3_Professional/4_files/4_2_csv_files/12_classes_ascending.py
+++ 3_Professional/4_files/4_2_csv_files/12_classes_ascending.py
@@ -8,14 +8,14 @@ CLASS_DELIMITER = '-'
 def sorted_student_counts(input_file, output_file):
     with open(input_file, encoding='utf-8') as file:
         reader = DictReader(file)
-        data = [row for row in reader]  # list of dictionaries
+        classes_data = [row for row in reader]  # list of dictionaries
 
-    classes = sorted(data[0].keys() - {YEAR_COL_NAME}, 
+    classes = sorted(classes_data[0].keys() - {YEAR_COL_NAME}, 
                      key=lambda x: (int(x.split(CLASS_DELIMITER)[0]), x.split(CLASS_DELIMITER)[1]))
     sorted_headers = [YEAR_COL_NAME] + classes
     sorted_data = []
 
-    for row in data:
+    for row in classes_data:
         sorted_row = {YEAR_COL_NAME: row[YEAR_COL_NAME]}
         sorted_row.update({cls: row[cls] for cls in classes})
         sorted_data.append(sorted_row)
