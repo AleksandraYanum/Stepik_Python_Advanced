64da755049dee960baf92ceb993f11b5242260e9
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2024-10-01 13:56:49 +0200

    Extracted func

--- 3_Professional/4_files/4_2_csv_files/12_classes_ascending.py
+++ 3_Professional/4_files/4_2_csv_files/12_classes_ascending.py
@@ -11,31 +11,39 @@ CLASS_DELIMITER = '-'
 
 # Extract and sort class names from the first row
 def extract_sorted_classes(row, year_col_name=YEAR_COL_NAME, class_delimiter=CLASS_DELIMITER):
-    
+
     sorted_classes= sorted(row.keys() - {year_col_name}, 
                   key=lambda x: (int(x.split(class_delimiter)[0]), x.split(class_delimiter)[1]))
     return sorted_classes
 
 
+# Create a sorted row based on the classes' names
+def create_sorted_row(row, classes, year_col_name=YEAR_COL_NAME):
+    sorted_row = {year_col_name: row[year_col_name]}
+    sorted_row.update({cls: row[cls] for cls in classes})
+    return sorted_row
+
+
 def sorted_student_counts(input_file, output_file):
+
     with open(input_file, encoding='utf-8') as file:
+
         reader = DictReader(file)
-        classes_data = [row for row in reader]  # list of dictionaries
 
-    classes = sorted(classes_data[0].keys() - {YEAR_COL_NAME}, 
-                     key=lambda x: (int(x.split(CLASS_DELIMITER)[0]), x.split(CLASS_DELIMITER)[1]))
-    sorted_headers = [YEAR_COL_NAME] + classes
-    sorted_classes_data = []
-
-    for row in classes_data:
-        sorted_row = {YEAR_COL_NAME: row[YEAR_COL_NAME]}
-        sorted_row.update({cls: row[cls] for cls in classes})
-        sorted_classes_data.append(sorted_row)
+        first_row = next(reader)
+        classes = extract_sorted_classes(first_row)
+        sorted_headers = [YEAR_COL_NAME] + classes
+        sorted_classes_data = []
 
     with open(output_file, 'w', encoding='utf-8', newline='') as file:
+
         writer = DictWriter(file, fieldnames=sorted_headers)
         writer.writeheader()
         writer.writerows(sorted_classes_data)
+
+        sorted_row = {YEAR_COL_NAME: first_row[YEAR_COL_NAME]}
+        sorted_row.update({cls: first_row[cls] for cls in classes})
+        writer.writerow(sorted_row)
 
 
 def main():
